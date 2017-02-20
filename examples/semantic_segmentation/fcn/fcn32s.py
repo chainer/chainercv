93d05221febfb0ec076420bfd8e96d914bab8812
Yusuke Niitani <yuyuniitani@gmail.com>
2017-02-20 11:23:18 +0900

    improve fcn32

--- examples/semantic_segmentation/fcn/fcn32s.py
+++ examples/semantic_segmentation/fcn/fcn32s.py
@@ -21,7 +21,23 @@ def upsample_filt(size):
 
 class FCN32s(chainer.Chain):
 
-    """Full Convolutional Network 32s"""
+    """Full Convolutional Network 32s
+
+    Fully connected layers of VGG networks is used for their equivalent
+    convolutional layers.
+    Equivalent fully connected and
+    convolution layers have shapes O x I and O x I x H x W respectively for O
+    outputs channels, I input channels, H kernel height, and W kernel width.
+
+    TODO:
+        In the original fully convoltional neural networks implementation,
+        learning rate of bias term and W term differ. This is not implemented.
+        ``https://github.com/shelhamer/fcn.berkeleyvision.org/
+        blob/master/voc-fcn32s/net.py``.
+
+        Also, learning rate of the last layer is set to 0.
+
+    """
 
     def __init__(self, n_class=21):
         self.n_class = n_class
@@ -36,9 +52,17 @@ class FCN32s(chainer.Chain):
             score_fr=L.Convolution2D(4096, self.n_class, 1, stride=1, pad=0),
 
             upscore=L.Deconvolution2D(self.n_class, self.n_class, 64,
-                                      stride=32, pad=0, initialW=deconv_filter),
+                                      stride=32, pad=0, initialW=deconv_filter,
+                                      nobias=True),
         )
+        # copy fully connected layers' weights to equivalent conv layers
+        self.fc6.W.data[:] = self.vgg.fc6.W.data.reshape(self.fc6.W.shape)
+        self.fc6.b.data[:] = self.vgg.fc6.b.data.reshape(self.fc6.b.shape)
+        self.fc7.W.data[:] = self.vgg.fc7.W.data.reshape(self.fc7.W.shape)
+        self.fc7.b.data[:] = self.vgg.fc7.b.data.reshape(self.fc7.b.shape)
+
         self.vgg.conv1_1.pad = 100
+
         self.train = False
 
     def __call__(self, x, t=None):
