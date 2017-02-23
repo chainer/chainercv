c39b0f8808c671e90c7a5f66d0aae6caae2b77f8
Yusuke Niitani <yuyuniitani@gmail.com>
2017-02-23 12:27:23 +0900

    add detection dataset

--- chainer_cv/datasets/__init__.py
+++ chainer_cv/datasets/__init__.py
@@ -1,4 +1,4 @@
 from online_products.online_products_dataset import get_online_products  # NOQA
 from online_products.online_products_dataset import OnlineProductsDataset  # NOQA
-from pascal_voc.voc_semantic_segmentation import VOCSemanticSegmentationDataset  # NOQA
+from pascal_voc.voc_semantic_segmentation_dataset import VOCSemanticSegmentationDataset  # NOQA
 from imagenet.imagenet_dataset import ImagenetDataset  # NOQA
