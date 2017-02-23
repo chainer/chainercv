19470d30925ee187d62cc821f9880a8df6b77596
Yusuke Niitani <yuyuniitani@gmail.com>
2017-02-23 11:01:14 +0900

    flake8

--- chainer_cv/datasets/image_classification/imagenet_dataset.py
+++ chainer_cv/datasets/image_classification/imagenet_dataset.py
@@ -1,34 +1,26 @@
-
-import copy
-import numpy as np
-import os.path as osp
-from skimage.io import imread
-from skimage.color import gray2rgb
 import tarfile
 
 import chainer
 from chainer.dataset import download
 
 from chainer_cv import utils
-from chainer_cv.wrappers import KeepSubsetWrapper
 
 
 root = 'pfnet/chainer_cv/imagenet'
 
-def _get_imagenet(urls):
 
+def _get_imagenet(urls):
     data_root = download.get_dataset_directory(root)
-    #base_path = osp.join(data_root, 'Stanford_Online_Products')
-    #if osp.exists(base_path):
-    #    return base_path
+    # base_path = osp.join(data_root, 'Stanford_Online_Products')
+    # if osp.exists(base_path):
+    #     return base_path
 
     for url in urls:
         download_file_path = utils.cached_download(url)
 
         with tarfile.TarFile(download_file_path, 'r') as t:
             t.extractall(data_root)
-    return base_path
-
+    # return base_path
 
 
 class ImagenetDataset(chainer.datasets.ImageDataset):
@@ -48,7 +40,7 @@ class ImagenetDataset(chainer.datasets.ImageDataset):
         urls (list of strings): the list contains four urls of
             `[{Train images Tasks 1 & 2}, {Train images Task 3},
             {Validation Images}, {Test Images}]`.
-        
+
     """
 
     def __init__(self, data_dir='auto', urls=None):
