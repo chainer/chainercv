8a4abd80b42ac6997c807fb3289ad25d252af022
Yusuke Niitani <yuyuniitani@gmail.com>
2017-02-24 10:51:57 +0900

    add faster-rcnn more

--- examples/detection/faster_rcnn/setup.py
+++ examples/detection/faster_rcnn/setup.py
@@ -6,6 +6,7 @@ from Cython.Build import cythonize
 import numpy as np
 
 setup(
-    ext_modules = cythonize('lib/cython/bbox.pyx'),
+    ext_modules = cythonize(['lib/cython/bbox.pyx',
+                             'lib/cython/nms_cpu.pyx']),
     include_dirs=[np.get_include()]
 )
