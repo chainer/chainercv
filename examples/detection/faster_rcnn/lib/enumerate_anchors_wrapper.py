adc3682f22c4775994739c1de7dee9178fc77142
Yusuke Niitani <yuyuniitani@gmail.com>
2017-02-24 15:23:12 +0900

    add

--- examples/detection/faster_rcnn/lib/enumerate_anchors_wrapper.py
+++ examples/detection/faster_rcnn/lib/enumerate_anchors_wrapper.py
@@ -5,10 +5,16 @@ from region_proporsal.anchor_target_layer import AnchorTargetLayer
 
 class EnumerateAnchorsWrapper(DatasetWrapper):
 
-    def __init__(self, dataset):
+    def __init__(self, dataset, feature_shape):
+        super(EnumerateAnchorsWrapper, self).__init__(dataset)
         self.anchor_target_layer = AnchorTargetLayer()
+        self.feature_shape = feature_shape
 
-    
+    def _get_example(self, in_data):
+        img, bboxes = in_data
+        labels, bbox_targets, bbox_inside_weights, bbox_outside_weights =\
+            self.anchor_target_layer(bboxes, feature_shape, img.shape[2:])
+        return img, bboxes, labels, 
 
 
 if __name__ == '__main__':
