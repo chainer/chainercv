fccfc9b24de6059ac6db89d0c76ae2c96f4cb14f
Yusuke Niitani <yuyuniitani@gmail.com>
2017-03-03 20:57:06 +0900

    add to requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,8 +17,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
 import pkg_resources
 # import shlex
 # import sys
