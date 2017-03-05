492b0e28d9c1e2410ab612ac17ffa532a14dc8ee
Yusuke Niitani <yuyuniitani@gmail.com>
2017-03-05 16:21:51 +0900

    add doc to resize_transform

--- chainercv/transforms/resize_transform.py
+++ chainercv/transforms/resize_transform.py
@@ -3,9 +3,29 @@ import skimage.transform
 
 
 def resize(x, output_shape):
+    """Resize image to match the given shape.
+
+    Args:
+        x (~numpy.ndarray): array to be transformed. This is in CHW format.
+        output_shape (tuple): this is a tuple of length 2. Its elements are
+            ordered as (height, width).
+
+    """
+    if len(output_shape) != 2:
+        raise ValueError('length of the output_shape needs to be 2')
     x = x.transpose(1, 2, 0)
     scale = np.max(np.abs(x))
     x = skimage.transform.resize(
         x / scale, output_shape).astype(x.dtype)
     x = x.transpose(2, 0, 1) * scale
     return x
+
+
+if __name__ == '__main__':
+    from skimage.data import astronaut
+    data = astronaut().astype(np.float32)
+    data = data.transpose(2, 0, 1)
+    out = resize(data, (256, 128))
+    import matplotlib.pyplot as plt
+    plt.imshow(out.transpose(1, 2, 0).astype(np.uint8))
+    plt.show()
