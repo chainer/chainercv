2cf9af88b646ef204ea23390bf7d73fd4ce28623
Yusuke Niitani <yuyuniitani@gmail.com>
2017-03-10 15:33:23 +0900

    add vis_img_bbox to docs

--- chainercv/tasks/__init__.py
+++ chainercv/tasks/__init__.py
@@ -1,1 +1,2 @@
+from detection.vis_img_bbox import vis_img_bbox  # NOQA
 from pixel_correspondence.eval_pck import eval_pck  # NOQA
--- chainercv/tasks/detection/vis_img_bbox.py
+++ chainercv/tasks/detection/vis_img_bbox.py
@@ -1,13 +1,22 @@
 import numpy as np
 
-from matplotlib import pyplot as plt
-
 
 def vis_img_bbox(img, bboxes, label_names=None, ax=None):
     """Visualize bounding boxes inside image
 
     For notation, `H` and `W` are height and width of a image respectively.
     `R` is the number of bounding boxes in a image.
+
+    Example:
+
+        >>> import chainercv
+        >>> import matplotlib.pyplot as plot
+        >>> dataset = chainercv.datasets.VOCDetectionDataset()
+        >>> img, bboxes = dataset[10]
+        >>> img = chainercv.transforms.chw_to_pil_image(img)
+        >>> chainercv.tasks.vis_img_bbox(img, bboxes, dataset.labels)
+        >>> plot.show()
+
 
     Args:
         img (numpy.ndarray): array of shape (H, W, 3). This is in RGB format.
@@ -19,8 +28,9 @@ def vis_img_bbox(img, bboxes, label_names=None, ax=None):
             axis. If this is None, new axis is created.
 
     """
+    from matplotlib import pyplot as plot
     if ax is None:
-        fig = plt.figure()
+        fig = plot.figure()
         ax = fig.add_subplot(1, 1, 1)
     ax.imshow(img)
 
@@ -28,23 +38,9 @@ def vis_img_bbox(img, bboxes, label_names=None, ax=None):
         xy = (bbox[0], bbox[1])
         width = bbox[2] - bbox[0]
         height = bbox[3] - bbox[1]
-        ax.add_patch(plt.Rectangle(
+        ax.add_patch(plot.Rectangle(
             xy, width, height, fill=False, edgecolor='red', linewidth=3))
         if label_names is not None:
             ax.text(bbox[0], bbox[1], label_names[bbox[4].astype(np.int)],
                     style='italic',
                     bbox={'facecolor': 'white', 'alpha': 0.7, 'pad': 10})
-
-
-if __name__ == '__main__':
-    from chainercv.datasets import VOCDetectionDataset
-
-    dataset = VOCDetectionDataset()
-
-    for i in range(0, 100, 4):
-        fig = plt.figure(figsize=(14, 14))
-        for j in range(4):
-            ax = fig.add_subplot(2, 2, j + 1)
-            img, bboxes = dataset.get_raw_data(i + j)
-            vis_img_bbox(img, bboxes, dataset.labels, ax)
-        plt.show()
