ed41eeddf72e3bacb28696a34cdde49ba0aa7b83
Yusuke Niitani <yuyuniitani@gmail.com>
2017-03-13 13:29:42 +0900

    fix documentation of image transforms

--- chainercv/transforms/image/flip.py
+++ chainercv/transforms/image/flip.py
@@ -2,13 +2,13 @@ def flip(img, horizontal=False, vertical=False, copy=False):
     """Flip an image in vertical or horizontal direction as specified.
 
     Args:
-        img (numpy.ndarray): an array that gets flipped.
+        img (numpy.ndarray): An array that gets flipped. This is in CHW format.
         horizontal (bool): flip in horizontal direction
         vertical (bool): flip in vertical direction
         copy (bool): If False, a view of :obj:`img` will be returned.
 
     Returns:
-        Transformed :obj:`img`.
+        Transformed :obj:`img` in CHW format.
     """
     if horizontal:
         img = img[:, :, ::-1]
