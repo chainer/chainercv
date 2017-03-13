f4f6ae75b887dc7bc1923a5ffe6e382aa4f1b391
Yusuke Niitani <yuyuniitani@gmail.com>
2017-03-13 13:13:16 +0900

    add test data of 2007 PASCAL VOC

--- chainercv/datasets/pascal_voc/voc_utils.py
+++ chainercv/datasets/pascal_voc/voc_utils.py
@@ -10,22 +10,29 @@ urls = {
     '2012': 'http://host.robots.ox.ac.uk/pascal/VOC/voc2012/'
     'VOCtrainval_11-May-2012.tar',
     '2007': 'http://host.robots.ox.ac.uk/pascal/VOC/voc2007/'
-    'VOCtrainval_06-Nov-2007.tar'
+    'VOCtrainval_06-Nov-2007.tar',
+    '2007_test': 'http://host.robots.ox.ac.uk/pascal/VOC/voc2007/'
+    'VOCtest_06-Nov-2007.tar'
 }
 
 
-def get_pascal_voc(year):
+def get_pascal_voc(year, mode):
     if year not in urls:
         raise ValueError
+    key = year
+
+    if mode == 'test' and year == '2007':
+        key = '2007_test'
 
     data_root = download.get_dataset_directory(root)
     base_path = os.path.join(data_root, 'VOCdevkit/VOC{}'.format(year))
-    if os.path.exists(base_path):
+    mode_file = os.path.join(base_path, 'ImageSets/Main/{}.txt'.format(mode))
+    if os.path.exists(mode_file):
         # skip downloading
         return base_path
 
-    download_file_path = utils.cached_download(urls[year])
-    ext = os.path.splitext(urls[year])[1]
+    download_file_path = utils.cached_download(urls[key])
+    ext = os.path.splitext(urls[key])[1]
     utils.extractall(download_file_path, data_root, ext)
     return base_path
 
