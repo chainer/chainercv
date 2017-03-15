474441d8dfd3f64416d9a30b94200fc84babb2ae
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-03-15 22:45:08 +0900

    update docs

--- chainercv/transforms/image/resize.py
+++ chainercv/transforms/image/resize.py
@@ -30,9 +30,17 @@ def resize(img, output_shape):
 
     A bilinear interpolation is used for resizing.
 
+    This method uses `cv2` or `Pillow` for the backend.
+    If `cv2` is installed, it uses `cv2`. This backend is highly
+    recommended because it is faster than `Pillow`. If you are using Anaconda,
+    you can install it by following command.
+        $ conda install -c menpo opencv=2.4.11
+
+    If `cv2` is not installed, this method uses `Pillow`.
+
     Args:
         img (~numpy.ndarray): An array to be transformed.
-            This is in CHW format.
+            This is in CHW format and the type should be :obj:`numpy.float32`.
         output_shape (tuple): this is a tuple of length 2. Its elements are
             ordered as (height, width).
 
