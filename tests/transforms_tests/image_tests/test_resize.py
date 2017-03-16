9c4e6ecdb6698576fa096c4e49f003bc4dec120a
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-03-16 09:18:51 +0900

    skip some tests if cv2 is not installed

--- tests/transforms_tests/image_tests/test_resize.py
+++ tests/transforms_tests/image_tests/test_resize.py
@@ -5,15 +5,23 @@ import numpy as np
 from chainer import testing
 from chainercv.transforms import resize
 
+import importlib
+try:
+    importlib.import_module('cv2')
+    optional_modules = True
+except ImportError:
+    optional_modules = False
 
-class TestResize(unittest.TestCase):
 
-    def test_resize(self):
-        img = np.random.uniform(size=(3, 24, 32))
+if optional_modules:
+    class TestResize(unittest.TestCase):
 
-        out = resize(img, output_shape=(32, 64))
+        def test_resize(self):
+            img = np.random.uniform(size=(3, 24, 32))
 
-        self.assertEqual(out.shape, (3, 32, 64))
+            out = resize(img, output_shape=(32, 64))
+
+            self.assertEqual(out.shape, (3, 32, 64))
 
 
 testing.run_module(__name__, __file__)
--- tests/transforms_tests/image_tests/test_scale.py
+++ tests/transforms_tests/image_tests/test_scale.py
@@ -6,19 +6,28 @@ from chainer import testing
 from chainercv.transforms import scale
 
 
-class TestScale(unittest.TestCase):
+import importlib
+try:
+    importlib.import_module('cv2')
+    optional_modules = True
+except ImportError:
+    optional_modules = False
 
-    def test_scale_1(self):
-        img = np.random.uniform(size=(3, 24, 16))
 
-        out = scale(img, 8)
-        self.assertEqual(out.shape, (3, 12, 8))
+if optional_modules:
+    class TestScale(unittest.TestCase):
 
-    def test_scale_2(self):
-        img = np.random.uniform(size=(3, 16, 24))
+        def test_scale_1(self):
+            img = np.random.uniform(size=(3, 24, 16))
 
-        out = scale(img, 8)
-        self.assertEqual(out.shape, (3, 8, 12))
+            out = scale(img, 8)
+            self.assertEqual(out.shape, (3, 12, 8))
+
+            def test_scale_2(self):
+                img = np.random.uniform(size=(3, 16, 24))
+
+                out = scale(img, 8)
+                self.assertEqual(out.shape, (3, 8, 12))
 
 
 testing.run_module(__name__, __file__)
