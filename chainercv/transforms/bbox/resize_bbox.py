788635b599b3d2705ef657e119254ee56dbfab11
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-03-24 14:16:06 +0900

    fix resize_bbox

--- chainercv/transforms/bbox/resize_bbox.py
+++ chainercv/transforms/bbox/resize_bbox.py
@@ -11,9 +11,9 @@ def resize_bbox(bbox, input_shape, output_shape):
     Args:
         bbox (~numpy.ndarray): shape is :math:`(R, 4)`. :math:`R` is
             the number of bounding boxes.
-        input_shape (tuple): A tuple of length 2. The height and the width
+        input_shape (tuple): A tuple of length 2. The width and the height
             of the image before resized.
-        output_shape (tuple): A tuple of length 2. The height and the width
+        output_shape (tuple): A tuple of length 2. The width and the height
             of the image after resized.
 
     Returns:
@@ -22,8 +22,8 @@ def resize_bbox(bbox, input_shape, output_shape):
 
     """
     bbox = bbox.copy()
-    h_scale = float(output_shape[1]) / input_shape[1]
-    v_scale = float(output_shape[0]) / input_shape[0]
+    h_scale = float(output_shape[0]) / input_shape[0]
+    v_scale = float(output_shape[1]) / input_shape[1]
     bbox[:, 0] = h_scale * bbox[:, 0]
     bbox[:, 2] = h_scale * bbox[:, 2]
     bbox[:, 1] = v_scale * bbox[:, 1]
