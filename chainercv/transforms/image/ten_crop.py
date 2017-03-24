d0aa05bd7a88fe5e5346690820ca710daf84232c
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-03-24 14:13:19 +0900

    fix ten_crop

--- chainercv/transforms/image/ten_crop.py
+++ chainercv/transforms/image/ten_crop.py
@@ -25,16 +25,16 @@ def ten_crop(img, output_shape):
         img (~numpy.ndarray): An image array to be cropped. This is in
             CHW format.
         output_shape (tuple): the size of output images after cropping.
-            This value is :math:`(heihgt, width)`.
+            This value is :math:`(width, heihgt)`.
 
     Returns:
         The cropped arrays. The shape of tensor is :math:`(10, C, H, W)`.
 
     """
-    H, W = output_shape
+    W, H = output_shape
     iH, iW = img.shape[1:3]
 
-    if iH < H or iW < W:
+    if iW < W or iH < H:
         raise ValueError('shape of image is larger than output shape')
 
     crops = np.stack((
