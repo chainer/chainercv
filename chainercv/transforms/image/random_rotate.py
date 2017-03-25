d92848bc81602a6fd37a5cbb61a4621c2755dbcf
Yusuke Niitani <yuyuniitani@gmail.com>
2017-03-25 09:29:41 +0900

    change doc of return type

--- chainercv/transforms/image/random_rotate.py
+++ chainercv/transforms/image/random_rotate.py
@@ -10,19 +10,19 @@ def random_rotate(img, return_param=False):
         return_param (bool): returns information of rotation.
 
     Returns:
-        numpy.ndarray or tuple of numpy.ndarray and dict.
+        *ndarray* or *(ndarray, dict)*:
 
-            If :obj:`return_param = False`,\
-            returns an array :obj:`out_img` that is the result of rotation.
+        If :obj:`return_param = False`,\
+        returns an array :obj:`out_img` that is the result of rotation.
 
-            If :obj:`return_param = True`,\
-            returns a tuple whose elements are :obj:`out_img, param`.\
-            :obj:`param` is a dictionary of intermediate parameters whose
-            contents are listed below with key, value-type and the description
-            of the value.
+        If :obj:`return_param = True`,\
+        returns a tuple whose elements are :obj:`out_img, param`.\
+        :obj:`param` is a dictionary of intermediate parameters whose
+        contents are listed below with key, value-type and the description
+        of the value.
 
-            * **k** (*int*): The integer that represents the number of\
-                times the image is rotated by 90 degrees.
+        * **k** (*int*): The integer that represents the number of\
+            times the image is rotated by 90 degrees.
 
     """
     k = np.random.randint(4)
