f350c97e3bd6c902ff37d5be0e4910b61cac2d19
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-03-30 18:42:14 +0900

    improve tests

--- tests/transforms_tests/bbox_tests/test_non_maximum_suppression.py
+++ tests/transforms_tests/bbox_tests/test_non_maximum_suppression.py
@@ -9,17 +9,18 @@ from chainercv.transforms import non_maximum_suppression
 @testing.parameterize(
     {'threshold': 1, 'expect': (True, True, True, True)},
     {'threshold': 0.5, 'expect': (True, True, False, True)},
-    {'threshold': 0.3, 'expect': (True, True, False, False)},
-    {'threshold': 0.2, 'expect': (True, False, False, False)},
+    {'threshold': 0.3, 'expect': (True, False, True, True)},
+    {'threshold': 0.2, 'expect': (True, False, False, True)},
+    {'threshold': 0, 'expect': (True, False, False, False)},
 )
 class TestNonMaximumSuppression(unittest.TestCase):
 
     def setUp(self):
         self.bbox = np.array((
-            (1, 2, 3, 4),
-            (0, 1, 4, 5),  # 4/16
-            (1, 2, 3, 5),  # 4/6, 6/16
-            (1, 2, 5, 6),  # 6/16, 9/23, 4/16
+            (0, 0, 4, 4),
+            (1, 1, 5, 5),  # 9/23
+            (2, 1, 6, 5),  # 6/26, 12/20
+            (4, 0, 8, 4),  # 0/32, 3/29, 6/26
         ))
         self.expect = np.array(self.expect)
 
