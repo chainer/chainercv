063348080ea94b8e3221da06742773ecf8fc1d24
Yusuke Niitani <yuyuniitani@gmail.com>
2017-04-23 10:52:30 +0900

    use option fit_short

--- chainercv/transforms/image/scale.py
+++ chainercv/transforms/image/scale.py
@@ -1,15 +1,15 @@
 from chainercv.transforms import resize
 
 
-def scale(img, size, fit='short'):
+def scale(img, size, fit_short=True):
     """Rescales the input image to the given "size".
 
-    When :obj:`fit == short`, the input image will be resized so that
+    When :obj:`fit_short == True`, the input image will be resized so that
     the shorter edge will be scaled to length :obj:`size` after
     resizing. For example, if the height of the image is larger than
     its width, image will be resized to (size * height / width, size).
 
-    When :obj:`fit == long`, the input image will be resized so that
+    Otherwise, the input image will be resized so that
     the longer edge will be scaled to length :obj:`size` after
     resizing.
 
@@ -17,7 +17,7 @@ def scale(img, size, fit='short'):
         img (~numpy.ndarray): An image array to be scaled. This is in
             CHW format.
         size (int): The length of the smaller edge.
-        fit ({'short', 'long'}): Determines whether to match the length
+        fit_short (bool): Determines whether to match the length
             of the shorter edge or the longer edge to :obj:`size`.
 
     Returns:
@@ -28,16 +28,14 @@ def scale(img, size, fit='short'):
     if (W <= H and W == size) or (H <= W and H == size):
         return img
 
-    if fit == 'short':
+    if fit_short:
         if W < H:
             out_size = (size, int(size * H / W))
         else:
             out_size = (int(size * W / H), size)
-    elif fit == 'long':
-        if W < H:
-            out_size = (int(size * W / H), size)
-        else:
-            out_size = (size, int(size * H / W))
     else:
-        raise ValueError('fit needs to be either \'short\' or \'long\'')
+        if W < H:
+            out_size = (int(size * W / H), size)
+        else:
+            out_size = (size, int(size * H / W))
     return resize(img, out_size)
--- tests/transforms_tests/image_tests/test_scale.py
+++ tests/transforms_tests/image_tests/test_scale.py
@@ -8,20 +8,20 @@ from chainercv.transforms import scale
 
 @testing.parameterize(
     {'in_shape': (3, 24, 16), 'size': 8,
-     'fit': 'short', 'out_shape': (3, 12, 8)},
+     'fit_short': True, 'out_shape': (3, 12, 8)},
     {'in_shape': (3, 16, 24), 'size': 8,
-     'fit': 'short', 'out_shape': (3, 8, 12)},
+     'fit_short': True, 'out_shape': (3, 8, 12)},
     {'in_shape': (3, 24, 16), 'size': 36,
-     'fit': 'long', 'out_shape': (3, 36, 24)},
+     'fit_short': False, 'out_shape': (3, 36, 24)},
     {'in_shape': (3, 16, 24), 'size': 36,
-     'fit': 'long', 'out_shape': (3, 24, 36)}
+     'fit_short': False, 'out_shape': (3, 24, 36)}
 )
 class TestScale(unittest.TestCase):
 
     def test_scale(self):
         img = np.random.uniform(size=self.in_shape)
 
-        out = scale(img, self.size, fit=self.fit)
+        out = scale(img, self.size, fit_short=self.fit_short)
         self.assertEqual(out.shape, self.out_shape)
 
 
