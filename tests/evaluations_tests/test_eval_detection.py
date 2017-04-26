79be820d4a061fac357499f55c1656de6c1dd2ab
Yusuke Niitani <yuyuniitani@gmail.com>
2017-04-26 17:46:17 +0900

    more holistic test

--- tests/evaluations_tests/test_eval_detection.py
+++ tests/evaluations_tests/test_eval_detection.py
@@ -33,24 +33,37 @@ class TestEvalDetectionOneBbox(unittest.TestCase):
         np.testing.assert_equal(results[0]['precision'], self.prec)
 
 
-@testing.parameterize(
-    {'minoverlap': 0.4,
-     'rec': np.array([0.5, 0.5, 1.]),
-     'prec': np.array([1., 0.5, 2. / 3.])},
-)
-class TestEvalDetectionTwoBboxes(unittest.TestCase):
+class TestEvalDetectionMultipleBboxes(unittest.TestCase):
+
+    minoverlap = 0.4
+    rec0 = np.array([0.0, 0.5, 0.5])
+    prec0 = np.array([0., 0.5, 1. / 3.])
+    ap0 = 0.25
+    rec1 = np.array([0., 1.])
+    prec1 = np.array([0., 0.5])
+    ap1 = 0.5
+    mean_ap = 0.375
 
     def test_eval_detection(self):
-        bboxes = [np.array([
-            [0., 0., 1., 1.], [0., 0., 2., 3.], [1., 1., 3., 3.]])]
-        labels = [np.array([0, 0, 0])]
-        confs = [np.array([1., 0.9, 0.8])]
-        gt_bboxes = [np.array([[0., 0., 1., 1.], [1., 0., 4., 4.]])]
-        gt_labels = [np.array([0, 0])]
-        # iou is [1., 0.33, 0.083] and [0.09, 0.33, 0.45]
+        bboxes = [
+            np.array([[0., 4., 1., 5.], [0., 0., 1., 1.]]),
+            np.array([[0., 0., 2., 2.], [2., 2., 3., 3.], [5., 5., 7., 7.]])
+        ]
+        labels = [np.array([0, 0]), np.array([0, 1, 1])]
+        confs = [np.array([1., 0.9]), np.array([0.7, 0.6, 0.8])]
+        gt_bboxes = [
+            np.array([[0., 0., 1., 1.], [1., 0., 4., 4.]]),
+            np.array([[2., 2., 3., 3.]])
+        ]
+        gt_labels = [np.array([0, 0]), np.array([1])]
 
         results = eval_detection(
             bboxes, labels, confs, gt_bboxes, gt_labels,
-            n_class=1, minoverlap=self.minoverlap)
-        np.testing.assert_equal(results[0]['recall'], self.rec)
-        np.testing.assert_equal(results[0]['precision'], self.prec)
+            n_class=3, minoverlap=self.minoverlap)
+        np.testing.assert_equal(results[0]['recall'], self.rec0)
+        np.testing.assert_equal(results[0]['precision'], self.prec0)
+        np.testing.assert_equal(results[0]['ap'], self.ap0)
+        np.testing.assert_equal(results[1]['recall'], self.rec1)
+        np.testing.assert_equal(results[1]['precision'], self.prec1)
+        np.testing.assert_equal(results[1]['ap'], self.ap1)
+        np.testing.assert_equal(results['map'], self.mean_ap)
