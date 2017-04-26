943dcbbec5834622738c684d4b05cf97452e5526
Yusuke Niitani <yuyuniitani@gmail.com>
2017-04-26 16:21:51 +0900

    add eval_detection

---
+++ tests/evaluations_tests/test_eval_detection.py
@@ -0,0 +1,56 @@
+import unittest
+
+import numpy as np
+
+from chainercv.evaluations import eval_detection
+
+from chainer import testing
+
+
+@testing.parameterize(
+    {'minoverlap': 0.5,
+     'rec': np.array([0., 0., 1.]),
+     'prec': np.array([0., 0., 1. / 3.])},
+    {'minoverlap': 0.97,
+     'rec': np.array([0., 0., 0.]),
+     'prec': np.array([0., 0., 0.])}
+)
+class TestEvalDetectionOneBbox(unittest.TestCase):
+
+    def test_eval_detection(self):
+        bboxes = [np.array([
+            [0., 0., 1., 1.], [0., 0., 2., 2.], [0.3, 0.3, 0.5, 0.5]])]
+        labels = [np.array([0, 0, 0])]
+        confs = [np.array([0.8, 0.9, 1.])]
+        gt_bboxes = [np.array([[0., 0., 1., 0.9]])]
+        gt_labels = [np.array([0])]
+        # iou is [0.95, 0.422, 0.3789]
+
+        results = eval_detection(
+            bboxes, labels, confs, gt_bboxes, gt_labels,
+            n_class=1, minoverlap=self.minoverlap)
+        np.testing.assert_equal(results[0]['recall'], self.rec)
+        np.testing.assert_equal(results[0]['precision'], self.prec)
+
+
+@testing.parameterize(
+    {'minoverlap': 0.4,
+     'rec': np.array([0.5, 0.5, 1.]),
+     'prec': np.array([1., 0.5, 2. / 3.])},
+)
+class TestEvalDetectionTwoBboxes(unittest.TestCase):
+
+    def test_eval_detection(self):
+        bboxes = [np.array([
+            [0., 0., 1., 1.], [0., 0., 2., 3.], [1., 1., 3., 3.]])]
+        labels = [np.array([0, 0, 0])]
+        confs = [np.array([1., 0.9, 0.8])]
+        gt_bboxes = [np.array([[0., 0., 1., 1.], [1., 0., 4., 4.]])]
+        gt_labels = [np.array([0, 0])]
+        # iou is [1., 0.33, 0.083] and [0.09, 0.33, 0.45]
+
+        results = eval_detection(
+            bboxes, labels, confs, gt_bboxes, gt_labels,
+            n_class=1, minoverlap=self.minoverlap)
+        np.testing.assert_equal(results[0]['recall'], self.rec)
+        np.testing.assert_equal(results[0]['precision'], self.prec)
