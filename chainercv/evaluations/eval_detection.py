96f60aac076ad5281b3e9783d213ba414f426afb
Yusuke Niitani <yuyuniitani@gmail.com>
2017-04-27 14:51:27 +0900

    stop using xrange

--- chainercv/evaluations/eval_detection.py
+++ chainercv/evaluations/eval_detection.py
@@ -61,8 +61,8 @@ def eval_detection(
 
     valid_cls = np.zeros((n_class,), dtype=np.bool)
     n_img = len(bboxes)
-    _bboxes = [[None for _ in six.moves.range(n_img)] for _ in xrange(n_class)]
-    _confs = [[None for _ in six.moves.range(n_img)] for _ in xrange(n_class)]
+    _bboxes = [[None for _ in six.moves.range(n_img)] for _ in six.moves.range(n_class)]
+    _confs = [[None for _ in six.moves.range(n_img)] for _ in six.move.range(n_class)]
 
     for i in range(n_img):
         for cls in range(n_class):
