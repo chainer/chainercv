9d79ba48371c00a13cc6555e4d141f56b77e8b28
Yusuke Niitani <yuyuniitani@gmail.com>
2017-04-29 21:49:49 +0900

    add missing import for evaluations in __init__

--- chainercv/__init__.py
+++ chainercv/__init__.py
@@ -1,4 +1,5 @@
 from chainercv import datasets  # NOQA
+from chainercv import evaluations  # NOQA
 from chainercv import extensions  # NOQA
 from chainercv import functions  # NOQA
 from chainercv import links  # NOQA
