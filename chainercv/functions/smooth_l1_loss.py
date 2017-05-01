634d4514fe3540cda69cec527c31331605c835f8
Yusuke Niitani <yuyuniitani@gmail.com>
2017-05-01 12:23:07 +0900

    fix loss

--- chainercv/functions/smooth_l1_loss.py
+++ chainercv/functions/smooth_l1_loss.py
@@ -9,11 +9,11 @@ def smooth_l1_loss(x, t, inside_weights, outside_weights, sigma):
     sigma2 = sigma ** 2
     xp = chainer.cuda.get_array_module(x)
     diff = inside_weights * (x - t)
-    abs_diff = F.absolute(diff).data
-    flag = (abs_diff < (1. / sigma2))
+    abs_diff = F.absolute(diff)
+    flag = (abs_diff.data < (1. / sigma2)).astype(np.float32)
 
-    y = (flag.astype(np.float32) * 0.5 * F.square(diff) * sigma2 +
-         xp.logical_not(flag).astype(np.float32) * (abs_diff - 0.5 / sigma2))
+    y = (flag * (sigma2 / 2.) * F.square(diff) +
+         (1 - flag) * (abs_diff - 0.5 / sigma2))
 
     y = y * outside_weights
     y /= y.shape[0]
