aca34b98e4fd5bbdf87fd61b70773bc9459bd624
Yusuke Niitani <yuyuniitani@gmail.com>
2017-05-04 22:47:01 +0900

    only support bbox whose shape is (R, 4)

--- chainercv/links/utils/bbox/bbox_overlap.py
+++ chainercv/links/utils/bbox/bbox_overlap.py
@@ -27,16 +27,16 @@ def bbox_overlap(bbox, query_bbox):
         box in :obj:`query_bbox`.
 
     """
-    if bbox.shape[1] < 4 or query_bbox.shape[1] < 4:
+    if bbox.shape[1] != 4 or query_bbox.shape[1] != 4:
         raise IndexError
     xp = cuda.get_array_module(bbox)
 
     # left top
     lt = xp.maximum(bbox[:, None, :2], query_bbox[:, :2])
     # right bottom
-    rb = xp.minimum(bbox[:, None, 2:4], query_bbox[:, 2:])
+    rb = xp.minimum(bbox[:, None, 2:], query_bbox[:, 2:])
 
     area_i = xp.prod(rb - lt, axis=2) * (lt < rb).all(axis=2)
-    area_0 = xp.prod(bbox[:, 2:4] - bbox[:, :2], axis=1)
-    area_1 = xp.prod(query_bbox[:, 2:4] - query_bbox[:, :2], axis=1)
+    area_0 = xp.prod(bbox[:, 2:] - bbox[:, :2], axis=1)
+    area_1 = xp.prod(query_bbox[:, 2:] - query_bbox[:, :2], axis=1)
     return area_i / (area_0[:, None] + area_1 - area_i)
