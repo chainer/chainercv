53521acb1756db4b914db5614ab1b6a4bdd9cd3a
Yusuke Niitani <yuyuniitani@gmail.com>
2017-05-04 22:09:25 +0900

    delete semantic_segmentation_vis_report

--- chainercv/extensions/__init__.py
+++ chainercv/extensions/__init__.py
@@ -1,2 +1,1 @@
 from chainercv.extensions.detection.detection_vis_report import DetectionVisReport  # NOQA
-from chainercv.extensions.semantic_segmentation.semantic_segmentation_vis_report import SemanticSegmentationVisReport  # NOQA
