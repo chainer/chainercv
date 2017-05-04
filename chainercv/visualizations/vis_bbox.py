87862436f5ee9f3429869530ce715c0f52264ef7
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-05-04 14:46:59 +0900

    support score in vis_bbox

--- chainercv/visualizations/vis_bbox.py
+++ chainercv/visualizations/vis_bbox.py
@@ -3,7 +3,7 @@ import numpy as np
 from chainercv.visualizations.vis_image import vis_image
 
 
-def vis_bbox(img, bbox, label=None, label_names=None, ax=None):
+def vis_bbox(img, bbox, label=None, score=None, label_names=None, ax=None):
     """Visualize bounding boxes inside image.
 
     Example:
@@ -12,7 +12,8 @@ def vis_bbox(img, bbox, label=None, label_names=None, ax=None):
         >>> import matplotlib.pyplot as plot
         >>> dataset = chainercv.datasets.VOCDetectionDataset()
         >>> img, bbox, label = dataset[60]
-        >>> chainercv.visualizations.vis_bbox(img, bbox, label, dataset.labels)
+        >>> chainercv.visualizations.vis_bbox(
+                img, bbox, label, label_names=dataset.labels)
         >>> plot.show()
 
     Args:
@@ -20,12 +21,15 @@ def vis_bbox(img, bbox, label=None, label_names=None, ax=None):
             This is in BGR format and the range of its value is
             :math:`[0, 255]`.
         bbox (~numpy.ndarray): An array of shape :math:`(R, 4)`, where
-            :math:`R` is the number of bounding boxes in the image. Elements
-            are organized
+            :math:`R` is the number of bounding boxes in the image.
+            Each element is organized
             by :obj:`(x_min, y_min, x_max, y_max)` in the second axis.
         label (~numpy.ndarray): An integer array of shape :math:`(R,)`.
             The values correspond to id for label names stored in
             :obj:`label_names`. This is optional.
+        score (~numpy.ndarray): A float array of shape :math:`(R,)`.
+             Each value indicates how confident the prediction is.
+             This is optional.
         label_names (iterable of strings): Name of labels ordered according
             to label_ids. If this is :obj:`None`, labels will be skipped.
         ax (matplotlib.axes.Axis): The visualization is displayed on this
@@ -46,10 +50,19 @@ def vis_bbox(img, bbox, label=None, label_names=None, ax=None):
         height = bbox_elem[3] - bbox_elem[1]
         ax.add_patch(plot.Rectangle(
             xy, width, height, fill=False, edgecolor='red', linewidth=3))
-        if label_names is not None:
+
+        caption = list()
+
+        if label is not None and label_names is not None:
             label_elem = label[i]
+            caption.append(label_names[label_elem])
+        if score is not None:
+            score_elem = score[i]
+            caption.append('{:.2f}'.format(score_elem))
+
+        if len(caption) > 0:
             ax.text(bbox_elem[0], bbox_elem[1],
-                    label_names[label_elem.astype(np.int)],
+                    ': '.join(caption),
                     style='italic',
                     bbox={'facecolor': 'white', 'alpha': 0.7, 'pad': 10})
     return ax
