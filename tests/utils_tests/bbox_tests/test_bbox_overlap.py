505e8a691555beeb58cc7491d5b1f1590191d8d7
Yusuke Niitani <yuyuniitani@gmail.com>
2017-05-07 14:07:02 +0900

    change name to bbox_a and bbox_b in tests

--- tests/utils_tests/bbox_tests/test_bbox_overlap.py
+++ tests/utils_tests/bbox_tests/test_bbox_overlap.py
@@ -14,17 +14,17 @@ from chainercv.utils import bbox_overlap
 class TestBboxOverlap(unittest.TestCase):
 
     def setUp(self):
-        self.bbox = np.array([[0, 0, 8, 8]], dtype=np.float32)
-        self.query_bbox = np.array([[3, 5, 10, 12], [9, 10, 11, 12]],
-                                   dtype=np.float32)
+        self.bbox_a = np.array([[0, 0, 8, 8]], dtype=np.float32)
+        self.bbox_b = np.array([[3, 5, 10, 12], [9, 10, 11, 12]],
+                               dtype=np.float32)
 
         o0 = (5 * 3) / (8 * 8 + 7 * 7 - 5 * 3)
         o1 = 0.
         self.expected = np.array([[o0, o1]], dtype=np.float32)
 
-    def check(self, bbox, query_bbox, expected):
-        xp = cuda.get_array_module(bbox)
-        overlap = bbox_overlap(bbox, query_bbox)
+    def check(self, bbox_a, bbox_b, expected):
+        xp = cuda.get_array_module(bbox_a)
+        overlap = bbox_overlap(bbox_a, bbox_b)
 
         self.assertIsInstance(overlap, xp.ndarray)
         np.testing.assert_equal(
@@ -32,31 +32,31 @@ class TestBboxOverlap(unittest.TestCase):
             cuda.to_cpu(expected))
 
     def test_bbox_overlap_cpu(self):
-        self.check(self.bbox, self.query_bbox, self.expected)
+        self.check(self.bbox_a, self.bbox_b, self.expected)
 
     @attr.gpu
     def test_bbox_overlap_gpu(self):
         self.check(
-            cuda.to_gpu(self.bbox),
-            cuda.to_gpu(self.query_bbox),
+            cuda.to_gpu(self.bbox_a),
+            cuda.to_gpu(self.bbox_b),
             cuda.to_gpu(self.expected))
 
 
 class TestBboxOverlapInvalidShape(unittest.TestCase):
 
     def test_bbox_overlap_invalid_bbox(self):
-        bbox = np.array([[0, 0, 8]], dtype=np.float32)
-        query_bbox = np.array([[1, 1, 9, 9]], dtype=np.float32)
+        bbox_a = np.array([[0, 0, 8]], dtype=np.float32)
+        bbox_b = np.array([[1, 1, 9, 9]], dtype=np.float32)
 
         with self.assertRaises(IndexError):
-            bbox_overlap(bbox, query_bbox)
+            bbox_overlap(bbox_a, bbox_b)
 
     def test_bbox_overlap_invalid_query_bbox(self):
-        bbox = np.array([[0, 0, 8, 8]], dtype=np.float32)
-        query_bbox = np.array([[1, 1, 9]], dtype=np.float32)
+        bbox_a = np.array([[0, 0, 8, 8]], dtype=np.float32)
+        bbox_b = np.array([[1, 1, 9]], dtype=np.float32)
 
         with self.assertRaises(IndexError):
-            bbox_overlap(bbox, query_bbox)
+            bbox_overlap(bbox_a, bbox_b)
 
 
 testing.run_module(__name__, __file__)
