39465451089ce2d6b7943803024c5a37f61c1827
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-05-08 11:16:11 +0900

    remove utils.extension

--- chainercv/utils/__init__.py
+++ chainercv/utils/__init__.py
@@ -1,6 +1,4 @@
 from chainercv.utils.download import cached_download  # NOQA
 from chainercv.utils.download import extractall  # NOQA
-from chainercv.utils.extension import check_type  # NOQA
-from chainercv.utils.extension import forward  # NOQA
 from chainercv.utils.image import read_image  # NOQA
 from chainercv.utils.test import ConstantReturnModel  # NOQA
