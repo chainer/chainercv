8155105464ecc385e023c39d29525acc5b9895f4
Yusuke Niitani <yuyuniitani@gmail.com>
2017-05-13 16:59:38 +0900

    change directory to links/model

--- chainercv/links/__init__.py
+++ chainercv/links/__init__.py
@@ -1,1 +1,1 @@
-from chainercv.links.faster_rcnn.utils.generate_anchor_base import generate_anchor_base  # NOQA
+from chainercv.links.model.faster_rcnn.utils.generate_anchor_base import generate_anchor_base  # NOQA
