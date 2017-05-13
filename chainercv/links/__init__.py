4a541f0e0631eefcda73efa7a992ec845b71f4c0
Yusuke Niitani <yuyuniitani@gmail.com>
2017-05-13 17:14:48 +0900

    add anchor_target_creator

--- chainercv/links/__init__.py
+++ chainercv/links/__init__.py
@@ -1,6 +1,7 @@
 from chainercv.links.model.faster_rcnn.faster_rcnn import FasterRCNNBase  # NOQA
 from chainercv.links.model.faster_rcnn.faster_rcnn_vgg import FasterRCNNVGG16  # NOQA
 from chainercv.links.model.faster_rcnn.region_proposal_network import RegionProposalNetwork  # NOQA
+from chainercv.links.model.faster_rcnn.utils.anchor_target_creator import AnchorTargetCreator  # NOQA
 from chainercv.links.model.faster_rcnn.utils.bbox_regression_target import bbox_regression_target  # NOQA
 from chainercv.links.model.faster_rcnn.utils.bbox_regression_target import bbox_regression_target_inv  # NOQA
 from chainercv.links.model.faster_rcnn.utils.generate_anchor_base import generate_anchor_base  # NOQA
---
+++ tests/links_tests/model_tests/faster_rcnn_tests/utils_tests/test_anchor_target_creator.py
@@ -0,0 +1,118 @@
+import unittest
+
+import numpy as np
+
+from chainer import cuda
+from chainer import testing
+from chainer.testing import attr
+from chainer import utils
+
+from chainercv.links import AnchorTargetCreator
+
+
+def _generate_bbox(n, img_size, min_length, max_length):
+    W, H = img_size
+    x_min = np.random.uniform(0, W - max_length, size=(n,))
+    y_min = np.random.uniform(0, H - max_length, size=(n,))
+    x_max = x_min + np.random.uniform(min_length, max_length, size=(n,))
+    y_max = y_min + np.random.uniform(min_length, max_length, size=(n,))
+    bbox = np.stack((x_min, y_min, x_max, y_max), axis=1).astype(np.float32)
+    return bbox
+
+
+class TestAnchorTargetCreator(unittest.TestCase):
+
+    img_size = (320, 240)
+    rpn_batch_size = 256
+    n_anchor_base = 9
+    rpn_fg_fraction = 0.5
+    rpn_bbox_inside_weight = (1., 0.9, 0.8, 0.7)
+
+    def setUp(self):
+        n_bbox = 8
+        self.feat_size = (self.img_size[0] // 16, self.img_size[1] // 16)
+        n_anchor = self.n_anchor_base * np.prod(self.feat_size)
+
+        self.anchor = _generate_bbox(n_anchor, self.img_size, 16, 200)
+        self.bbox = _generate_bbox(n_bbox, self.img_size, 16, 200)[None]
+        self.anchor_target_layer = AnchorTargetCreator(
+            self.rpn_batch_size, rpn_fg_fraction=self.rpn_fg_fraction,
+            rpn_bbox_inside_weight=self.rpn_bbox_inside_weight
+        )
+
+    def check_anchor_target_creator(
+            self, anchor_target_layer,
+            bbox, anchor, feat_size, img_size):
+        xp = cuda.get_array_module(bbox)
+
+        bbox_target, label, bbox_inside_weight, bbox_outside_weight =\
+            self.anchor_target_layer(bbox, anchor, feat_size, img_size)
+
+        # Test types
+        self.assertIsInstance(bbox_target, xp.ndarray)
+        self.assertIsInstance(label, xp.ndarray)
+        self.assertIsInstance(bbox_inside_weight, xp.ndarray)
+        self.assertIsInstance(bbox_outside_weight, xp.ndarray)
+
+        # Test shapes
+        self.assertEqual(bbox_target.shape,
+                         (1, 4 * self.n_anchor_base) + self.feat_size[::-1])
+        self.assertEqual(label.shape,
+                         (1, self.n_anchor_base) + self.feat_size[::-1])
+        self.assertEqual(bbox_inside_weight.shape,
+                         (1, 4 * self.n_anchor_base) + self.feat_size[::-1])
+        self.assertEqual(bbox_outside_weight.shape,
+                         (1, 4 * self.n_anchor_base) + self.feat_size[::-1])
+
+        # Test ratio of foreground and background labels
+        np.testing.assert_equal(
+            cuda.to_cpu(utils.force_array(xp.sum(label >= 0))),
+            self.rpn_batch_size)
+        n_fg = cuda.to_cpu(utils.force_array(xp.sum(label == 1)))
+        n_bg = cuda.to_cpu(utils.force_array(xp.sum(label == 0)))
+        self.assertLessEqual(
+            n_fg, self.rpn_batch_size * self.rpn_fg_fraction)
+        self.assertLessEqual(n_bg, self.rpn_batch_size - n_fg)
+
+        # Test bbox_inside_weight
+        _bbox_inside_weight = bbox_inside_weight.reshape(
+            (1, self.n_anchor_base, 4) + self.feat_size[::-1])
+        _bbox_inside_weight = _bbox_inside_weight.transpose(0, 1, 3, 4, 2)
+        bbox_inside_weight_masked = _bbox_inside_weight[label == 1]
+        bbox_inside_weight_target = xp.tile(
+            xp.array(self.rpn_bbox_inside_weight),
+            bbox_inside_weight_masked.shape[0]).reshape(-1, 4)
+        np.testing.assert_almost_equal(
+            cuda.to_cpu(bbox_inside_weight_masked),
+            cuda.to_cpu(bbox_inside_weight_target))
+
+        # Test bbox_outside_weight
+        _bbox_outside_weight = bbox_outside_weight.reshape(
+            (1, self.n_anchor_base, 4) + self.feat_size[::-1])
+        _bbox_outside_weight = _bbox_outside_weight.transpose(0, 1, 3, 4, 2)
+        bbox_outside_weight_masked = _bbox_outside_weight[label >= 0]
+        bbox_outside_weight_target = xp.ones_like(
+            bbox_outside_weight_masked) / (n_fg + n_bg)
+        np.testing.assert_almost_equal(
+            cuda.to_cpu(bbox_outside_weight_masked),
+            cuda.to_cpu(bbox_outside_weight_target))
+
+    def test_anchor_target_creator_cpu(self):
+        self.check_anchor_target_creator(
+            self.anchor_target_layer,
+            self.bbox,
+            self.anchor,
+            self.feat_size,
+            self.img_size)
+
+    @attr.gpu
+    def test_anchor_target_creator_gpu(self):
+        self.check_anchor_target_creator(
+            self.anchor_target_layer,
+            cuda.to_gpu(self.bbox),
+            cuda.to_gpu(self.anchor),
+            self.feat_size,
+            self.img_size)
+
+
+testing.run_module(__name__, __file__)
