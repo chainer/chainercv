56a28a3400c4d2735623b5394d5f867fd0bec72a
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-05-25 12:52:31 +0900

    add docs

--- chainercv/utils/testing/apply_detection_link.py
+++ chainercv/utils/testing/apply_detection_link.py
@@ -2,6 +2,37 @@ import numpy as np
 
 
 def apply_detection_link(target, iterator, hook=None):
+    """Apply a detection link to an iterator
+
+    This function applies a detection link to an iterator.
+    It stacks the outputs of the detection link
+    into three lists,
+    :obj:`pred_bboxes`, :obj:`pred_labels` and :obj:`pred_scores`.
+    This function also stacks the values returned by the iterator.
+    These values can be used for evaluation.
+
+    Args:
+        target (chainer.Link): An detection link. This link must have
+            :meth:`predict` method which take a list of images and returns
+            :obj:`bboxes`, :obj:`labels` and :obj:`scores`.
+        iterator (chainer.Iterator): An iterator. Each samples must have
+            an image for its first element. This image is passed to
+            :obj:`target`. The rests are stacked into :obj:`gt_values`.
+        hook: An callable which called after each iteration.
+            :obj:`pred_bboxes`, :obj:`pred_labels`, :obj:`pred_scores` and
+            :obj:`gt_values` are passed. Note that these values do not contain
+            the data of previous iterations.
+
+    returns:
+        tuple of list:
+        This function returns four lists, :obj:`pred_bboxes`,
+        :obj:`pred_labels`, :obj:`pred_scores` and :obj:`gt_values`.
+        :obj:`gt_values` is a tuple of lists. Each list corresponds to an value
+        of samples from the iterator. For example, if the iterator returns
+        batches of :obj:`img, val0, val1`, :obj:`gt_values` will be
+        :obj:`([val0], [val1])`.
+    """
+
     pred_bboxes = list()
     pred_labels = list()
     pred_scores = list()
@@ -24,7 +55,7 @@ def apply_detection_link(target, iterator, hook=None):
                 batch_imgs.append(sample[0])
                 batch_gt_values.append(sample[1:])
 
-        batch_gt_values = [list(bv) for bv in zip(*batch_gt_values)]
+        batch_gt_values = tuple(list(bv) for bv in zip(*batch_gt_values))
 
         batch_pred_bboxes, batch_pred_labels, batch_pred_scores = \
             target.predict(batch_imgs)
