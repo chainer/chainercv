42e543880e600a15d2fe86cb54f193f3e8cac319
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-05-26 15:22:40 +0900

    add vis_label

--- chainercv/visualizations/__init__.py
+++ chainercv/visualizations/__init__.py
@@ -1,3 +1,4 @@
 from chainercv.visualizations.vis_bbox import vis_bbox  # NOQA
 from chainercv.visualizations.vis_image import vis_image  # NOQA
+from chainercv.visualizations.vis_label import vis_label  # NOQA
 from chainercv.visualizations.vis_keypoint import vis_keypoint  # NOQA
--- docs/source/reference/visualizations.rst
+++ docs/source/reference/visualizations.rst
@@ -12,6 +12,10 @@ vis_image
 ~~~~~~~~~
 .. autofunction:: vis_image
 
+vis_label
+~~~~~~~~~
+.. autofunction:: vis_label
+
 vis_keypoint
 ~~~~~~~~~~~~
 .. autofunction:: vis_keypoint
