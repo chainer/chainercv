a428068f8e5c1e6ab9ebf988e4f5d8b57478ca5b
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-05-26 15:34:15 +0900

    do not show image in vis_label

--- chainercv/visualizations/vis_label.py
+++ chainercv/visualizations/vis_label.py
@@ -1,7 +1,4 @@
-from chainercv.visualizations.vis_image import vis_image
-
-
-def vis_label(img, label, alpha=0.5, label_names=None, ax=None):
+def vis_label(label, alpha=1, label_names=None, ax=None):
     """Visualize label of semantic segmentation.
 
     Example:
@@ -12,26 +9,21 @@ def vis_label(img, label, alpha=0.5, label_names=None, ax=None):
         >>> from chainercv.visualizations import vis_label
         >>> import matplotlib.pyplot as plot
         >>> dataset = VOCSemanticSegmentationDataset()
-        >>> img, label = dataset[60]
-        >>> vis_label(img, label,
+        >>> _, label = dataset[60]
+        >>> vis_label(label,
         ...         label_names=voc_semantic segmentation_label_names)
         >>> plot.show()
 
     Args:
-        img (~numpy.ndarray): An array of shape :math:`(3, height, width)`.
-            This is in RGB format and the range of its value is
-            :math:`[0, 255]`. If this argument is specified, this function
-            blends :obj:`label` and :obj:`img` using :obj:`alpha`.
-            If :obj:`img` is :obj:`None`, this function shows only :obj:`label`
-            and ignores :obj:`alpha`.
         label (~numpy.ndarray): An integer array of shape
             :math:`(1, height, width)`.
             The values correspond to id for label names stored in
             :obj:`label_names`.
-        alpha (float): The value which is used for blending :obj:`label` and
-            :obj:`img`. The range of this value is :math:`[0, 1]`. If this
-            value is :obj:`1`, this function shows only :obj:`label`.
-            The default value is :obj:`0.5`.
+        alpha (float): The value which determines transparency of the figure.
+            The range of this value is :math:`[0, 1]`. If this
+            value is :obj:`0`, the figure will be completely transparent.
+            The default value is :obj:`1`. This option is useful for
+            overlaying labels on the source images.
         label_names (iterable of strings): Name of labels ordered according
             to label ids. If this is :obj:`None`, labels will be skipped.
         ax (matplotlib.axes.Axis): The visualization is displayed on this
@@ -47,13 +39,6 @@ def vis_label(img, label, alpha=0.5, label_names=None, ax=None):
     if ax is None:
         fig = plot.figure()
         ax = fig.add_subplot(1, 1, 1)
-
-    if img is None:
-        alpha = 1
-    else:
-        if not img.shape[1:] == label.shape[1:]:
-            raise ValueError('The size of image must be same as that of label')
-        vis_image(img, ax=ax)
 
     ax.imshow(label[0], vmin=-1, vmax=label.max(), alpha=alpha)
 
