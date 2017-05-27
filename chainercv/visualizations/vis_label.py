7575348a7edf2ea2b862249e8d190fc9f2237350
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-05-27 10:41:28 +0900

    change the order of arguments

--- chainercv/visualizations/vis_label.py
+++ chainercv/visualizations/vis_label.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def vis_label(label, alpha=1, label_names=None, ax=None):
+def vis_label(label, label_names=None, alpha=1, ax=None):
     """Visualize label of semantic segmentation.
 
     Example:
@@ -15,8 +15,9 @@ def vis_label(label, alpha=1, label_names=None, ax=None):
         >>> dataset = VOCSemanticSegmentationDataset()
         >>> img, label = dataset[60]
         >>> ax = vis_image(img)
-        >>> vis_label(label, alpha=0.75,
-        ...           label_names=voc_semantic_segmentation_label_names, ax=ax)
+        >>> vis_label(
+        ...     label, label_names=voc_semantic_segmentation_label_names,
+        ...     alpha=0.75, ax=ax)
         >>> plot.show()
 
     Args:
@@ -24,13 +25,13 @@ def vis_label(label, alpha=1, label_names=None, ax=None):
             :math:`(height, width)`.
             The values correspond to id for label names stored in
             :obj:`label_names`.
+        label_names (iterable of strings): Name of labels ordered according
+            to label ids. If this is :obj:`None`, labels will be skipped.
         alpha (float): The value which determines transparency of the figure.
             The range of this value is :math:`[0, 1]`. If this
             value is :obj:`0`, the figure will be completely transparent.
             The default value is :obj:`1`. This option is useful for
             overlaying labels on the source images.
-        label_names (iterable of strings): Name of labels ordered according
-            to label ids. If this is :obj:`None`, labels will be skipped.
         ax (matplotlib.axes.Axis): The visualization is displayed on this
             axis. If this is :obj:`None` (default), a new axis is created.
 
