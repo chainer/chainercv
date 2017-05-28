d6958e32f21d003c3a99cf7cc17fc1ca08e30b30
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-05-28 16:49:15 +0900

    update tests

--- tests/links_tests/model_tests/ssd_tests/test_multibox_loss.py
+++ tests/links_tests/model_tests/ssd_tests/test_multibox_loss.py
@@ -2,6 +2,7 @@ import numpy as np
 import unittest
 
 import chainer
+from chainer import cuda
 from chainer import testing
 from chainer.testing import attr
 
@@ -17,21 +18,26 @@ from chainercv.links.model.ssd import multibox_loss
 class TestMultiboxLoss(unittest.TestCase):
 
     def setUp(self):
-        self.x_loc = chainer.Variable(np.random.uniform(
-            -1, 1, size=(self.batchsize, self.n_bbox, 4))
-            .astype(np.float32))
-        self.x_conf = chainer.Variable(np.random.uniform(
-            -1, 1, size=(self.batchsize, self.n_bbox, self.n_class))
-            .astype(np.float32))
+        self.x_loc = np.random.uniform(
+            -1, 1, size=(self.batchsize, self.n_bbox, 4)) \
+            .astype(np.float32)
+        self.x_conf = np.random.uniform(
+            -1, 1, size=(self.batchsize, self.n_bbox, self.n_class)) \
+            .astype(np.float32)
 
-        self.t_loc = chainer.Variable(np.random.uniform(
-            -1, 1, size=(self.batchsize, self.n_bbox, 4))
-            .astype(np.float32))
-        self.t_conf = chainer.Variable(np.random.randint(
-            self.n_class, size=(self.batchsize, self.n_bbox))
-            .astype(np.int32))
+        self.t_loc = np.random.uniform(
+            -1, 1, size=(self.batchsize, self.n_bbox, 4)) \
+            .astype(np.float32)
+        self.t_conf = np.random.randint(
+            self.n_class, size=(self.batchsize, self.n_bbox)) \
+            .astype(np.int32)
 
     def _check_forward(self, x_loc, x_conf, t_loc, t_conf):
+        x_loc = chainer.Variable(x_loc)
+        x_conf = chainer.Variable(x_conf)
+        t_loc = chainer.Variable(t_loc)
+        t_conf = chainer.Variable(t_conf)
+
         loss_loc, loss_conf = multibox_loss(
             x_loc, x_conf, t_loc, t_conf, self.k)
 
@@ -51,12 +57,9 @@ class TestMultiboxLoss(unittest.TestCase):
 
     @attr.gpu
     def test_forward_gpu(self):
-        self.x_loc.to_gpu()
-        self.x_conf.to_gpu()
-        self.t_loc.to_gpu()
-        self.t_conf.to_gpu()
         self._check_forward(
-            self.x_loc, self.x_conf, self.t_loc, self.t_conf)
+            cuda.to_gpu(self.x_loc), cuda.to_gpu(self.x_conf),
+            cuda.to_gpu(self.t_loc), cuda.to_gpu(self.t_conf))
 
 
 testing.run_module(__name__, __file__)
