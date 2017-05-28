7890226d1f7c48e42888768467e41f29bf4b1422
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-05-28 16:32:36 +0900

    fix bug

--- chainercv/links/model/ssd/multibox_loss.py
+++ chainercv/links/model/ssd/multibox_loss.py
@@ -7,7 +7,7 @@ import chainer.functions as F
 def _elementwise_softmax_cross_entropy(x, t):
     assert x.shape[:-1] == t.shape
     shape = t.shape
-    x = F.reshape(x, (-1, shape[-1]))
+    x = F.reshape(x, (-1, x.shape[-1]))
     t = F.flatten(t)
     return F.reshape(
         F.softmax_cross_entropy(x, t, reduce='no'), shape)
@@ -56,16 +56,18 @@ def multibox_loss(x_loc, x_conf, t_loc, t_conf, k):
     xp = chainer.cuda.get_array_module(t_conf.data)
 
     positive = t_conf.data > 0
-    if xp.logical_not(positive).all():
+    n_positive = positive.sum()
+    if n_positive == 0:
         return 0, 0
 
     loss_loc = F.huber_loss(x_loc, t_loc, 1, reduce='no')
+    loss_loc = F.sum(loss_loc, axis=2)
     loss_loc *= positive.astype(loss_loc.dtype)
-    loss_loc = F.sum(loss_loc) / positive.sum()
+    loss_loc = F.sum(loss_loc) / n_positive
 
     loss_conf = _elementwise_softmax_cross_entropy(x_conf, t_conf)
     hard_negative = _hard_negative(loss_conf.data, positive, k)
     loss_conf *= xp.logical_or(positive, hard_negative).astype(loss_conf.dtype)
-    loss_conf = F.sum(loss_conf) / positive.sum()
+    loss_conf = F.sum(loss_conf) / n_positive
 
     return loss_loc, loss_conf
