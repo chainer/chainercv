32a9224221099898f38f9c1f4465111de32a021a
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-05-28 16:14:10 +0900

    add docs

--- chainercv/links/model/ssd/multibox_loss.py
+++ chainercv/links/model/ssd/multibox_loss.py
@@ -23,6 +23,36 @@ def _hard_negative(x, positive, k):
 
 
 def multibox_loss(x_loc, x_conf, t_loc, t_conf, k):
+    """Computes multibox loss
+
+    This is a loss function used in [#]_.
+
+    .. [#] Wei Liu, Dragomir Anguelov, Dumitru Erhan,
+       Christian Szegedy, Scott Reed, Cheng-Yang Fu, Alexander C. Berg.
+       SSD: Single Shot MultiBox Detector. ECCV 2016.
+
+    Args:
+        x_loc (chainer.Variable): A variable which indicates predicted
+            locations of bounding boxes. Its shape is :math:`(B, K, 4)`,
+            where :math:`B` is the number of samples in the batch and
+            :math:`K` is the number of default bounding boxes.
+        x_conf (chainer.Variable): A variable which indicates predicted
+            classes of bounding boxes. Its shape is :math:`(B, K, n\_class)`.
+            This function assumes the first class is background (negative).
+        t_loc (chainer.Variable): A variable which indicates ground truth
+            locations of bounding boxes. Its shape is :math:`(B, K, 4)`.
+        t_conf (chainer.Variable): A variable which indicates ground truth
+            classes of bounding boxes. Its shape is :math:`(B, K)`.
+        k (float): A coefficient which is used to hard negative mining.
+            This value determines the ratio between the number of positives
+            and that of mined negatives. The value used in the original paper
+            is :obj:`3`.
+
+    Returns:
+        tuple of chainer.Variable:
+        This function returns two :obj:`chainer.Variable`: :obj:`loss_loc` and
+        :obj:`loss_conf`.
+    """
     xp = chainer.cuda.get_array_module(t_conf.data)
 
     positive = t_conf.data > 0
