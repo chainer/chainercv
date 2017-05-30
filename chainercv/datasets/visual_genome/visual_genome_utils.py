3e8ff003383f54a399cf941a4a9cef986349bcb8
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2017-05-30 20:30:52 +0000

    Add docs and refactor functions

--- chainercv/datasets/visual_genome/visual_genome_utils.py
+++ chainercv/datasets/visual_genome/visual_genome_utils.py
@@ -18,7 +18,15 @@ region_descriptions_url = 'http://visualgenome.org/static/data/dataset/' \
 
 def get_visual_genome():
 
+    """Get the default path to the Visual Genome image directory.
+
+    Returns:
+        str: A path to the image directory.
+
+    """
+
     def move_files(src_dir, dst_dir):
+        # Move all files in the src_dir to the dst_dir and remove the src_dir
         for f in os.listdir(src_dir):
             src = os.path.join(src_dir, f)
             if os.path.isfile(src):
@@ -28,39 +36,42 @@ def get_visual_genome():
 
     data_root = download.get_dataset_directory(root)
     base_path = os.path.join(data_root, 'VG_100K_ALL')
+
     if os.path.exists(base_path):
         return base_path
 
+    print('Caching Visual Genome image files...')
     os.mkdir(base_path)
-    move_files(get_extract_data(vg_100k_url, data_root, 'VG_100K'), base_path)
-    move_files(get_extract_data(vg_100k_2_url, data_root, 'VG_100K_2'),
+    move_files(_get_extract_data(vg_100k_url, data_root, 'VG_100K'), base_path)
+    move_files(_get_extract_data(vg_100k_2_url, data_root, 'VG_100K_2'),
                base_path)
 
     return base_path
 
 
 def get_image_data():
+
+    """Get the default path to the image data JSON file.
+
+    Returns:
+        str: A path to the image data JSON file.
+
+    """
     data_root = download.get_dataset_directory(root)
-    return get_extract_data(image_data_url, data_root, 'image_data.json')
+    return _get_extract_data(image_data_url, data_root, 'image_data.json')
 
 
 def get_region_descriptions():
+
+    """Get the default path to the region descriptions JSON file.
+
+    Returns:
+        str: A path to the region descriptions JSON file.
+
+    """
     data_root = download.get_dataset_directory(root)
-    return get_extract_data(region_descriptions_url, data_root,
-                            'region_description.json')
-
-
-def get_extract_data(url, data_root, member_path):
-    base_path = os.path.join(data_root, member_path)
-
-    if os.path.exists(base_path):
-        return base_path
-
-    download_file_path = utils.cached_download(url)
-    ext = os.path.splitext(url)[1]
-    utils.extractall(download_file_path, data_root, ext)
-
-    return base_path
+    return _get_extract_data(region_descriptions_url, data_root,
+                             'region_descriptions.json')
 
 
 class VisualGenomeDatasetBase(chainer.dataset.DatasetMixin):
@@ -89,3 +100,16 @@ class VisualGenomeDatasetBase(chainer.dataset.DatasetMixin):
     def get_image(self, img_id):
         img_path = os.path.join(self.data_dir, str(img_id) + '.jpg')
         return utils.read_image(img_path, color=True)
+
+
+def _get_extract_data(url, data_root, member_path):
+    base_path = os.path.join(data_root, member_path)
+
+    if os.path.exists(base_path):
+        return base_path
+
+    download_file_path = utils.cached_download(url)
+    ext = os.path.splitext(url)[1]
+    utils.extractall(download_file_path, data_root, ext)
+
+    return base_path
