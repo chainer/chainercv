7430dabe21fe505e411a1fefc2fb05218af84057
Yusuke Niitani <yuyuniitani@gmail.com>
2017-05-30 18:28:09 +0900

    add iterator to test case

--- tests/evaluations_tests/test_eval_semantic_segmentation.py
+++ tests/evaluations_tests/test_eval_semantic_segmentation.py
@@ -8,7 +8,23 @@ from chainercv.evaluations import calc_confusion_matrix
 from chainercv.evaluations import eval_semantic_segmentation
 
 
+def _pred_iterator():
+    pred_labels = np.repeat([[[1, 1, 0], [0, 0, 1]]], 2, axis=0)
+    for pred_label in pred_labels:
+        yield pred_label
+
+
+def _gt_iterator():
+    gt_labels = np.repeat([[[1, 0, 0], [0, -1, 1]]], 2, axis=0)
+    for gt_label in gt_labels:
+        yield gt_label
+
+
 @testing.parameterize(
+    {'pred_labels': _pred_iterator(),
+     'gt_labels': _gt_iterator(),
+     'iou': np.array([4. / 6., 4. / 6.])
+     },
     {'pred_labels': np.repeat([[[1, 1, 0], [0, 0, 1]]], 2, axis=0),
      'gt_labels': np.repeat([[[1, 0, 0], [0, -1, 1]]], 2, axis=0),
      'iou': np.array([4. / 6., 4. / 6.])
