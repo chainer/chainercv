0afc2040b3a6792b8d64331a4f22c9a4fcf25f70
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-05-31 22:34:36 +0900

    add test

--- tests/utils_tests/iterator_tests/test_apply_prediction_to_iterator.py
+++ tests/utils_tests/iterator_tests/test_apply_prediction_to_iterator.py
@@ -81,4 +81,28 @@ class TestApplyPredictionToIterator(unittest.TestCase):
                     self.assertEqual(val, dataset_val)
 
 
+class TestApplyPredictionToIteratorWithInfiniteIterator(unittest.TestCase):
+
+    def test_apply_prediction_to_iterator_with_infinite_iterator(self):
+        def predict(imgs):
+            n_img = len(imgs)
+            return [np.random.uniform(size=(48, 64)) for _ in range(n_img)]
+
+        dataset = list()
+        for _ in range(5):
+            H, W = np.random.randint(8, 16, size=2)
+            dataset.append(np.random.randint(0, 256, size=(3, H, W)))
+
+        iterator = SerialIterator(dataset, 2)
+
+        imgs, pred_values, gt_values = apply_prediction_to_iterator(
+            predict, iterator)
+
+        for _ in range(10):
+            next(imgs)
+
+        for _ in range(10):
+            next(pred_values[0])
+
+
 testing.run_module(__name__, __file__)
