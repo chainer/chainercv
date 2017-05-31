a498fada233bf7450eea7f6e47a491e6767ac477
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-05-31 10:06:39 +0900

    add to __init__

--- chainercv/utils/iterator/__init__.py
+++ chainercv/utils/iterator/__init__.py
@@ -1,3 +1,4 @@
 from chainercv.utils.iterator.apply_detection_link import apply_detection_link  # NOQA
+from chainercv.utils.iterator.apply_prediction_to_iterator import apply_prediction_to_iterator  # NOQA
 from chainercv.utils.iterator.apply_semantic_segmentation_link import apply_semantic_segmentation_link  # NOQA
 from chainercv.utils.iterator.split_iterator import split_iterator  # NOQA
