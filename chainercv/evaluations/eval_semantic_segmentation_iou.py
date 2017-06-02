babf7c9f50282143ab8efee96a587bf5cb74123f
Yusuke Niitani <yuyuniitani@gmail.com>
2017-06-02 11:33:21 +0900

    reflect comment on eval_semantic_segmentation_iou

--- chainercv/evaluations/eval_semantic_segmentation_iou.py
+++ chainercv/evaluations/eval_semantic_segmentation_iou.py
@@ -1,42 +1,37 @@
 from __future__ import division
 
 import numpy as np
+import six
 
 
 def calc_semantic_segmentation_confusion(pred_labels, gt_labels, n_class):
     """Collect confusion matrix.
 
     Args:
-        pred_label (iterable of numpy.ndarray): A collection of predicted
+        pred_labels (iterable of numpy.ndarray): A collection of predicted
             labels. The shape of a label array
             is :math:`(H, W)`. :math:`H` and :math:`W`
             are height and width of the label.
-            For example, This is an array of shape :math:`(N, H, W)`.
-        gt_label (iterable of numpy.ndarray): A collection of ground
+        gt_labels (iterable of numpy.ndarray): A collection of ground
             truth label. The shape of a ground truth label array is
             :math:`(H, W)`. The corresponding prediction label should
             have the same shape.
-            A pixel with value "-1" will be ignored during evaluation.
+            A pixel with value :obj:`-1` will be ignored during evaluation.
         n_class (int): The number of classes.
 
     Returns:
         numpy.ndarray:
         A confusion matrix. Its shape is :math:`(n\_class, n\_class)`.
-        The :math:`(i, j)` th element corresponds to the number of times
-        a pixel with ground truth class :math:`i` is predicted
-        to be class :math:`j`.
+        The :math:`(i, j)` th element corresponds to the number of pixels
+        that are labeled as class :math:`i` by the ground truth and
+        class :math:`j` by the prediction.
 
     """
     pred_labels = iter(pred_labels)
     gt_labels = iter(gt_labels)
 
     confusion = np.zeros((n_class, n_class), dtype=np.int64)
-    while True:
-        try:
-            pred_label = next(pred_labels)
-            gt_label = next(gt_labels)
-        except StopIteration:
-            break
+    for pred_label, gt_label in six.moves.zip(pred_labels, gt_labels):
         if pred_label.ndim != 2 or gt_label.ndim != 2:
             raise ValueError('ndim of inputs should be two.')
         if pred_label.shape != gt_label.shape:
@@ -48,13 +43,21 @@ def calc_semantic_segmentation_confusion(pred_labels, gt_labels, n_class):
         confusion += np.bincount(
             n_class * gt_label[mask].astype(int) +
             pred_label[mask], minlength=n_class**2).reshape(n_class, n_class)
+
+    for iter_ in (pred_labels, gt_labels):
+        # This code assumes any iterator does not contain None as its items.
+        if next(iter_, None) is not None:
+            raise ValueError('Length of input iterables need to be same')
     return confusion
 
 
 def calc_semantic_segmentation_iou(confusion):
     """Calculate Intersection over Union with given confusion matrix.
 
-    The definition of Intersection over Union (IoU) is as follows.
+    The definition of Intersection over Union (IoU) is as follows,
+    where :math:`N_{ij}` is the number of pixels
+    that are labeled as class :math:`i` by the ground truth and
+    class :math:`j` by the prediction.
 
     * :math:`\\text{IoU of the i-th class} =  \
         \\frac{N_{ii}}{\\sum_{j=1}^k N_{ij} + \\sum_{j=1}^k N_{ji} - N_{ii}}`
@@ -62,14 +65,14 @@ def calc_semantic_segmentation_iou(confusion):
     Args:
         confusion (numpy.ndarray): A confusion matrix. Its shape is
             :math:`(n\_class, n\_class)`.
-            The :math:`(i, j)` th element corresponds to the number times
-            the pixel with ground truth class :math:`i` is predicted
-            to be class :math:`j`.
+            The :math:`(i, j)` th element corresponds to the number of pixels
+            that are labeled as class :math:`i` by the ground truth and
+            class :math:`j` by the prediction.
 
     Returns:
         numpy.ndarray:
-        IoU calculated from the given confusion matrix. Its shapes is
-        :math:`(n\_class)`.
+        An array of IoUs for the :math:`n\_class` classes. Its shape is
+        :math:`(n\_class,)`.
 
     """
     iou_denominator = (confusion.sum(axis=1) + confusion.sum(axis=0)
@@ -85,9 +88,10 @@ def eval_semantic_segmentation_iou(pred_labels, gt_labels, n_class):
     for the task of semantic segmentation.
 
     The definition of IoU and a related metric, mean Intersection
-    over Union (mIoU), are as follow, where
-    :math:`N_{ij}` is the amount of pixels of class :math:`i`
-    inferred to belong to :math:`j` and there is :math:`k` classes.
+    over Union (mIoU), are as follow,
+    where :math:`N_{ij}` is the number of pixels
+    that are labeled as class :math:`i` by the ground truth and
+    class :math:`j` by the prediction.
 
     * :math:`\\text{IoU of the i-th class} =  \
         \\frac{N_{ii}}{\\sum_{j=1}^k N_{ij} + \\sum_{j=1}^k N_{ji} - N_{ii}}`
@@ -97,7 +101,7 @@ def eval_semantic_segmentation_iou(pred_labels, gt_labels, n_class):
 
     mIoU can be computed by taking :obj:`numpy.nanmean` of the IoUs returned
     by this function.
-    The more detailed descriptions on the above metric can be found at a
+    The more detailed descriptions of the above metric can be found in a
     review on semantic segmentation [#]_.
 
     .. [#] Alberto Garcia-Garcia, Sergio Orts-Escolano, Sergiu Oprea, \
@@ -106,20 +110,23 @@ def eval_semantic_segmentation_iou(pred_labels, gt_labels, n_class):
     <https://arxiv.org/abs/1704.06857>`_. arXiv 2017.
 
     Args:
-        pred_label (iterable of numpy.ndarray): A collection of predicted
+        pred_labels (iterable of numpy.ndarray): A collection of predicted
             labels. The shape of a label array
             is :math:`(H, W)`. :math:`H` and :math:`W`
             are height and width of the label.
-        gt_label (iterable of numpy.ndarray): A collection of ground
-            truth label. The shape of a ground truth label array is
+            For example, this is a list of labels
+            :obj:`[label_0, label_1, ...]`, where
+            :obj:`label_i.shape= (H_i, W_i)`.
+        gt_labels (iterable of numpy.ndarray): A collection of ground
+            truth labels. The shape of a ground truth label array is
             :math:`(H, W)`. The corresponding prediction label should
             have the same shape.
-            A pixel with value "-1" will be ignored during evaluation.
+            A pixel with value :obj:`-1` will be ignored during evaluation.
         n_class (int): The number of classes.
 
     Returns:
         numpy.ndarray:
-        An IoU computed as above for each class. Its shape is
+        An array of IoUs for the :math:`n\_class` classes. Its shape is
         :math:`(n\_class,)`.
 
     """
