31a33a9fd76c7b3e8e23ca3c96a176a15f08f3cb
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-06-02 14:13:31 +0900

    separate functions

--- chainercv/evaluations/__init__.py
+++ chainercv/evaluations/__init__.py
@@ -1,3 +1,5 @@
-from chainercv.evaluations.eval_detection_voc import eval_detection_voc  # NOQA
+from chainercv.evaluations.eval_detection_voc_ap import calc_detection_voc_ap  # NOQA
+from chainercv.evaluations.eval_detection_voc_ap import calc_detection_voc_prec_rec  # NOQA
+from chainercv.evaluations.eval_detection_voc_ap import eval_detection_voc_ap  # NOQA
 from chainercv.evaluations.eval_pck import eval_pck  # NOQA
 from chainercv.evaluations.eval_semantic_segmentation import eval_semantic_segmentation  # NOQA
--- chainercv/evaluations/eval_detection_voc_ap.py
+++ chainercv/evaluations/eval_detection_voc_ap.py
@@ -8,10 +8,84 @@ import six
 from chainercv.utils.bbox.bbox_iou import bbox_iou
 
 
-def eval_detection_voc(
+def eval_detection_voc_ap(
         pred_bboxes, pred_labels, pred_scores, gt_bboxes, gt_labels,
         gt_difficults=None,
         iou_thresh=0.5, use_07_metric=False):
+    """Calculate detection metrics based on evaluation code of PASCAL VOC.
+
+    This function evaluates recall, precison and average precision with
+    respect to a class as well as mean average precision.
+    This evaluates predicted bounding boxes obtained from a dataset which
+    has :math:`N` images.
+
+    Mean average precision is calculated by taking a mean of average
+    precisions for all classes which have at least one bounding box
+    assigned by the predictions or the ground truth labels.
+    The code is based on the evaluation code used in PASCAL VOC Challenge.
+
+    Args:
+        pred_bboxes (iterable of numpy.ndarray): An iterable of :math:`N`
+            bounding boxes.
+            Its index corresponds to an index for the base dataset.
+            Each element of :obj:`pred_bboxes` is a set of coordinates
+            of bounding boxes. This is an array whose shape is :math:`(R, 4)`,
+            where :math:`R` corresponds
+            to the number of bounding boxes, which may vary among boxes.
+            The second axis corresponds to :obj:`x_min, y_min, x_max, y_max`
+            of a box.
+        pred_labels (iterable of numpy.ndarray): An iterable of labels.
+            Similar to :obj:`pred_bboxes`, its index corresponds to an
+            index for the base dataset. Its length is :math:`N`.
+        pred_scores (iterable of numpy.ndarray): An iterable of confidence
+            scores for predicted bounding boxes. Similar to :obj:`pred_bboxes`,
+            its index corresponds to an index for the base dataset.
+            Its length is :math:`N`.
+        gt_bboxes (iterable of numpy.ndarray): An iterable of ground truth
+            bounding boxes
+            whose length is :math:`N`. An element of :obj:`gt_bboxes` is a
+            bounding box whose shape is :math:`(R, 4)`. Note that the number of
+            bounding boxes in each image does not need to be same as the number
+            of corresponding predicted boxes.
+        gt_labels (iterable of numpy.ndarray): An iterable of ground truth
+            labels which are organized similarly to :obj:`gt_bboxes`.
+        gt_difficults (iterable of numpy.ndarray): An iterable of boolean
+            arrays which is organized similarly to :obj:`gt_bboxes`.
+            This tells whether the
+            corresponding ground truth bounding box is difficult or not.
+            By default, this is :obj:`None`. In that case, this function
+            considers all bounding boxes to be not difficult.
+        iou_thresh (float): A prediction is correct if its Intersection over
+            Union with the ground truth is above this value.
+        use_07_metric (bool): Whether to use PASCAL VOC 2007 evaluation metric
+            for calculating average precision. The default value is
+            :obj:`False`.
+
+    Returns:
+        list:
+
+        This function returns a list of average precisions.
+        The :math:`l`-th value corresponds to the average precision
+        for class :math:`l`. If class :math:`l` does not exist in
+        either :obj:`pred_labels` or :obj:`gt_labels`, the corresponding
+        value is set to :obj:`None`.
+
+    """
+
+    prec, rec = calc_detection_voc_prec_rec(
+        pred_bboxes, pred_labels, pred_scores,
+        gt_bboxes, gt_labels, gt_difficults,
+        iou_thresh=iou_thresh)
+
+    ap = calc_detection_voc_ap(prec, rec)
+
+    return ap
+
+
+def calc_detection_voc_prec_rec(
+        pred_bboxes, pred_labels, pred_scores, gt_bboxes, gt_labels,
+        gt_difficults=None,
+        iou_thresh=0.5):
     """Calculate detection metrics based on evaluation code of PASCAL VOC.
 
     This function evaluates recall, precison and average precision with
@@ -150,8 +224,10 @@ def eval_detection_voc(
         if next(iter_, None) is not None:
             raise ValueError('Length of input iterables need to be same.')
 
-    # Accumulate recall, precison and ap
-    results = {}
+    n_fg_class = max(n_pos.keys()) + 1
+    prec = [None] * n_fg_class
+    rec = [None] * n_fg_class
+
     for l in n_pos.keys():
         score_l = np.array(score[l])
         match_l = np.array(match[l], dtype=np.int8)
@@ -161,41 +237,44 @@ def eval_detection_voc(
 
         tp = np.cumsum(match_l == 1)
         fp = np.cumsum(match_l == 0)
-        rec = tp / n_pos[l]
-        prec = tp / np.maximum(fp + tp, np.finfo(np.float64).eps)
 
-        ap = _voc_ap(rec, prec, use_07_metric=use_07_metric)
-        results[l] = {}
-        results[l]['recall'] = rec
-        results[l]['precision'] = prec
-        results[l]['ap'] = ap
-    results['map'] = np.asscalar(np.mean(
-        [result['ap'] for result in results.values()]))
-    return results
+        prec[l] = tp / np.maximum(fp + tp, np.finfo(np.float64).eps)
+        rec[l] = tp / n_pos[l]
+
+    return prec, rec
 
 
-def _voc_ap(rec, prec, use_07_metric=False):
-    if use_07_metric:
-        # 11 point metric
-        ap = 0.
-        for t in np.arange(0., 1.1, 0.1):
-            if np.sum(rec >= t) == 0:
-                p = 0
-            else:
-                p = np.max(prec[rec >= t])
-            ap = ap + p / 11.
-    else:
-        # correct AP calculation
-        # first append sentinel values at the end
-        mrec = np.concatenate(([0.], rec, [1.]))
-        mpre = np.concatenate(([0.], prec, [0.]))
+def calc_detection_voc_ap(prec, rec, use_07_metric=False):
+    ap = list()
+    for prec_l, rec_l in six.moves.zip(prec, rec):
+        if prec is None or rec is None:
+            ap.append(None)
+            continue
 
-        mpre = np.maximum.accumulate(mpre[::-1])[::-1]
+        if use_07_metric:
+            # 11 point metric
+            ap_l = 0
+            for t in np.arange(0., 1.1, 0.1):
+                if np.sum(rec >= t) == 0:
+                    p = 0
+                else:
+                    p = np.max(prec[rec >= t])
+                ap_l += p / 11
+        else:
+            # correct AP calculation
+            # first append sentinel values at the end
+            mrec = np.concatenate(([0.], rec, [1.]))
+            mpre = np.concatenate(([0.], prec, [0.]))
 
-        # to calculate area under PR curve, look for points
-        # where X axis (recall) changes value
-        i = np.where(mrec[1:] != mrec[:-1])[0]
+            mpre = np.maximum.accumulate(mpre[::-1])[::-1]
 
-        # and sum (\Delta recall) * prec
-        ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+            # to calculate area under PR curve, look for points
+            # where X axis (recall) changes value
+            i = np.where(mrec[1:] != mrec[:-1])[0]
+
+            # and sum (\Delta recall) * prec
+            ap_l = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+
+        ap.append(ap_l)
+
     return ap
