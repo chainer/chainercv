569e66fe020b9e13316d9c5fb5871bffdb1570c9
Yusuke Niitani <yuyuniitani@gmail.com>
2017-06-02 11:37:50 +0900

    fix test

--- tests/evaluations_tests/test_eval_semantic_segmentation_iou.py
+++ tests/evaluations_tests/test_eval_semantic_segmentation_iou.py
@@ -11,21 +11,9 @@ from chainercv.evaluations import calc_semantic_segmentation_iou
 from chainercv.evaluations import eval_semantic_segmentation_iou
 
 
-def _pred_iterator():
-    pred_labels = np.repeat([[[1, 1, 0], [0, 0, 1]]], 2, axis=0)
-    for pred_label in pred_labels:
-        yield pred_label
-
-
-def _gt_iterator():
-    gt_labels = np.repeat([[[1, 0, 0], [0, -1, 1]]], 2, axis=0)
-    for gt_label in gt_labels:
-        yield gt_label
-
-
 @testing.parameterize(
-    {'pred_labels': _pred_iterator(),
-     'gt_labels': _gt_iterator(),
+    {'pred_labels': iter(np.repeat([[[1, 1, 0], [0, 0, 1]]], 2, axis=0)),
+     'gt_labels': iter(np.repeat([[[1, 0, 0], [0, -1, 1]]], 2, axis=0)),
      'iou': np.array([4. / 6., 4. / 6.])
      },
     {'pred_labels': np.array([[[0, 0, 0], [0, 0, 0]]]),
