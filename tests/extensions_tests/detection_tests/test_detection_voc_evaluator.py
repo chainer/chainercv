93996c171f9a94709953273a700545d6a7fce821
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-06-04 02:37:10 +0900

    fix test

--- tests/extensions_tests/detection_tests/test_detection_voc_evaluator.py
+++ tests/extensions_tests/detection_tests/test_detection_voc_evaluator.py
@@ -56,27 +56,27 @@ class TestDetectionVOCEvaluator(unittest.TestCase):
         # evaluator collect results in order to calculate their mean.
         self.assertEqual(len(reporter.observation), 0)
 
-        self.assertEqual(mean['target/map'], self.expected_ap)
-        self.assertIs(mean['target/ap/cls0'], np.nan)
-        self.assertEqual(mean['target/ap/cls1'], self.expected_ap)
-        self.assertIs(mean['target/ap/cls2'], np.nan)
+        np.testing.assert_equal(mean['target/map'], self.expected_ap)
+        np.testing.assert_equal(mean['target/ap/cls0'], np.nan)
+        np.testing.assert_equal(mean['target/ap/cls1'], self.expected_ap)
+        np.testing.assert_equal(mean['target/ap/cls2'], np.nan)
 
     def test_call(self):
         mean = self.evaluator()
         # main is used as default
-        self.assertEqual(mean['main/map'], self.expected_ap)
-        self.assertIs(mean['main/ap/cls0'], np.nan)
-        self.assertEqual(mean['main/ap/cls1'], self.expected_ap)
-        self.assertIs(mean['main/ap/cls2'], np.nan)
+        np.testing.assert_equal(mean['main/map'], self.expected_ap)
+        np.testing.assert_equal(mean['main/ap/cls0'], np.nan)
+        np.testing.assert_equal(mean['main/ap/cls1'], self.expected_ap)
+        np.testing.assert_equal(mean['main/ap/cls2'], np.nan)
 
     def test_evaluator_name(self):
         self.evaluator.name = 'eval'
         mean = self.evaluator()
         # name is used as a prefix
-        self.assertEqual(mean['eval/main/map'], self.expected_ap)
-        self.assertIs(mean['eval/main/ap/cls0'], np.nan)
-        self.assertEqual(mean['eval/main/ap/cls1'], self.expected_ap)
-        self.assertIs(mean['eval/main/ap/cls2'], np.nan)
+        np.testing.assert_equal(mean['eval/main/map'], self.expected_ap)
+        np.testing.assert_equal(mean['eval/main/ap/cls0'], np.nan)
+        np.testing.assert_equal(mean['eval/main/ap/cls1'], self.expected_ap)
+        np.testing.assert_equal(mean['eval/main/ap/cls2'], np.nan)
 
     def test_current_report(self):
         reporter = chainer.Reporter()
