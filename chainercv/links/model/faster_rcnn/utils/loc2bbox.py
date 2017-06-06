7c0bf7f9aecad10141b26c07ac0c1755aa5d7850
Yusuke Niitani <yuyuniitani@gmail.com>
2017-06-06 14:37:25 +0900

    fix faster r-cnn

--- chainercv/links/model/faster_rcnn/utils/loc2bbox.py
+++ chainercv/links/model/faster_rcnn/utils/loc2bbox.py
@@ -8,16 +8,16 @@ def loc2bbox(src_bbox, loc):
     :meth:`bbox2loc`, this function decodes the representation to
     coordinates in 2D image coordinates.
 
-    Given scales and offsets :math:`t_x, t_y, t_w, t_h` and a bounding
-    box whose center is :math:`p_x, p_y` and size :math:`p_w, p_h`,
-    the decoded bounding box's center :math:`\\hat{g}_x`, :math:`\\hat{g}_y`
-    and size :math:`\\hat{g}_w`, :math:`\\hat{g}_h` are calculated
+    Given scales and offsets :math:`t_y, t_x, t_h, t_w` and a bounding
+    box whose center is :math:`(y, x) = p_y, p_x` and size :math:`p_h, p_w`,
+    the decoded bounding box's center :math:`\\hat{g}_y`, :math:`\\hat{g}_x`
+    and size :math:`\\hat{g}_h`, :math:`\\hat{g}_w` are calculated
     by the following formulas.
 
-    * :math:`\\hat{g}_x = p_w t_x + p_x`
     * :math:`\\hat{g}_y = p_h t_y + p_y`
-    * :math:`\\hat{g}_w = p_w \\exp(t_w)`
+    * :math:`\\hat{g}_x = p_w t_x + p_x`
     * :math:`\\hat{g}_h = p_h \\exp(t_h)`
+    * :math:`\\hat{g}_w = p_w \\exp(t_w)`
 
     The decoding formulas are used in works such as R-CNN [#]_.
 
@@ -30,16 +30,16 @@ def loc2bbox(src_bbox, loc):
     Args:
         src_bbox (array): A coordinates of bounding boxes.
             Its shape is :math:`(R, 4)`. These coordinates are used to
-            compute :math:`p_x, p_y, p_w, p_h`.
+            compute :math:`p_y, p_x, p_h, p_w`.
         loc (array): An array with offsets and scales.
             The shapes of :obj:`src_bbox` and :obj:`loc` should be same.
-            This contains values :math:`t_x, t_y, t_w, t_h`.
+            This contains values :math:`t_y, t_x, t_h, t_w`.
 
     Returns:
         array:
         Decoded bounding box coordinates. Its shape is :math:`(R, 4)`. \
         The second axis contains four values \
-        :math:`\\hat{g}_x, \\hat{g}_y, \\hat{g}_w, \\hat{g}_h`.
+        :math:`\\hat{g}_y, \\hat{g}_x, \\hat{g}_h, \\hat{g}_w`.
 
     """
     xp = cuda.get_array_module(src_bbox)
@@ -49,25 +49,25 @@ def loc2bbox(src_bbox, loc):
 
     src_bbox = src_bbox.astype(src_bbox.dtype, copy=False)
 
-    src_width = src_bbox[:, 2] - src_bbox[:, 0]
-    src_height = src_bbox[:, 3] - src_bbox[:, 1]
-    src_ctr_x = src_bbox[:, 0] + 0.5 * src_width
-    src_ctr_y = src_bbox[:, 1] + 0.5 * src_height
+    src_height = src_bbox[:, 2] - src_bbox[:, 0]
+    src_width = src_bbox[:, 3] - src_bbox[:, 1]
+    src_ctr_y = src_bbox[:, 0] + 0.5 * src_height
+    src_ctr_x = src_bbox[:, 1] + 0.5 * src_width
 
-    dx = loc[:, 0::4]
-    dy = loc[:, 1::4]
-    dw = loc[:, 2::4]
-    dh = loc[:, 3::4]
+    dy = loc[:, 0::4]
+    dx = loc[:, 1::4]
+    dh = loc[:, 2::4]
+    dw = loc[:, 3::4]
 
-    ctr_x = dx * src_width[:, xp.newaxis] + src_ctr_x[:, xp.newaxis]
     ctr_y = dy * src_height[:, xp.newaxis] + src_ctr_y[:, xp.newaxis]
-    w = xp.exp(dw) * src_width[:, xp.newaxis]
+    ctr_x = dx * src_width[:, xp.newaxis] + src_ctr_x[:, xp.newaxis]
     h = xp.exp(dh) * src_height[:, xp.newaxis]
+    w = xp.exp(dw) * src_width[:, xp.newaxis]
 
     dst_bbox = xp.zeros(loc.shape, dtype=loc.dtype)
-    dst_bbox[:, 0::4] = ctr_x - 0.5 * w
-    dst_bbox[:, 1::4] = ctr_y - 0.5 * h
-    dst_bbox[:, 2::4] = ctr_x + 0.5 * w
-    dst_bbox[:, 3::4] = ctr_y + 0.5 * h
+    dst_bbox[:, 0::4] = ctr_y - 0.5 * h
+    dst_bbox[:, 1::4] = ctr_x - 0.5 * w
+    dst_bbox[:, 2::4] = ctr_y + 0.5 * h
+    dst_bbox[:, 3::4] = ctr_x + 0.5 * w
 
     return dst_bbox
