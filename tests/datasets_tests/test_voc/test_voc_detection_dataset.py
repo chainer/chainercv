4dcb9d0a52cd4bf04d69ca3d17fc14caacab0575
Yusuke Niitani <yuyuniitani@gmail.com>
2017-06-06 16:19:52 +0900

    use_repeat

--- tests/datasets_tests/test_voc/test_voc_detection_dataset.py
+++ tests/datasets_tests/test_voc/test_voc_detection_dataset.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 
 from chainer import testing
+from chainer.testing import condition
 from chainer.testing import attr
 
 from chainercv.datasets import voc_detection_label_names
@@ -34,38 +35,39 @@ class TestVOCDetectionDataset(unittest.TestCase):
         self.n_out = 4 if self.return_difficult else 3
 
     @attr.slow
+    @condition.repeat(10)
     def test_get_example(self):
-        for i in np.random.randint(0, len(self.dataset), size=(10,)):
-            out = self.dataset[i]
+        i = np.random.randint(0, len(self.dataset))
+        out = self.dataset[i]
 
-            self.assertEqual(len(out), self.n_out)
+        self.assertEqual(len(out), self.n_out)
 
-            img, bbox, label = out[:3]
+        img, bbox, label = out[:3]
 
-            self.assertIsInstance(img, np.ndarray)
-            self.assertEqual(img.dtype, np.float32)
-            self.assertEqual(img.shape[0], 3)
-            self.assertEqual(img.ndim, 3)
-            self.assertGreaterEqual(np.min(img), 0)
-            self.assertLessEqual(np.max(img), 255)
+        self.assertIsInstance(img, np.ndarray)
+        self.assertEqual(img.dtype, np.float32)
+        self.assertEqual(img.shape[0], 3)
+        self.assertEqual(img.ndim, 3)
+        self.assertGreaterEqual(np.min(img), 0)
+        self.assertLessEqual(np.max(img), 255)
 
-            self.assertIsInstance(bbox, np.ndarray)
-            self.assertEqual(bbox.dtype, np.float32)
-            self.assertEqual(bbox.ndim, 2)
-            self.assertEqual(bbox.shape[1], 4)
+        self.assertIsInstance(bbox, np.ndarray)
+        self.assertEqual(bbox.dtype, np.float32)
+        self.assertEqual(bbox.ndim, 2)
+        self.assertEqual(bbox.shape[1], 4)
 
-            self.assertIsInstance(label, np.ndarray)
-            self.assertEqual(label.dtype, np.int32)
-            self.assertEqual(label.shape, (bbox.shape[0],))
-            self.assertGreaterEqual(np.min(label), 0)
-            self.assertLessEqual(
-                np.max(label), len(voc_detection_label_names) - 1)
+        self.assertIsInstance(label, np.ndarray)
+        self.assertEqual(label.dtype, np.int32)
+        self.assertEqual(label.shape, (bbox.shape[0],))
+        self.assertGreaterEqual(np.min(label), 0)
+        self.assertLessEqual(
+            np.max(label), len(voc_detection_label_names) - 1)
 
-            if self.n_out == 4:
-                difficult = out[3]
-                self.assertIsInstance(difficult, np.ndarray)
-                self.assertEqual(difficult.dtype, np.bool)
-                self.assertEqual(difficult.shape, (bbox.shape[0],))
+        if self.n_out == 4:
+            difficult = out[3]
+            self.assertIsInstance(difficult, np.ndarray)
+            self.assertEqual(difficult.dtype, np.bool)
+            self.assertEqual(difficult.shape, (bbox.shape[0],))
 
 
 testing.run_module(__name__, __file__)
