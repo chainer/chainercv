3b3025726f5523f8a97c9ead3615608345cd618b
Yusuke Niitani <yuyuniitani@gmail.com>
2017-06-14 12:33:06 +0900

    respect alphabetical order

--- chainercv/evaluations/__init__.py
+++ chainercv/evaluations/__init__.py
@@ -1,7 +1,7 @@
+from chainercv.evaluations.calc_pck import calc_pck  # NOQA
 from chainercv.evaluations.eval_detection_voc_ap import calc_detection_voc_ap  # NOQA
 from chainercv.evaluations.eval_detection_voc_ap import calc_detection_voc_prec_rec  # NOQA
 from chainercv.evaluations.eval_detection_voc_ap import eval_detection_voc_ap  # NOQA
-from chainercv.evaluations.calc_pck import calc_pck  # NOQA
 from chainercv.evaluations.eval_semantic_segmentation_iou import calc_semantic_segmentation_confusion  # NOQA
 from chainercv.evaluations.eval_semantic_segmentation_iou import calc_semantic_segmentation_iou  # NOQA
 from chainercv.evaluations.eval_semantic_segmentation_iou import eval_semantic_segmentation_iou  # NOQA
