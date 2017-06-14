b68b2e8e47fb181b6ba19f612221a4e02cba8aa3
Yusuke Niitani <yuyuniitani@gmail.com>
2017-06-14 09:49:56 +0900

    fix __init__.py

--- chainercv/extensions/__init__.py
+++ chainercv/extensions/__init__.py
@@ -1,2 +1,3 @@
 from chainercv.extensions.detection.detection_vis_report import DetectionVisReport  # NOQA
 from chainercv.extensions.detection.detection_voc_evaluator import DetectionVOCEvaluator  # NOQA
+from chainercv.extensions.semantic_segmentation.semantic_segmentation_evaluator import SemanticSegmentationEvaluator  # NOQA
