55ecaca919172d834aecdf7c504ea78c21f2a5a6
Yusuke Niitani <yuyuniitani@gmail.com>
2017-06-14 17:40:59 +0900

    improve eval_imagenet

--- examples/classification/eval_imagenet.py
+++ examples/classification/eval_imagenet.py
@@ -5,12 +5,15 @@ import numpy as np
 
 import chainer
 import chainer.links as L
+import chainer.functions as F
 from chainer import iterators
 from chainer import training
 from chainer.training import extensions
 
 from chainercv.datasets import ImageFolderDataset
 from chainercv.links import VGG16Layers
+
+from chainercv.utils import apply_prediction_to_iterator
 
 
 def main():
@@ -26,15 +29,21 @@ def main():
         dataset, args.batchsize, repeat=False, shuffle=False,
         n_processes=4)
 
-    model = L.Classifier(VGG16Layers())
+    model = VGG16Layers()
 
     if args.gpu >= 0:
         chainer.cuda.get_device(args.gpu).use()
         model.to_gpu()
 
-    
-    result = extensions.Evaluator(iterator, model)
-    print result
+    imgs, pred_values, gt_values = apply_prediction_to_iterator(model.predict, iterator)
+    del imgs
+
+    pred_labels, = pred_values
+    gt_labels, = gt_values
+
+    accuracy = F.accuracy(
+        np.array(list(pred_labels)), np.array(list(gt_labels))).data
+    print accuracy
 
 
 if __name__ == '__main__':
