d36146c6d3d87412e85e99847db56b8c7aff1851
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-06-16 10:42:39 +0900

    canvas -> image

--- chainercv/utils/testing/assertions/assert_is_bbox.py
+++ chainercv/utils/testing/assertions/assert_is_bbox.py
@@ -11,9 +11,9 @@ def assert_is_bbox(bbox, size=None):
 
     Args:
         bbox (~numpy.ndarray): Bounding boxes to be checked.
-        size (tuple of ints): The size of canvas.
+        size (tuple of ints): The size of an image.
             If this argument is specified,
-            Each bounding box should be within the canvas.
+            Each bounding box should be within the image.
     """
 
     assert isinstance(bbox, np.ndarray), \
