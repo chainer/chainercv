49bee22f06bc1f15a4ec2dd866b684338c8955b4
Yusuke Niitani <yuyuniitani@gmail.com>
2017-06-19 09:28:36 +0900

    exploit the fact that functions is ordered dict

--- chainercv/links/model/sequential_feature_extraction_chain.py
+++ chainercv/links/model/sequential_feature_extraction_chain.py
@@ -1,44 +1,39 @@
-import copy
-
 import chainer
 
 
 class SequentialFeatureExtractionChain(chainer.Chain):
 
     def __init__(self, feature_names, link_generators):
+        super(SequentialFeatureExtractionChain, self).__init__()
+
         if (not isinstance(feature_names, str) and
-                all([isinstance(feature, str) for feature in feature_names])):
+                all([isinstance(name, str) for name in feature_names])):
             return_tuple = True
         else:
             return_tuple = False
             feature_names = [feature_names]
         self._return_tuple = return_tuple
-        self._feature_names = feature_names
-
-        super(SequentialFeatureExtractionChain, self).__init__()
-
+        self._feature_names = list(feature_names)
         if any([name not in self.functions for
                 name in self._feature_names]):
             raise ValueError('Elements of `feature_names` shuold be one of '
                              '{}.'.format(self.functions.keys()))
 
-        # Remove all functions that are not necessary.
-        self._unused_function_names = []
-        pop_funcs = False
-        features = list(self._feature_names)
+        # Collect functions that are not going to be used.
+        unused_function_names = []
+        unused = False
         for name in self.functions.keys():
-            if pop_funcs:
-                self._unused_function_names.append(name)
-
-            if name in features:
-                features.remove(name)
-            if len(features) == 0:
-                pop_funcs = True
+            if unused:
+                unused_function_names.append(name)
+            if name in feature_names:
+                feature_names.remove(name)
+            if len(feature_names) == 0:
+                unused = True
 
         with self.init_scope():
             for name, link_gen in link_generators.items():
                 # Ignore layers whose names match functions that are removed.
-                if name not in self._unused_function_names:
+                if name not in unused_function_names:
                     setattr(self, name, link_gen())
 
     @property
@@ -58,17 +53,18 @@ class SequentialFeatureExtractionChain(chainer.Chain):
             is passed to :meth:`__init__`.
 
         """
-        functions = copy.copy(self.functions)
-        for name in self._unused_function_names:
-            functions.pop(name)
+        feature_names = list(self._feature_names)
 
         features = {}
         h = x
-        for name, funcs in functions.items():
+        for name, funcs in self.functions.items():
+            if len(feature_names) == 0:
+                break
             for func in funcs:
                 h = func(h)
-            if name in self._feature_names:
+            if name in feature_names:
                 features[name] = h
+                feature_names.remove(name)
 
         if self._return_tuple:
             features = tuple(
