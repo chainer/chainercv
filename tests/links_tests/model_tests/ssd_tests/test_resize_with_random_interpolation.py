f920625aded9e035d2d87b328027c035748a279c
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-06-23 15:00:42 +0900

    skip if no cv2

--- tests/links_tests/model_tests/ssd_tests/test_resize_with_random_interpolation.py
+++ tests/links_tests/model_tests/ssd_tests/test_resize_with_random_interpolation.py
@@ -14,11 +14,15 @@ except ImportError:
 class TestResizeWithRandomInterpolation(unittest.TestCase):
 
     def test_resize_color(self):
+        if not optional_modules:
+            return
         img = np.random.uniform(size=(3, 24, 32))
         out = resize_with_random_interpolation(img, size=(32, 64))
         self.assertEqual(out.shape, (3, 32, 64))
 
     def test_resize_grayscale(self):
+        if not optional_modules:
+            return
         img = np.random.uniform(size=(1, 24, 32))
         out = resize_with_random_interpolation(img, size=(32, 64))
         self.assertEqual(out.shape, (1, 32, 64))
