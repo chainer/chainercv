6e54438a913cce4a5550072c6c28e7c5b9e2a55f
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-06-30 14:11:52 +0900

    add comment

--- examples/ssd/train.py
+++ examples/ssd/train.py
@@ -55,16 +55,26 @@ class Transform(object):
         self.mean = mean
 
     def __call__(self, in_data):
+        # There are five data augmentation steps
+        # 1. Color augmentation
+        # 2. Random expansion
+        # 3. Random cropping
+        # 4. Resizing with random interpolation
+        # 5. Random horizontal flipping
+
         img, bbox, label = in_data
 
+        # 1. Color augmentation
         img = random_distort(img)
 
+        # 2. Random expansion
         if np.random.randint(2):
             img, param = transforms.random_expand(
                 img, fill=self.mean, return_param=True)
             bbox = transforms.translate_bbox(
                 bbox, y_offset=param['y_offset'], x_offset=param['x_offset'])
 
+        # 3. Random cropping
         img, param = random_crop_with_bbox_constraints(
             img, bbox, return_param=True)
         bbox, param = transforms.crop_bbox(
@@ -72,15 +82,18 @@ class Transform(object):
             allow_outside_center=False, return_param=True)
         label = label[param['index']]
 
+        # 4. Resizing with random interpolatation
         _, H, W = img.shape
         img = resize_with_random_interpolation(img, (self.size, self.size))
         bbox = transforms.resize_bbox(bbox, (H, W), (self.size, self.size))
 
+        # 5. Random horizontal flipping
         img, params = transforms.random_flip(
             img, x_random=True, return_param=True)
         bbox = transforms.flip_bbox(
             bbox, (self.size, self.size), x_flip=params['x_flip'])
 
+        # Preparation for SSD network
         img -= self.mean
         mb_loc, mb_label = self.coder.encode(
             transforms.resize_bbox(bbox, (self.size, self.size), (1, 1)),
