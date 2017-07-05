703ad10a04bd69b05c535cac9a57b72a6a8f9457
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-07-05 13:53:34 +0900

    fix indent style

--- examples/ssd/train.py
+++ examples/ssd/train.py
@@ -156,10 +156,9 @@ def main():
     trainer.extend(extensions.LogReport(trigger=log_interval))
     trainer.extend(extensions.observe_lr(), trigger=log_interval)
     trainer.extend(extensions.PrintReport(
-        [
-            'epoch', 'iteration', 'lr',
-            'main/loss', 'main/loss/loc', 'main/loss/conf',
-            'validation/main/map']),
+        ['epoch', 'iteration', 'lr',
+         'main/loss', 'main/loss/loc', 'main/loss/conf',
+         'validation/main/map']),
         trigger=log_interval)
     trainer.extend(extensions.ProgressBar(update_interval=10))
 
