3b323a11139e03fc3ae23af218eefb86a117b853
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-07-06 16:57:57 +0900

    fix parentheses

--- chainercv/evaluations/eval_detection_voc.py
+++ chainercv/evaluations/eval_detection_voc.py
@@ -233,7 +233,7 @@ def calc_detection_voc_prec_rec(
 
         # If an element of fp + tp is 0,
         # the corresponding element of prec[l] is nan.
-        prec[l] = tp / fp + tp
+        prec[l] = tp / (fp + tp)
         # If n_pos[l] is 0, rec[l] is None.
         if n_pos[l] > 0:
             rec[l] = tp / n_pos[l]
