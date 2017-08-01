1f3a6ad53929195a69654f94ccc8efda22e6780d
Kenta Kubo <kabuto669@icloud.com>
2017-08-01 22:25:43 +0900

    Fix deprecated method usage

--- examples/ssd/caffe2npz.py
+++ examples/ssd/caffe2npz.py
@@ -14,30 +14,30 @@ def rename(name):
         i, j = map(int, m.groups())
         if i >= 6:
             i += 2
-        return 'extractor/conv{:d}_{:d}'.format(i, j)
+        return 'extractor/conv{:d}_{:d}'.format(i, j), True
 
     m = re.match(r'fc([67])$', name)
     if m:
-        return 'extractor/conv{:d}'.format(int(m.group(1)))
+        return 'extractor/conv{:d}'.format(int(m.group(1))), True
 
     if name == r'conv4_3_norm':
-        return 'extractor/norm4'
+        return 'extractor/norm4', True
 
     m = re.match(r'conv4_3_norm_mbox_(loc|conf)$', name)
     if m:
-        return 'multibox/{:s}/0'.format(m.group(1))
+        return 'multibox/{:s}/0'.format(m.group(1)), True
 
     m = re.match(r'fc7_mbox_(loc|conf)$', name)
     if m:
-        return ('multibox/{:s}/1'.format(m.group(1)))
+        return ('multibox/{:s}/1'.format(m.group(1))), True
 
     m = re.match(r'conv(\d+)_2_mbox_(loc|conf)$', name)
     if m:
         i, type_ = int(m.group(1)), m.group(2)
         if i >= 6:
-            return 'multibox/{:s}/{:d}'.format(type_, i - 4)
+            return 'multibox/{:s}/{:d}'.format(type_, i - 4), True
 
-    return name
+    return name, False
 
 
 class SSDCaffeFunction(caffe.CaffeFunction):
@@ -46,8 +46,8 @@ class SSDCaffeFunction(caffe.CaffeFunction):
         print('loading weights from {:s} ... '.format(model_path))
         super(SSDCaffeFunction, self).__init__(model_path)
 
-    def add_link(self, name, link):
-        new_name = rename(name)
+    def __setattr__(self, name, link):
+        new_name, match = rename(name)
 
         if new_name == 'extractor/conv1_1':
             # BGR -> RGB
@@ -59,17 +59,18 @@ class SSDCaffeFunction(caffe.CaffeFunction):
                 data = data.reshape((-1, 4) + data.shape[1:])
                 data[:, [1, 0, 3, 2]] = data.copy()
             print('{:s} -> {:s} (xy -> yx)'.format(name, new_name))
-        else:
+        elif match:
             print('{:s} -> {:s}'.format(name, new_name))
 
-        super(SSDCaffeFunction, self).add_link(new_name, link)
+        super(SSDCaffeFunction, self).__setattr__(new_name, link)
 
     @caffe._layer('Normalize', None)
     def _setup_normarize(self, layer):
         blobs = layer.blobs
         func = Normalize(caffe._get_num(blobs[0]))
         func.scale.data[:] = np.array(blobs[0].data)
-        self.add_link(layer.name, func)
+        with self.init_scope():
+            setattr(self, layer.name, func)
 
     @caffe._layer('AnnotatedData', None)
     @caffe._layer('Flatten', None)
