163ea83e1521951b80aad0fbd23c754e9f15d43d
Shunta Saito <shunta.saito@gmail.com>
2017-08-15 16:23:31 +0900

    Update demo.py

--- examples/pspnet/demo.py
+++ examples/pspnet/demo.py
@@ -5,15 +5,10 @@ import argparse
 import os
 
 import matplotlib.pyplot as plot
-import numpy as np
 
 import chainer
-from chainer import serializers
-# from chainercv.datasets import ade20k_label_colors
-# from chainercv.datasets import ade20k_label_names
-# from chainercv.datasets import cityscapes_labels
-# from chainercv.datasets import cityscapes_label_colors
-# from chainercv.datasets import cityscapes_label_names
+from chainercv.datasets import cityscapes_label_colors
+from chainercv.datasets import cityscapes_label_names
 from chainercv.datasets import voc_semantic_segmentation_label_colors
 from chainercv.datasets import voc_semantic_segmentation_label_names
 from chainercv.links import PSPNet
@@ -26,22 +21,20 @@ if __name__ == '__main__':
     parser.add_argument('--img_fn', '-f', type=str)
     parser.add_argument('--gpu', '-g', type=int, default=-1)
     parser.add_argument('--scales', '-s', type=float, nargs='*', default=None)
-    parser.add_argument(
-        '--model', '-m', type=str, choices=['VOC', 'Cityscapes', 'ADE20K'])
+    parser.add_argument('--model', '-m', type=str,
+                        choices=['voc2012', 'cityscapes'])
     args = parser.parse_args()
 
     chainer.config.train = False
 
-    if args.model == 'VOC':
+    if args.model == 'voc2012':
         model = PSPNet(pretrained_model='voc2012')
         labels = voc_semantic_segmentation_label_names
         colors = voc_semantic_segmentation_label_colors
-    elif args.model == 'Cityscapes':
+    elif args.model == 'cityscapes':
         model = PSPNet(pretrained_model='cityscapes')
         labels = cityscapes_label_names
         colors = cityscapes_label_colors
-    elif args.model == 'ADE20K':
-        model = PSPNet(pretrained_model='ade20k')
 
     if args.gpu >= 0:
         chainer.cuda.get_device_from_id(args.gpu).use()
@@ -57,10 +50,3 @@ if __name__ == '__main__':
               borderaxespad=0.)
     base = os.path.splitext(os.path.basename(args.img_fn))[0]
     plot.savefig('predict_{}.png'.format(base), bbox_inches='tight', dpi=400)
-
-    if args.model == 'Cityscapes':
-        label_out = np.zeros((img.shape[1], img.shape[2], 3), dtype=np.uint8)
-        for label in cityscapes_labels:
-            label_out[np.where(pred == label.trainId)] = label.color
-        io.imsave(
-            'predict_{}_color({}).png'.format(base, args.scales), label_out)
