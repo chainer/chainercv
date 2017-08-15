5173ad13baee35289439325b3e316717672eb68b
Shunta Saito <shunta.saito@gmail.com>
2017-08-15 15:46:09 +0900

    Write a test

--- chainercv/datasets/cityscapes/cityscapes_semantic_segmentation_dataset.py
+++ chainercv/datasets/cityscapes/cityscapes_semantic_segmentation_dataset.py
@@ -2,10 +2,10 @@ import glob
 import os
 
 import numpy as np
-from chainer import dataset
-from chainercv.utils import read_image
 
-from datasets import cityscapes_labels
+from chainer import dataset
+from chainercv.datasets import cityscapes_labels
+from chainercv.utils import read_image
 
 
 class CityscapesSemanticSegmentationDataset(dataset.DatasetMixin):
@@ -22,7 +22,7 @@ class CityscapesSemanticSegmentationDataset(dataset.DatasetMixin):
             ``leftImg8bit``.
         label_dir (string): Path to the dir which contains labels. It should
             end with either ``gtFine`` or ``gtCoarse``.
-        split ({'train', 'val', 'test'}): Select from dataset splits used in
+        split ({'train', 'val'}): Select from dataset splits used in
             Cityscapes dataset.
         ignore_labels (bool): If True, the labels marked ``ignoreInEval``
             defined in the original
@@ -36,23 +36,16 @@ class CityscapesSemanticSegmentationDataset(dataset.DatasetMixin):
         self.ignore_labels = ignore_labels
 
         self.label_fns, self.img_fns = [], []
-        if label_dir is not None:
-            resol = os.path.basename(label_dir)
-            for dname in glob.glob('{}/*'.format(label_dir)):
-                if split in dname:
-                    for label_fn in glob.glob(
-                            '{}/*/*_labelIds.png'.format(dname)):
-                        self.label_fns.append(label_fn)
-            for label_fn in self.label_fns:
-                img_fn = label_fn.replace(resol, 'leftImg8bit')
-                img_fn = img_fn.replace('_labelIds', '')
-                self.img_fns.append(img_fn)
-        else:
-            for dname in glob.glob('{}/*'.format(img_dir)):
-                if split in dname:
-                    for img_fn in glob.glob(
-                            '{}/*_leftImg8bit.png'.format(dname)):
-                        self.img_fns.append(img_fn)
+        resol = os.path.basename(label_dir)
+        for dname in glob.glob('{}/*'.format(label_dir)):
+            if split in dname:
+                for label_fn in glob.glob(
+                        '{}/*/*_labelIds.png'.format(dname)):
+                    self.label_fns.append(label_fn)
+        for label_fn in self.label_fns:
+            img_fn = label_fn.replace(resol, 'leftImg8bit')
+            img_fn = img_fn.replace('_labelIds', '')
+            self.img_fns.append(img_fn)
 
     def __len__(self):
         return len(self.img_fns)
@@ -74,8 +67,6 @@ class CityscapesSemanticSegmentationDataset(dataset.DatasetMixin):
 
         """
         img = read_image(self.img_fns[i])
-        if self.label_fns == []:
-            return img
         label_orig = read_image(
             self.label_fns[i], dtype=np.int32, color=False)[0]
         H, W = label_orig.shape
