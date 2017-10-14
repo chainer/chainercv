4e31ea2a9fbf2d84dce2e25913a2ce9f2c3299fc
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-14 21:16:46 +0900

    add docs

--- chainercv/dataset/annotated_image_dataset_mixin.py
+++ chainercv/dataset/annotated_image_dataset_mixin.py
@@ -3,6 +3,17 @@ import chainer
 
 class AnnotationDataset(chainer.dataset.DatasetMixin):
 
+    """A helper dataset class for :class:`Annotatedimagedatasetmixin`.
+
+    This is a dataset class for :class:`Annotatedimagedatasetmixin`.
+    This dataset returns annotations only.
+    Annotations are retrived from :meth:`self._base.get_annotation`.
+
+    Args:
+        base (object): A base dataset. This dataset should implement
+            :meth:`__len__` and :meth:`get_annotation`.
+    """
+
     def __init__(self, base):
         self._base = base
 
@@ -15,6 +26,18 @@ class AnnotationDataset(chainer.dataset.DatasetMixin):
 
 class AnnotatedImageDatasetMixin(chainer.dataset.DatasetMixin):
 
+    """A dataset mix-in for images with annotations.
+
+    This mix-in provides :meth:`__getitem__` and :obj:`annotations`.
+    User should implement :meth:`__len__`, :meth:`get_image` and
+    :meth:`get_annotation`.
+
+    Parameters:
+        annotations (AnnotationDataset): An dataset that contains annotations.
+            :meth:`annotations.__getitem__` calls :meth:`get_annotation`
+            internally.
+    """
+
     def get_example(self, i):
         img = self.get_image(i)
         anno = self.get_annotation(i)
@@ -25,3 +48,15 @@ class AnnotatedImageDatasetMixin(chainer.dataset.DatasetMixin):
     @property
     def annotations(self):
         return AnnotationDataset(self)
+
+    def __len__(self):
+        """Returns the number of examples."""
+        raise NotImplementedError
+
+    def get_image(self, i):
+        """Returns the image of the i-th example."""
+        raise NotImplementedError
+
+    def get_annotation(self, i):
+        """Returns the annotation(s) of the i-th example."""
+        raise NotImplementedError
