1c5c93c3eebc1775f65861311d7f1842bbb73714
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-10-27 05:17:00 +0900

    add ProgressHook

--- chainercv/utils/iterator/__init__.py
+++ chainercv/utils/iterator/__init__.py
@@ -1,2 +1,3 @@
 from chainercv.utils.iterator.apply_prediction_to_iterator import apply_prediction_to_iterator  # NOQA
+from chainercv.utils.iterator.ProgressHook import ProgressHook  # NOQA
 from chainercv.utils.iterator.unzip import unzip  # NOQA
