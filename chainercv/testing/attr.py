69c336cd8e95730699b5a6e70c682b970e970987
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-12-07 13:42:44 +0900

    fix

--- chainercv/testing/attr.py
+++ chainercv/testing/attr.py
@@ -1,12 +1,11 @@
-from chainer.testing import attr
-
-from attr import cudnn  # NOQA
-from attr import slow  # NOQA
-from attr import multi_gpu  # NOQA
-from attr import gpu  # NOQA
+from chainer.testing.attr import cudnn  # NOQA
+from chainer.testing.attr import slow  # NOQA
+from chainer.testing.attr import multi_gpu  # NOQA
+from chainer.testing.attr import gpu  # NOQA
 
 try:
     import pytest
     disk = pytest.mark.disk
 except ImportError:
-    disk = attr._dummy_callable
+    from chainer.testing.attr import _dummy_callable
+    disk = _dummy_callable
