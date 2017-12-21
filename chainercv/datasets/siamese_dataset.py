93bf78a460d411c0612695f02e6e0b781054fb04
Yusuke Niitani <yuyuniitani@gmail.com>
2017-12-21 15:23:27 +0900

    work when no pos or no neg

--- chainercv/datasets/siamese_dataset.py
+++ chainercv/datasets/siamese_dataset.py
@@ -16,8 +16,13 @@ class SiameseDataset(chainer.dataset.DatasetMixin):
                 labels_0 = [example[1] for example in dataset_0]
             if labels_1 is None:
                 labels_1 = [example[1] for example in dataset_1]
-        self.labels_0 = labels_0
-        self.labels_1 = labels_1
+            labels_0 = np.array(labels_0)
+            labels_1 = np.array(labels_1)
+            if not (labels_0.dtype == np.int32 and labels_0.ndim == 1 and
+                    labels_1.dtype == np.int32 and labels_1.ndim == 1):
+                raise ValueError('the labels are invalid.')
+        self._labels_0 = labels_0
+        self._labels_1 = labels_1
 
     def __len__(self):
         return len(self._dataset_0)
@@ -27,15 +32,20 @@ class SiameseDataset(chainer.dataset.DatasetMixin):
             idx0 = np.random.choice(np.arange(len(self._dataset_0)))
             idx1 = np.random.choice(np.arange(len(self._dataset_1)))
         else:
-            idx0 = np.random.choice(np.arange(len(self._dataset_0)))
-            label_0 = self.labels_0[idx0]
-            eps = np.finfo(float).eps
-            if np.random.binomial(1, self._pos_ratio):
-                # get pos-pair
-                idx1_candidates = np.where(self.labels_1 == label_0)[0]
-            else:
-                # get neg-pair
-                idx1_candidates = np.where(self.labels_1 != label_0)[0]
+            n_search = 3
+            for count in range(n_search):
+                idx0 = np.random.choice(np.arange(len(self._dataset_0)))
+                label_0 = self._labels_0[idx0]
+                if np.random.binomial(1, self._pos_ratio):
+                    # get pos-pair
+                    idx1_candidates = np.where(self._labels_1 == label_0)[0]
+                else:
+                    # get neg-pair
+                    idx1_candidates = np.where(self._labels_1 != label_0)[0]
+                if len(idx1_candidates) > 0:
+                    break
+                elif count == n_search - 1:
+                    idx1_candidates = np.arange(len(self._labels_1))
             idx1 = np.random.choice(idx1_candidates)
 
         example_0 = self._dataset_0[idx0]
