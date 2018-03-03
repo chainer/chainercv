bdc7a4c6df09d2c4a81696b57792676d0fcd8b8f
Unknown <nannyakannya@gmail.com>
2018-03-03 16:25:31 +0900

    Use siamese dataset and rename to mixup from between class

---
+++ chainercv/datasets/mixup_dataset.py
@@ -0,0 +1,60 @@
+import numpy
+
+from chainer.dataset import dataset_mixin
+
+
+class MixUpSoftLabelDataset(dataset_mixin.DatasetMixin):
+
+    """Dataset which returns mixed images and labels for bc learning[1].
+
+    `MixUpSoftLabelDataset` chooses two images randomly and mix these images
+    and labels respectively by weighted average.
+
+    Unlike `LabeledImageDatasets`, label is a one-dimensional float array with
+    two nonzero weights (soft label). The summed weights is one.
+
+    The base dataset `__getitem__` should return image and label. Please see
+    the following example.
+
+    Example:
+
+        We construct a mixup dataset from MNIST.
+
+        .. code::
+
+            >>> from chainer.datasets import get_mnist
+            >>> from chainercv.datasets import SiameseDataset
+            >>> from chainercv.datasets import MixUpSoftLabelDataset
+            >>> mnist, _ = get_mnist()
+            >>> base_dataset = SiameseDataset(mnist, mnist)
+            >>> dataset = MixUpSoftLabelDataset(base_dataset)
+            >>> mixed_image, mixed_label = dataset[0]
+
+    Args:
+        dataset: The underlying dataset. dataset should returns two image
+            and their label. Typically, dataset is `SiameseDataset`.
+            More over, each element of each dataset should have same shape.
+            dataset also needs to support `__len__` as described above.
+        max_label: Max label index of base datasets.
+
+    See the papers for details: [1]`mixup: Beyond Empirical Risk Minimization
+    <https://arxiv.org/abs/1710.09412>`_.
+
+    """
+
+    def __init__(self, dataset, max_label):
+        self._dataset = dataset
+        self._max_label = max_label
+
+    def __len__(self):
+        return len(self._dataset)
+
+    def get_example(self, i):
+        image1, label1, image2, label2 = self._dataset[i]
+        mix_ratio = numpy.random.random()
+
+        image = mix_ratio * image1 + (1-mix_ratio) * image2
+        label = numpy.zeros(self._max_label + 1, dtype=numpy.float32)
+        label[label1] += mix_ratio
+        label[label2] += 1 - mix_ratio
+        return image, label
---
+++ tests/datasets_tests/test_mixup_dataset.py
@@ -0,0 +1,63 @@
+import unittest
+
+import numpy as np
+
+from chainer import testing
+
+from chainer.datasets import TupleDataset
+from chainercv.datasets import SiameseDataset
+from chainercv.datasets import MixUpSoftLabelDataset
+from chainercv.utils.testing.assertions.assert_is_image import assert_is_image
+
+
+N = 15
+
+
+@testing.parameterize(
+    # Positive and negative samples
+    {'labels_0': np.arange(N, dtype=np.int32) % 3,
+     'labels_1': np.arange(N, dtype=np.int32) % 3,
+     'pos_exist': True, 'neg_exist': True,
+     },
+    # No positive
+    {'labels_0': np.zeros(N, dtype=np.int32),
+     'labels_1': np.ones(N, dtype=np.int32),
+     'pos_exist': False, 'neg_exist': True
+     },
+    # No negative
+    {'labels_0': np.ones(N, dtype=np.int32),
+     'labels_1': np.ones(N, dtype=np.int32),
+     'pos_exist': True, 'neg_exist': False},
+)
+class TestMixupDataset(unittest.TestCase):
+
+    img_shape = (3, 32, 48)
+
+    def setUp(self):
+        np.random.shuffle(self.labels_0)
+        np.random.shuffle(self.labels_1)
+
+        dataset_0 = TupleDataset(
+            np.random.uniform(size=(N,) + self.img_shape), self.labels_0)
+        dataset_1 = TupleDataset(
+            np.random.uniform(size=(N,) + self.img_shape), self.labels_1)
+        self.n_class = np.max((self.labels_0, self.labels_1)) + 1
+        self.siamese_dataset = SiameseDataset(dataset_0, dataset_1)
+
+    def _check_example(self, example):
+        assert_is_image(example[0])
+        self.assertEqual(example[0].shape, self.img_shape)
+        self.assertEqual(example[1].dtype, np.float32)
+        self.assertEqual(example[1].ndim, 1)
+        self.assertEqual(example[1].shape[0], self.n_class + 1)
+        self.assertAlmostEqual(example[1].sum(), 1.0)
+
+    def test_mixup(self):
+        dataset = MixUpSoftLabelDataset(self.siamese_dataset, self.n_class)
+        for i in range(10):
+            example = dataset[i]
+            self._check_example(example)
+        self.assertEqual(len(dataset), N)
+
+
+testing.run_module(__name__, __file__)
