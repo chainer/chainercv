66b377647c9ebe72a0ffae9c7f2fa9f305f4d95f
fukatani <nannyakannya@gmail.com>
2018-03-25 00:07:18 +0900

    Addressed review comments

--- chainercv/datasets/mixup_soft_label_dataset.py
+++ chainercv/datasets/mixup_soft_label_dataset.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 from chainer.dataset import dataset_mixin
 
@@ -7,14 +7,12 @@ class MixUpSoftLabelDataset(dataset_mixin.DatasetMixin):
 
     """Dataset which returns mixed images and labels for mixup learning[1].
 
-    `MixUpSoftLabelDataset` mix two images and labels which is chosen by base
-    dataset respectively by weighted average.
+    :class:`MixUpSoftLabelDataset` mixes two pairs of labeled images fetched
+    from the base dataset.
 
     Unlike `LabeledImageDatasets`, label is a one-dimensional float array with
-    at most two nonzero weights (i.e. soft label). The summed weights is one.
-
-    The base dataset `__getitem__` should return image and label. Please see
-    the following example.
+    at most two nonzero weights (i.e. soft label). The sum of the two weights
+    is one.
 
     Example:
 
@@ -27,34 +25,37 @@ class MixUpSoftLabelDataset(dataset_mixin.DatasetMixin):
             >>> from chainercv.datasets import MixUpSoftLabelDataset
             >>> mnist, _ = get_mnist()
             >>> base_dataset = SiameseDataset(mnist, mnist)
-            >>> dataset = MixUpSoftLabelDataset(base_dataset)
+            >>> dataset = MixUpSoftLabelDataset(base_dataset, 10)
             >>> mixed_image, mixed_label = dataset[0]
+            >>> print(mixed_label.shape, mixed_label.dtype)
 
     Args:
-        dataset: The underlying dataset. dataset should returns two image
-            and their label. Typically, dataset is `SiameseDataset`.
-            More over, each element of each dataset should have same shape.
-            dataset also needs to support `__len__` as described above.
+        dataset: The underlying dataset. The dataset returns :obj:`img_0,
+        label_0, img_1, label_1`, which is a tuple containing two pairs of an
+        image and a label. Typically, dataset is `SiameseDataset`.
+
+            Moreover, each element of each dataset should have same shape.
         max_label: Max label index of base datasets.
 
-    See the papers for details: [1]`mixup: Beyond Empirical Risk Minimization
-    <https://arxiv.org/abs/1710.09412>`_.
+    .. [#] Hongyi Zhang, Moustapha Cisse, Yann N. Dauphin, David Lopez-Paz.
+        `mixup: Beyond Empirical Risk Minimization\
+        <https://arxiv.org/abs/1710.09412>`_. arXiv 2017.
 
     """
 
-    def __init__(self, dataset, max_label):
+    def __init__(self, dataset, n_class):
         self._dataset = dataset
-        self._max_label = max_label
+        self._n_class = n_class
 
     def __len__(self):
         return len(self._dataset)
 
     def get_example(self, i):
         image1, label1, image2, label2 = self._dataset[i]
-        mix_ratio = numpy.random.random()
+        mix_ratio = np.random.random()
 
         image = mix_ratio * image1 + (1-mix_ratio) * image2
-        label = numpy.zeros(self._max_label + 1, dtype=numpy.float32)
+        label = np.zeros(self._n_class + 1, dtype=np.float32)
         label[label1] += mix_ratio
         label[label2] += 1 - mix_ratio
         return image, label
