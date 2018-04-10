4fda6d1c940396029a054aa5157524912fc6afa2
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-04-10 20:12:11 +0900

    update train example

--- examples/ssd/train_multi.py
+++ examples/ssd/train_multi.py
@@ -1,6 +1,4 @@
 import argparse
-import copy
-import numpy as np
 
 import chainer
 from chainer.datasets import ConcatenatedDataset
@@ -11,6 +9,8 @@ from chainer import training
 from chainer.training import extensions
 from chainer.training import triggers
 
+import chainermn
+
 from chainercv.datasets import voc_bbox_label_names
 from chainercv.datasets import VOCBboxDataset
 from chainercv.extensions import DetectionVOCEvaluator
@@ -18,132 +18,31 @@ from chainercv.links.model.ssd import GradientScaling
 from chainercv.links.model.ssd import multibox_loss
 from chainercv.links import SSD300
 from chainercv.links import SSD512
-from chainercv import transforms
 
-from chainercv.links.model.ssd import random_crop_with_bbox_constraints
-from chainercv.links.model.ssd import random_distort
-from chainercv.links.model.ssd import resize_with_random_interpolation
+from train import Transform
 
 
 class MultiboxTrainChain(chainer.Chain):
 
-    def __init__(self, model, alpha=1, k=3):
+    def __init__(self, model, alpha=1, k=3, comm=None):
         super(MultiboxTrainChain, self).__init__()
         with self.init_scope():
             self.model = model
         self.alpha = alpha
         self.k = k
+        self.comm = comm
 
-    def __call__(self, imgs, gt_mb_locs, gt_mb_labels, n_positive):
+    def __call__(self, imgs, gt_mb_locs, gt_mb_labels):
         mb_locs, mb_confs = self.model(imgs)
         loc_loss, conf_loss = multibox_loss(
-            mb_locs, mb_confs, gt_mb_locs, gt_mb_labels, self.k)
+            mb_locs, mb_confs, gt_mb_locs, gt_mb_labels, self.k, self.comm)
         loss = loc_loss * self.alpha + conf_loss
 
         chainer.reporter.report(
             {'loss': loss, 'loss/loc': loc_loss, 'loss/conf': conf_loss},
             self)
 
-        # use the number of positives in all GPUs instead of that in this GPU
-        with chainer.cuda.get_device_from_array(gt_mb_labels):
-            if n_positive[0] > 0:
-                loss *= (gt_mb_labels > 0).sum() / n_positive[0]
         return loss
-
-
-class Transform(object):
-
-    def __init__(self, coder, size, mean):
-        # to send cpu, make a copy
-        self.coder = copy.copy(coder)
-        self.coder.to_cpu()
-
-        self.size = size
-        self.mean = mean
-
-    def __call__(self, in_data):
-        # There are five data augmentation steps
-        # 1. Color augmentation
-        # 2. Random expansion
-        # 3. Random cropping
-        # 4. Resizing with random interpolation
-        # 5. Random horizontal flipping
-
-        img, bbox, label = in_data
-
-        # 1. Color augmentation
-        img = random_distort(img)
-
-        # 2. Random expansion
-        if np.random.randint(2):
-            img, param = transforms.random_expand(
-                img, fill=self.mean, return_param=True)
-            bbox = transforms.translate_bbox(
-                bbox, y_offset=param['y_offset'], x_offset=param['x_offset'])
-
-        # 3. Random cropping
-        img, param = random_crop_with_bbox_constraints(
-            img, bbox, return_param=True)
-        bbox, param = transforms.crop_bbox(
-            bbox, y_slice=param['y_slice'], x_slice=param['x_slice'],
-            allow_outside_center=False, return_param=True)
-        label = label[param['index']]
-
-        # 4. Resizing with random interpolatation
-        _, H, W = img.shape
-        img = resize_with_random_interpolation(img, (self.size, self.size))
-        bbox = transforms.resize_bbox(bbox, (H, W), (self.size, self.size))
-
-        # 5. Random horizontal flipping
-        img, params = transforms.random_flip(
-            img, x_random=True, return_param=True)
-        bbox = transforms.flip_bbox(
-            bbox, (self.size, self.size), x_flip=params['x_flip'])
-
-        # Preparation for SSD network
-        img -= self.mean
-        mb_loc, mb_label = self.coder.encode(bbox, label)
-
-        return img, mb_loc, mb_label
-
-
-class TransformIterator(chainer.dataset.Iterator):
-
-    def __init__(self, iterator):
-        self._iterator = iterator
-
-    # (img, mb_loc, mb_label) -> (img, mb_loc, mb_label, n_positive),
-    # where n_positive is the total number of positives in the batch
-    def __next__(self):
-        batch = next(self._iterator)
-        n_positive = sum((mb_label > 0).sum() for _, _, mb_label in batch)
-        return [(img, mb_loc, mb_label, n_positive)
-                for img, mb_loc, mb_label in batch]
-
-    @property
-    def epoch(self):
-        return self._iterator.epoch
-
-    @property
-    def epoch_detail(self):
-        return self._iterator.epoch_detail
-
-    @property
-    def previous_epoch_detail(self):
-        return self._iterator.previous_epoch_detail
-
-    @property
-    def is_new_epoch(self):
-        return self._iterator.is_new_epoch
-
-    def finalize(self):
-        self._iterator.finalize()
-
-    def serialize(self, serializer):
-        self._iterator.serialize(serializer)
-
-    def reset(self):
-        self._iterator.reset()
 
 
 def main():
@@ -152,10 +51,12 @@ def main():
         '--model', choices=('ssd300', 'ssd512'), default='ssd300')
     parser.add_argument('--batchsize', type=int, default=32)
     parser.add_argument('--test_batchsize', type=int, default=32)
-    parser.add_argument('--gpu', type=int, nargs='+')
     parser.add_argument('--out', default='result')
     parser.add_argument('--resume')
     args = parser.parse_args()
+
+    comm = chainermn.create_communicator()
+    device = comm.intra_rank
 
     if args.model == 'ssd300':
         model = SSD300(
@@ -168,25 +69,30 @@ def main():
 
     model.use_preset('evaluate')
     train_chain = MultiboxTrainChain(model)
-    chainer.cuda.get_device_from_id(args.gpu[0]).use()
+    chainer.cuda.get_device_from_id(device).use()
+    model.to_gpu()
 
-    train = TransformDataset(
-        ConcatenatedDataset(
-            VOCBboxDataset(year='2007', split='trainval'),
-            VOCBboxDataset(year='2012', split='trainval')
-        ),
-        Transform(model.coder, model.insize, model.mean))
-    train_iter = TransformIterator(
-        chainer.iterators.MultiprocessIterator(train, args.batchsize))
+    if comm.rank == 0:
+        train = TransformDataset(
+            ConcatenatedDataset(
+                VOCBboxDataset(year='2007', split='trainval'),
+                VOCBboxDataset(year='2012', split='trainval')
+            ),
+            Transform(model.coder, model.insize, model.mean))
+        test = VOCBboxDataset(
+            year='2007', split='test',
+            use_difficult=True, return_difficult=True)
+        test_iter = chainer.iterators.SerialIterator(
+            test, args.test_batchsize, repeat=False, shuffle=False)
+    else:
+        train = None
 
-    test = VOCBboxDataset(
-        year='2007', split='test',
-        use_difficult=True, return_difficult=True)
-    test_iter = chainer.iterators.SerialIterator(
-        test, args.test_batchsize, repeat=False, shuffle=False)
+    train = chainermn.scatter_dataset(train, comm, shuffle=True)
+    train_iter = chainer.iterators.MultiprocessIterator(train, args.batchsize)
 
     # initial lr is set to 1e-3 by ExponentialShift
-    optimizer = chainer.optimizers.MomentumSGD()
+    optimizer = chainermn.create_multi_node_optimizer(
+        chainer.optimizers.MomentumSGD(), comm)
     optimizer.setup(train_chain)
     for param in train_chain.params():
         if param.name == 'b':
@@ -194,35 +100,34 @@ def main():
         else:
             param.update_rule.add_hook(WeightDecay(0.0005))
 
-    devices = {'main': args.gpu[0]}
-    devices.update(
-        ('other{:d}'.format(i + 1), gpu) for i, gpu in enumerate(args.gpu[1:]))
-    updater = training.ParallelUpdater(train_iter, optimizer, devices=devices)
+    updater = training.StandardUpdater(train_iter, optimizer, devices=device)
     trainer = training.Trainer(updater, (120000, 'iteration'), args.out)
     trainer.extend(
         extensions.ExponentialShift('lr', 0.1, init=1e-3),
         trigger=triggers.ManualScheduleTrigger([80000, 100000], 'iteration'))
 
-    trainer.extend(
-        DetectionVOCEvaluator(
-            test_iter, model, use_07_metric=True,
-            label_names=voc_bbox_label_names),
-        trigger=(10000, 'iteration'))
+    if comm.rank == 0:
+        trainer.extend(
+            DetectionVOCEvaluator(
+                test_iter, model, use_07_metric=True,
+                label_names=voc_bbox_label_names),
+            trigger=(10000, 'iteration'))
 
-    log_interval = 10, 'iteration'
-    trainer.extend(extensions.LogReport(trigger=log_interval))
-    trainer.extend(extensions.observe_lr(), trigger=log_interval)
-    trainer.extend(extensions.PrintReport(
-        ['epoch', 'iteration', 'lr',
-         'main/loss', 'main/loss/loc', 'main/loss/conf',
-         'validation/main/map']),
-        trigger=log_interval)
-    trainer.extend(extensions.ProgressBar(update_interval=10))
+        log_interval = 10, 'iteration'
+        trainer.extend(extensions.LogReport(trigger=log_interval))
+        trainer.extend(extensions.observe_lr(), trigger=log_interval)
+        trainer.extend(extensions.PrintReport(
+            ['epoch', 'iteration', 'lr',
+             'main/loss', 'main/loss/loc', 'main/loss/conf',
+             'validation/main/map']),
+            trigger=log_interval)
+        trainer.extend(extensions.ProgressBar(update_interval=10))
 
-    trainer.extend(extensions.snapshot(), trigger=(10000, 'iteration'))
-    trainer.extend(
-        extensions.snapshot_object(model, 'model_iter_{.updater.iteration}'),
-        trigger=(120000, 'iteration'))
+        trainer.extend(extensions.snapshot(), trigger=(10000, 'iteration'))
+        trainer.extend(
+            extensions.snapshot_object(
+                model, 'model_iter_{.updater.iteration}'),
+            trigger=(120000, 'iteration'))
 
     if args.resume:
         serializers.load_npz(args.resume, trainer)
