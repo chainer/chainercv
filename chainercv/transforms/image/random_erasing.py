15a161067cfa64f7d8e454ff23d9dacdaed8e8a8
akitotakeki <takeki@hal.t.u-tokyo.ac.jp>
2018-04-11 22:25:17 +0900

    fix doc and add scale option

--- chainercv/transforms/image/random_erasing.py
+++ chainercv/transforms/image/random_erasing.py
@@ -9,10 +9,11 @@ from chainercv.transforms.image.random_sized_crop import random_sized_crop
 def random_erasing(img, prob=0.5,
                    scale_ratio_range=(0.02, 0.4),
                    aspect_ratio_range=(0.3, 1 / 0.3),
-                   fixed_value=np.array((0.4914, 0.4822, 0.4465)),
                    random_value=True,
+                   scale=1.0,
+                   fixed_value=np.array((0.4914, 0.4822, 0.4465)),
                    return_param=False, copy=False):
-    """Select a rectangle region in an image and erase its pixels with mean values.
+    """Erase a rectangle region in an image with random or fixed values.
 
     The size :math:`(H_{erase}, W_{erase})` and the left top coordinate
     :math:`(y_{start}, x_{start})` of the region are calculated as follows:
@@ -50,10 +51,11 @@ def random_erasing(img, prob=0.5,
             the distribution from which a scale ratio is sampled.
         aspect_ratio_range (tuple of two floats): Determines
             the distribution from which an aspect ratio is sampled.
+        random_value (bool): Fill the rectangle region with random values.
+        scale (float): Pixel value scale.
         fixed_value (~numpy.ndarray): Determines pixel values
             to fill the rectangle region.
             The default value is the ImageNet mean value.
-        random_value (bool): Fill the rectangle region with random values.
         return_param (bool): Returns parameters if :obj:`True`.
 
     Returns:
@@ -79,7 +81,7 @@ def random_erasing(img, prob=0.5,
         crop, params = random_sized_crop(img, scale_ratio_range,
                                          aspect_ratio_range, return_param=True)
         if random_value:
-            crop[:] = np.random.random(crop.shape)
+            crop[:] = np.random.random(crop.shape) * scale
         else:
             crop[:] = fixed_value[:, None, None]
 
