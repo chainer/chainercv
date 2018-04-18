53796f93402c46e53e105b89c4489d220d5b4c98
Yusuke Niitani <yuyuniitani@gmail.com>
2018-04-18 10:52:10 +0900

    change the default return values and update doc

--- chainercv/datasets/coco/coco_bbox_dataset.py
+++ chainercv/datasets/coco/coco_bbox_dataset.py
@@ -3,8 +3,6 @@ import json
 import numpy as np
 import os
 
-import chainer
-
 from chainercv import utils
 
 from chainercv.datasets.coco.coco_utils import get_coco
@@ -18,55 +16,11 @@ class COCOBboxDataset(GetterDataset):
 
     .. _`MS COCO2014`: http://mscoco.org/dataset/#detections-challenge2015
 
-    When queried by an index, if :obj:`return_crowded == False`,
-    this dataset returns a corresponding
-    :obj:`img, bbox, label, crowded, area`, a tuple of an image, bounding
-    boxes, labels, crowdness indicators and areas of masks.
-    The parameters :obj:`return_crowded` and :obj:`return_area` decide
-    whether to return :obj:`crowded` and :obj:`area`.
-    :obj:`crowded` is a boolean array
-    that indicates whether bounding boxes are for crowd labeling.
-    When there are more than ten objects from the same category,
-    bounding boxes correspond to crowd of instances instead of individual
-    instances. Please see more detail in the Fig. 12 (e) of the summary
-    paper [#]_.
-
     There are total of 82,783 training and 40,504 validation images.
     'minval' split is a subset of validation images that constitutes
     5000 images in the validation images. The remaining validation
     images are called 'minvalminus'. Concrete list of image ids and
     annotations for these splits are found `here`_.
-
-    .. _`here`: https://github.com/rbgirshick/py-faster-rcnn/tree/master/data
-
-    The bounding boxes are packed into a two dimensional tensor of shape
-    :math:`(R, 4)`, where :math:`R` is the number of bounding boxes in
-    the image. The second axis represents attributes of the bounding box.
-    They are :obj:`(x_min, y_min, x_max, y_max)`, where the
-    four attributes are coordinates of the top left and the bottom right
-    vertices.
-
-    The labels are packed into a one dimensional tensor of shape :math:`(R,)`.
-    The class name of the label :math:`l` is :math:`l` th element of
-    :obj:`chainercv.datasets.coco_bbox_label_names`.
-
-    The array :obj:`crowded` is a one dimensional boolean array of shape
-    :math:`(R,)`.
-
-    The array :obj:`area` is a one dimensional flaot array of shape
-    :math:`(R,)`.
-
-    * :obj:`img.dtype == numpy.float32`
-    * :obj:`bbox.dtype == numpy.float32`
-    * :obj:`label.dtype == numpy.int32`
-    * :obj:`crowded.dtype == numpy.bool`
-    * :obj:`area.dtype == np.float32`
-
-    .. [#] Tsung-Yi Lin, Michael Maire, Serge Belongie, Lubomir Bourdev, \
-        Ross Girshick, James Hays, Pietro Perona, Deva Ramanan, \
-        C. Lawrence Zitnick, Piotr Dollar.
-        `Microsoft COCO: Common Objects in Context \
-        <https://arxiv.org/abs/1405.0312>`_. arXiv 2014.
 
     Args:
         data_dir (string): Path to the root of the training data. If this is
@@ -76,11 +30,45 @@ class COCOBboxDataset(GetterDataset):
             a split of the dataset.
         use_crowded (bool): If true, use bounding boxes that are labeled as
             crowded in the original annotation.
+        return_area (bool): If true, this dataset returns areas of masks
+            around objects.
         return_crowded (bool): If true, this dataset returns a boolean array
             that indicates whether bounding boxes are labeled as crowded
             or not. The default value is :obj:`False`.
-        return_area (bool): If true, this dataset returns areas of masks
-            around objects.
+
+    .. _`here`: https://github.com/rbgirshick/py-faster-rcnn/tree/master/data
+
+    This dataset returns the following data.
+
+        .. csv-table::
+            :header: name, shape, dtype, format
+
+            :obj:`img`, ":math:`(3, H, W)`", :obj:`float32`, \
+            "RGB, :math:`[0, 255]`"
+            :obj:`bbox` [#coco_bbox_1]_, ":math:`(R, 4)`", :obj:`float32`, \
+            ":math:`(y_{min}, x_{min}, y_{max}, x_{max})`"
+            :obj:`label` [#coco_bbox_1]_, ":math:`(R,)`", :obj:`int32`, \
+            ":math:`[0, n\_fg\_class - 1]`"
+            :obj:`area` [#coco_bbox_1]_ [#coco_bbox_2]_, ":math:`(R,)`", :obj:`float32`, --
+            :obj:`crowded` [#coco_bbox_3]_, ":math:`(R,)`", :obj:`bool`, --
+
+        .. [#coco_bbox_1] If :obj:`use_crowded = True`, \
+            :obj:`bbox`, :obj:`label` and :obj:`area` contain difficult instances.
+        .. [#coco_bbox_2] :obj:`area` is available \
+            if :obj:`return_area = True`.
+        .. [#coco_bbox_3] :obj:`crowded` is available \
+            if :obj:`return_crowded = True`.
+
+    When there are more than ten objects from the same category,
+    bounding boxes correspond to crowd of instances instead of individual
+    instances. Please see more detail in the Fig. 12 (e) of the summary
+    paper [#]_.
+
+    .. [#] Tsung-Yi Lin, Michael Maire, Serge Belongie, Lubomir Bourdev, \
+        Ross Girshick, James Hays, Pietro Perona, Deva Ramanan, \
+        C. Lawrence Zitnick, Piotr Dollar.
+        `Microsoft COCO: Common Objects in Context \
+        <https://arxiv.org/abs/1405.0312>`_. arXiv 2014.
 
     """
 
@@ -104,7 +92,7 @@ class COCOBboxDataset(GetterDataset):
         self.data_dir = data_dir
         anno = json.load(open(anno_fn, 'r'))
 
-        self.img_props = dict()
+        self.img_props = {}
         for img in anno['images']:
             self.img_props[img['id']] = img
         self.ids = sorted(list(self.img_props.keys()))
@@ -112,31 +100,35 @@ class COCOBboxDataset(GetterDataset):
         cats = anno['categories']
         self.cat_ids = [cat['id'] for cat in cats]
 
-        self.anns = dict()
+        self.anns = {}
         self.imgToAnns = defaultdict(list)
         for ann in anno['annotations']:
             self.imgToAnns[ann['image_id']].append(ann)
             self.anns[ann['id']] = ann
 
-        self.add_getter('image', self._get_image)
+        self.add_getter('img', self._get_image)
         self.add_getter(['bbox', 'label', 'crowded', 'area'],
                         self._get_annotations)
+
+        keys = ['img', 'bbox', 'label']
+        if self.return_area:
+            keys.append('area')
+        if self.return_crowded:
+            keys.append('crowded')
 
     def __len__(self):
         return len(self.ids)
 
     def _get_image(self, i):
-        img_id = self.ids[i]
-        img_fn = os.path.join(
-            self.img_root, self.img_props[img_id]['file_name'])
-        img = utils.read_image(img_fn, dtype=np.float32, color=True)
+        img_path = os.path.join(
+            self.img_root, self.img_props[self.ids[i]]['file_name'])
+        img = utils.read_image(img_path, dtype=np.float32, color=True)
         return img
 
     def _get_annotations(self, i):
-        img_id = self.ids[i]
         # List[{'segmentation', 'area', 'iscrowd',
         #       'image_id', 'bbox', 'category_id', 'id'}]
-        annotation = self.imgToAnns[img_id]
+        annotation = self.imgToAnns[self.ids[i]]
         bbox = np.array([ann['bbox'] for ann in annotation],
                         dtype=np.float32)
         if len(bbox) == 0:
@@ -150,42 +142,23 @@ class COCOBboxDataset(GetterDataset):
         label = np.array([self.cat_ids.index(ann['category_id'])
                           for ann in annotation], dtype=np.int32)
 
-        crowded = np.array([ann['iscrowd']
-                            for ann in annotation], dtype=np.bool)
-
         area = np.array([ann['area']
                          for ann in annotation], dtype=np.float32)
+
+        crowded = np.array([ann['iscrowd']
+                            for ann in annotation], dtype=np.bool)
 
         # Remove invalid boxes
         bbox_area = np.prod(bbox[:, 2:] - bbox[:, :2], axis=1)
         keep_mask = np.logical_and(bbox[:, 0] <= bbox[:, 2],
                                    bbox[:, 1] <= bbox[:, 3])
         keep_mask = np.logical_and(keep_mask, bbox_area > 0)
-        bbox = bbox[keep_mask]
-        label = label[keep_mask]
-        crowded = crowded[keep_mask]
-        area = area[keep_mask]
-        return bbox, label, crowded, area
-
-
-    def get_example(self, i):
-        img_id = self.ids[i]
-        img_fn = os.path.join(
-            self.img_root, self.img_props[img_id]['file_name'])
-        img = utils.read_image(img_fn, dtype=np.float32, color=True)
-        _, H, W = img.shape
-
-        bbox, label, crowded, area = self._get_annotations(i)
 
         if not self.use_crowded:
-            bbox = bbox[np.logical_not(crowded)]
-            label = label[np.logical_not(crowded)]
-            area = area[np.logical_not(crowded)]
-            crowded = crowded[np.logical_not(crowded)]
+            keep_mask = np.logical_and(keep_mask, np.logical_not(crowded))
 
-        example = [img, bbox, label]
-        if self.return_crowded:
-            example += [crowded]
-        if self.return_area:
-            example += [area]
-        return tuple(example)
+        bbox = bbox[keep_mask]
+        label = label[keep_mask]
+        area = area[keep_mask]
+        crowded = crowded[keep_mask]
+        return bbox, label, area, crowded
