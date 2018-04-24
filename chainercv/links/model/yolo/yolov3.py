827e12f3b9506e0f78cc79445441cd9d1f25c055
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-04-25 08:28:18 +0900

    separate extractor and subnet

--- chainercv/links/model/yolo/yolov3.py
+++ chainercv/links/model/yolo/yolov3.py
@@ -26,11 +26,11 @@ class ResidualBlock(chainer.ChainList):
         return h
 
 
-class YOLOv3(chainer.ChainList):
+class Darknet53Extractor(chainer.ChainList):
 
     insize = 416
 
-    def __init__(self, n_fg_class):
+    def __init__(self):
         super().__init__()
 
         # Darknet53
@@ -47,26 +47,49 @@ class YOLOv3(chainer.ChainList):
         for i, n in enumerate((512, 256, 128)):
             if i > 0:
                 self.append(Conv2DBNActiv(n, 1, activ=_leaky_relu))
-            for _ in range(3):
-                self.append(Conv2DBNActiv(n, 1, activ=_leaky_relu))
-                self.append(Conv2DBNActiv(n * 2, 3, pad=1, activ=_leaky_relu))
-            self.append(Convolution2D((1 + 4 + n_fg_class) * 3, 1))
+            self.append(Conv2DBNActiv(n, 1, activ=_leaky_relu))
+            self.append(Conv2DBNActiv(n * 2, 3, pad=1, activ=_leaky_relu))
+            self.append(Conv2DBNActiv(n, 1, activ=_leaky_relu))
+            self.append(Conv2DBNActiv(n * 2, 3, pad=1, activ=_leaky_relu))
+            self.append(Conv2DBNActiv(n, 1, activ=_leaky_relu))
 
     def __call__(self, x):
-        ys = []
-
         h = x
         hs = []
         for i, link in enumerate(self):
             h = link(h)
-            if i in {35, 43, 51}:
-                ys.append(h)
-
-            if i in {14, 23, 33, 41}:
+            if i in {33, 39, 45}:
+                yield h
+            elif i in {14, 23}:
                 hs.append(h)
-            elif i in {35, 43}:
-                h = hs.pop()
-            elif i in {36, 44}:
+            elif i in {34, 40}:
                 h = F.concat((_upsample(h), hs.pop()))
 
-        return ys
+
+class YOLOv3(chainer.Chain):
+
+    def __init__(self, n_fg_class):
+        super().__init__()
+        self.n_fg_class = n_fg_class
+
+        with self.init_scope():
+            self.extractor = Darknet53Extractor()
+            self.subnet = chainer.ChainList()
+
+        for n in (512, 256, 128):
+            self.subnet.append(chainer.Sequential(
+                Conv2DBNActiv(n * 2, 3, pad=1, activ=_leaky_relu),
+                Convolution2D(3 * (1 + 4 + self.n_fg_class), 1)))
+
+    @property
+    def insize(self):
+        return self.extractor.insize
+
+    def __call__(self, x):
+        ys = []
+        for h, link in zip(self.extractor(x), self.subnet):
+            h = link(h)
+            h = F.reshape(h, (h.shape[0], 3, 1 + 4 + self.n_fg_class, -1))
+            h = F.transpose(h, (0, 3, 1, 2))
+            ys.append(h)
+        return F.concat(ys)
