bd7eff2faad921b09b54c4bc218ea381d6d655ef
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-05-03 02:28:35 +0900

    fix path

--- chainercv/links/model/yolo/yolo_v2.py
+++ chainercv/links/model/yolo/yolo_v2.py
@@ -118,7 +118,7 @@ class YOLOv2(chainer.Chain):
         'voc0712': {
             'n_fg_class': 20,
             'url': 'https://github.com/yuyu2172/share-weights/releases/'
-            'download/0.0.6/yolo_v3_voc0712_2018_05_01.npz'
+            'download/0.0.6/yolo_v2_voc0712_2018_05_03.npz'
         },
     }
 
