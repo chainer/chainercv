daabbe7d777db18ecdee922c93218bfbd078b305
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-05-02 14:33:18 +0900

    fix resnet

--- chainercv/links/model/resnet/resnet.py
+++ chainercv/links/model/resnet/resnet.py
@@ -128,8 +128,8 @@ class ResNet(PickableSequentialChain):
             },
             152: {
                 'imagenet': {
-                    'param': {'n_class': 1000, 'mean': None},
-                    'default': {'mean': _imagenet_mean},
+                    'param': {'n_class': 1000, 'mean': _imagenet_mean},
+                    'overwritable': {'mean'},
                     'url': 'https://github.com/yuyu2172/share-weights/'
                     'releases/download/0.0.6/'
                     'resnet152_imagenet_convert_2018_03_07.npz'
