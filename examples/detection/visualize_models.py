89fc495f28979aad89ddb1ccf72d7370ba4e0112
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-05-02 15:10:53 +0900

    use slice

--- examples/detection/visualize_models.py
+++ examples/detection/visualize_models.py
@@ -12,24 +12,23 @@ from chainercv.visualizations import vis_bbox
 
 
 def main():
-    dataset = VOCBboxDataset(year='2007', split='test')
+    dataset = VOCBboxDataset(year='2007', split='test') \
+        .slice[[29, 301, 189, 229], 'img']
     models = [
         ('Faster R-CNN', FasterRCNNVGG16(pretrained_model='voc07')),
         ('SSD300', SSD300(pretrained_model='voc0712')),
         ('SSD512', SSD512(pretrained_model='voc0712')),
         ('YOLOv3', YOLOv3(pretrained_model='voc0712')),
     ]
-    indices = [29, 301, 189, 229]
 
     fig = plt.figure(figsize=(30, 30))
-    for i, idx in enumerate(indices):
+    for i, img in enumerate(dataset):
         for j, (name, model) in enumerate(models):
-            img, _, _ = dataset[idx]
             bboxes, labels, scores = model.predict([img])
             bbox, label, score = bboxes[0], labels[0], scores[0]
 
             ax = fig.add_subplot(
-                len(indices), len(models), i * len(models) + j + 1)
+                len(dataset), len(models), i * len(models) + j + 1)
             vis_bbox(
                 img, bbox, label, score,
                 label_names=voc_bbox_label_names, ax=ax
