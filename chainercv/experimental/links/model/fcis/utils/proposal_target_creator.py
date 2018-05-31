7bc13739e7cd4e6962873269ef8cd16358e61e58
Shingo Kitagawa <shingogo@hotmail.co.jp>
2018-06-01 02:13:00 +0900

    refactor ProposalTargetCreator

--- chainercv/experimental/links/model/fcis/utils/proposal_target_creator.py
+++ chainercv/experimental/links/model/fcis/utils/proposal_target_creator.py
@@ -7,29 +7,6 @@ from chainercv.transforms.image.resize import resize
 from chainercv.utils.bbox.bbox_iou import bbox_iou
 
 
-def _generate_gt_roi_msk(gt_msk, gt_bb, bb, mask_size):
-    y_min = max(bb[0], gt_bb[0])
-    x_min = max(bb[1], gt_bb[1])
-    y_max = min(bb[2], gt_bb[2])
-    x_max = min(bb[3], gt_bb[3])
-
-    if y_min > y_max or x_min > x_max:
-        return np.zeros((mask_size, mask_size))
-
-    h = y_max - y_min
-    w = x_max - x_min
-    y_start = y_min - bb[0]
-    x_start = x_min - bb[1]
-    y_end = y_start + h
-    x_end = x_start + w
-
-    gt_roi_msk = np.zeros(
-        (bb[2] - bb[0], bb[3] - bb[1]), dtype=np.float32)
-    gt_clipped_msk = gt_msk[y_min:y_max, x_min:x_max]
-    gt_roi_msk[y_start:y_end, x_start:x_end] = gt_clipped_msk
-    return gt_roi_msk
-
-
 class ProposalTargetCreator(object):
     def __init__(
             self, n_sample=128,
@@ -114,12 +91,11 @@ class ProposalTargetCreator(object):
 
         for i, pos_ind in enumerate(pos_index):
             bb = np.round(sample_roi[i]).astype(np.int)
-            gt_bb = np.round(bbox[gt_assignment[pos_ind]]).astype(np.int)
             gt_msk = mask[gt_assignment[pos_ind]]
-            gt_roi_msk = _generate_gt_roi_msk(
-                gt_msk, gt_bb, bb, self.mask_size)
+            gt_roi_msk = gt_msk[bb[0]:bb[2], bb[1]:bb[3]]
             gt_roi_msk = resize(
-                gt_roi_msk[None], (self.mask_size, self.mask_size))[0]
+                gt_roi_msk.astype(np.float32)[None],
+                (self.mask_size, self.mask_size))[0]
             gt_roi_msk = (gt_roi_msk >= self.binary_thresh).astype(np.int)
             gt_roi_mask[i] = gt_roi_msk
 
