ba29779196d3163d2ff745ca20660a9b4715cce4
Shingo Kitagawa <shingogo@hotmail.co.jp>
2018-09-16 16:17:29 +0900

    rotate_bbox accepts angle in [-180, 180]

--- chainercv/transforms/bbox/rotate_bbox.py
+++ chainercv/transforms/bbox/rotate_bbox.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 
-def rotate_bbox(bbox, k, size):
-    """Rotate bounding boxes by 90, 180, 270 or 360 degrees.
+def rotate_bbox(bbox, angle, size):
+    """Rotate bounding boxes by degrees.
 
     Args:
         bbox (~numpy.ndarray): An array whose shape is :math:`(R, 4)`.
             :math:`R` is the number of bounding boxes.
-        k (int): The integer that represents the number of times the
+        angle (float): Clock-wise rotation angle (degree) in [-180, 180].
             image is rotated by 90 degrees.
         size (tuple): A tuple of length 2. The height and the width
             of the image.
@@ -17,18 +17,24 @@ def rotate_bbox(bbox, k, size):
         Bounding boxes rescaled according to the given :obj:`k`.
 
     """
-    if k % 4 == 0:
+    if angle % 90 != 0:
+        raise ValueError(
+            'angle in [180, 90, 0, -90, -180] is only supported: {}'
+            .format(angle))
         return bbox
     H, W = size
-    if k % 4 == 1:
+    if angle == 0:
+        return bbox
+
+    if angle == 90:
         rotated_bbox = np.concatenate(
             (W - bbox[:, 3:4], bbox[:, 0:1],
              W - bbox[:, 1:2], bbox[:, 2:3]), axis=1)
-    elif k % 4 == 2:
+    elif angle in [180, -180]:
         rotated_bbox = np.concatenate(
             (H - bbox[:, 2:3], W - bbox[:, 3:4],
              H - bbox[:, 0:1], W - bbox[:, 1:2]), axis=1)
-    elif k % 4 == 3:
+    elif angle == -90:
         rotated_bbox = np.concatenate(
             (bbox[:, 1:2], H - bbox[:, 2:3],
              bbox[:, 3:4], H - bbox[:, 0:1]), axis=1)
--- tests/transforms_tests/bbox_tests/test_rotate_bbox.py
+++ tests/transforms_tests/bbox_tests/test_rotate_bbox.py
@@ -8,7 +8,7 @@ from chainercv.utils import generate_random_bbox
 
 
 @testing.parameterize(*testing.product({
-    'k': [0, 1, 2, 3]
+    'angle': [180, 90, 0, -90, -180]
 }))
 class TestRotateBbox(unittest.TestCase):
 
@@ -16,12 +16,12 @@ class TestRotateBbox(unittest.TestCase):
         size = (32, 24)
         bbox = generate_random_bbox(10, size, 0, 24)
 
-        out = rotate_bbox(bbox, self.k, size)
-        if (4 - self.k) % 2 == 1:
+        out = rotate_bbox(bbox, self.angle, size)
+        if self.angle % 180 != 0:
             rotate_size = size[::-1]
         else:
             rotate_size = size
-        out = rotate_bbox(out, 4 - self.k, rotate_size)
+        out = rotate_bbox(out, -1 * self.angle, rotate_size)
 
         np.testing.assert_almost_equal(out, bbox, decimal=6)
 
