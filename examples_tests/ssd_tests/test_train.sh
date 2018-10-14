2eb6f61b450a8b4ad8baebe6beac8f44eff82244
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-10-14 20:59:53 +0900

    set batchsize

--- examples_tests/ssd_tests/test_train.sh
+++ examples_tests/ssd_tests/test_train.sh
@@ -3,5 +3,5 @@ set -e
 
 cd examples/ssd
 
-$PYTHON train.py --model ssd300 --gpu 0 --short
-$PYTHON train.py --model ssd512 --gpu 0 --short
+$PYTHON train.py --model ssd300 --batchsize 2 --gpu 0 --short
+$PYTHON train.py --model ssd512 --batchsize 2 --gpu 0 --short
