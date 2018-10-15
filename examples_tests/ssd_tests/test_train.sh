0cffd595d182cddc18a2058cd13af562000af9ab
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-10-15 10:43:47 +0900

    fix test script

--- examples_tests/ssd_tests/test_train.sh
+++ examples_tests/ssd_tests/test_train.sh
@@ -1,7 +1,5 @@
-#! /usr/bin/env sh
-set -e
-
 cd examples/ssd
+sed -e 's/return_difficult=True\)/return_difficult=True).slice[:20]/' -i train.py
 
-$PYTHON train.py --model ssd300 --batchsize 2 --gpu 0 --short
-$PYTHON train.py --model ssd512 --batchsize 2 --gpu 0 --short
+$PYTHON train.py --model ssd300 --batchsize 2 --iteration 12 --step 8 10 --gpu 0
+$PYTHON train.py --model ssd512 --batchsize 2 --iteration 12 --step 8 10 --gpu 0
