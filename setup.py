04a160b359e15718a76eaec2b7089dd47d97d21a
Yusuke Niitani <yuyuniitani@gmail.com>
2018-10-26 08:33:55 +0900

    update chainercv to v0.11

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ cmdclass = {
 
 setup(
     name='chainercv',
-    version='0.10.0',
+    version='0.11.0',
     packages=find_packages(),
     author='Yusuke Niitani, Toru Ogawa',
     author_email='niitani@preferred.jp, ogawa@preferred.jp',
