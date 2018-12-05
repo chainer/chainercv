8ffb55af91c495fe5dd94dc6240af43b37288bf8
Yusuke Niitani <yuyuniitani@gmail.com>
2018-12-05 18:56:39 +0900

    simplify eval_camvid

--- examples/semantic_segmentation/eval_camvid.py
+++ examples/semantic_segmentation/eval_camvid.py
@@ -1,11 +1,8 @@
 from __future__ import division
 
 import argparse
-from collections import defaultdict
-import numpy as np
 
 import chainer
-from chainer.dataset import concat_examples
 
 from chainercv.datasets import camvid_label_names
 from chainercv.datasets import CamVidDataset
@@ -13,31 +10,6 @@ from chainercv.evaluations import eval_semantic_segmentation
 from chainercv.links import SegNetBasic
 from chainercv.utils import apply_to_iterator
 from chainercv.utils import ProgressHook
-
-
-def calc_bn_statistics(model, batchsize):
-    train = CamVidDataset(split='train')
-    it = chainer.iterators.SerialIterator(
-        train, batchsize, repeat=False, shuffle=False)
-    bn_avg_mean = defaultdict(np.float32)
-    bn_avg_var = defaultdict(np.float32)
-
-    n_iter = 0
-    for batch in it:
-        imgs, _ = concat_examples(batch)
-        model(model.xp.array(imgs))
-        for name, link in model.namedlinks():
-            if name.endswith('_bn'):
-                bn_avg_mean[name] += link.avg_mean
-                bn_avg_var[name] += link.avg_var
-        n_iter += 1
-
-    for name, link in model.namedlinks():
-        if name.endswith('_bn'):
-            link.avg_mean = bn_avg_mean[name] / n_iter
-            link.avg_var = bn_avg_var[name] / n_iter
-
-    return model
 
 
 def main():
@@ -53,8 +25,6 @@ def main():
     if args.gpu >= 0:
         chainer.cuda.get_device_from_id(args.gpu).use()
         model.to_gpu()
-
-    model = calc_bn_statistics(model, args.batchsize)
 
     test = CamVidDataset(split='test')
     it = chainer.iterators.SerialIterator(test, batch_size=args.batchsize,
