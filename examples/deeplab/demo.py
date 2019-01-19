4b4be61778aec77c44c70b97191850bd46f6ee51
Junki Ishikawa <69guitar1015@gmail.com>
2019-01-19 21:19:29 +0900

    Add example

---
+++ examples/deeplab/demo.py
@@ -0,0 +1,49 @@
+import argparse
+
+import matplotlib.pyplot as plt
+
+import chainer
+
+from chainercv.datasets import voc_semantic_segmentation_label_colors
+from chainercv.datasets import voc_semantic_segmentation_label_names
+from chainercv.links import DeepLabV3plusXception65
+from chainercv import utils
+from chainercv.visualizations import vis_image
+from chainercv.visualizations import vis_semantic_segmentation
+
+
+def main():
+    chainer.config.train = False
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', type=int, default=-1)
+    parser.add_argument('--pretrained-weight')
+    parser.add_argument('image')
+    args = parser.parse_args()
+
+    model = DeepLabV3plusXception65(
+        n_class=len(voc_semantic_segmentation_label_names))
+
+    chainer.serializers.load_npz(args.pretrained_weight, model)
+
+    if args.gpu >= 0:
+        chainer.cuda.get_device_from_id(args.gpu).use()
+        model.to_gpu()
+
+    img = utils.read_image(args.image, color=True)
+    labels = model.predict([img])
+    label = labels[0]
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 2, 1)
+    vis_image(img, ax=ax1)
+    ax2 = fig.add_subplot(1, 2, 2)
+    # Do not overlay the label image on the color image
+    vis_semantic_segmentation(
+        None, label, voc_semantic_segmentation_label_names,
+        voc_semantic_segmentation_label_colors, ax=ax2)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
