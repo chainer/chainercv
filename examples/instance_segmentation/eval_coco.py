1f0a9ceaca4ee0c05e5bbf95bd6455855334ddef
Shingo Kitagawa <shingogo@hotmail.co.jp>
2019-01-30 19:24:53 +0900

    update eval_coco.py

--- examples/instance_segmentation/eval_coco.py
+++ examples/instance_segmentation/eval_coco.py
@@ -16,21 +16,19 @@ def main():
     parser.add_argument(
         '--model', choices=('fcis_resnet101',),
         default='fcis_resnet101')
-    parser.add_argument('--pretrained-model')
+    parser.add_argument('--pretrained-model', default=None)
     parser.add_argument('--gpu', type=int, default=-1)
     args = parser.parse_args()
 
     if args.model == 'fcis_resnet101':
-        if args.pretrained_model:
-            pretrained_model = args.pretrained_model
-        else:
-            pretrained_model = 'coco'
+        if args.pretrained_model is None:
+            args.pretrained_model = 'coco'
         proposal_creator_params = FCISResNet101.proposal_creator_params
         proposal_creator_params['min_size'] = 2
         model = FCISResNet101(
             n_fg_class=len(coco_instance_segmentation_label_names),
             anchor_scales=(4, 8, 16, 32),
-            pretrained_model=pretrained_model,
+            pretrained_model=args.pretrained_model,
             proposal_creator_params=proposal_creator_params)
 
     model.use_preset('coco_evaluate')
