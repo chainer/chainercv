a198dc2bfa4dc6721b2e740a4b425e43e7a0865e
Shingo Kitagawa <shingogo@hotmail.co.jp>
2019-02-18 18:40:54 +0900

    refactor cuda kernel in ps_roi_average_pooling_2d

--- chainercv/functions/ps_roi_average_pooling_2d.py
+++ chainercv/functions/ps_roi_average_pooling_2d.py
@@ -157,14 +157,14 @@ class PSROIAveragePooling2D(function.Function):
             (n_roi, self.out_c, self.out_h, self.out_w), dtype=np.float32)
         cuda.elementwise(
             '''
-            raw float32 bottom_data, raw float32 bottom_rois,
+            raw T bottom_data, raw T bottom_rois,
             raw int32 bottom_roi_indices,
-            float32 spatial_scale, int32 channels,
+            T spatial_scale, int32 channels,
             int32 height, int32 width,
             int32 pooled_dim, int32 pooled_height, int32 pooled_width,
             int32 group_size
             ''',
-            'float32 top_data',
+            'T top_data',
             '''
             // pos in output filter
             int ph = (i / pooled_width) % pooled_height;
@@ -173,31 +173,27 @@ class PSROIAveragePooling2D(function.Function):
             int n = i / pooled_width / pooled_height / pooled_dim;
 
             int roi_batch_ind = bottom_roi_indices[n];
-            float roi_start_h = static_cast<float>(
-                round(bottom_rois[n * 4 + 0])) * spatial_scale;
-            float roi_start_w = static_cast<float>(
-                round(bottom_rois[n * 4 + 1])) * spatial_scale;
-            float roi_end_h = static_cast<float>(
-                round(bottom_rois[n * 4 + 2])) * spatial_scale;
-            float roi_end_w = static_cast<float>(
-                round(bottom_rois[n * 4 + 3])) * spatial_scale;
+            T roi_start_h = bottom_rois[n * 4 + 0] * spatial_scale;
+            T roi_start_w = bottom_rois[n * 4 + 1] * spatial_scale;
+            T roi_end_h = bottom_rois[n * 4 + 2] * spatial_scale;
+            T roi_end_w = bottom_rois[n * 4 + 3] * spatial_scale;
 
             // Force too small ROIs to be 1x1
-            float roi_height = max(roi_end_h - roi_start_h, 0.1);
-            float roi_width = max(roi_end_w - roi_start_w, 0.1);  // avoid 0
+            T roi_height = max(roi_end_h - roi_start_h, 0.1);
+            T roi_width = max(roi_end_w - roi_start_w, 0.1);  // avoid 0
 
             // Compute w and h at bottom
-            float bin_size_h = roi_height / static_cast<float>(pooled_height);
-            float bin_size_w = roi_width / static_cast<float>(pooled_width);
+            T bin_size_h = roi_height / static_cast<T>(pooled_height);
+            T bin_size_w = roi_width / static_cast<T>(pooled_width);
 
-            int hstart = static_cast<int>(floor(static_cast<float>(ph)
-                                                * bin_size_h + roi_start_h));
-            int wstart = static_cast<int>(floor(static_cast<float>(pw)
-                                                * bin_size_w + roi_start_w));
-            int hend = static_cast<int>(ceil(static_cast<float>(ph + 1)
-                                             * bin_size_h + roi_start_h));
-            int wend = static_cast<int>(ceil(static_cast<float>(pw + 1)
-                                            * bin_size_w + roi_start_w));
+            int hstart = floor(
+                static_cast<T>(ph) * bin_size_h + roi_start_h);
+            int wstart = floor(
+                static_cast<T>(pw) * bin_size_w + roi_start_w);
+            int hend = ceil(
+                static_cast<T>(ph + 1) * bin_size_h + roi_start_h);
+            int wend = ceil(
+                static_cast<T>(pw + 1) * bin_size_w + roi_start_w);
 
             // Add roi offsets and clip to input boundaries
             hstart = min(max(hstart, 0), height);
@@ -208,15 +204,15 @@ class PSROIAveragePooling2D(function.Function):
 
             // Compute c at bottom
             int gh = floor(
-                static_cast<float>(ph) * group_size / pooled_height);
+                static_cast<T>(ph) * group_size / pooled_height);
             int gw = floor(
-                static_cast<float>(pw) * group_size / pooled_width);
+                static_cast<T>(pw) * group_size / pooled_width);
             gh = min(max(gh, 0), group_size - 1);
             gw = min(max(gw, 0), group_size - 1);
             int c = (ctop * group_size + gh) * group_size + gw;
 
             int data_offset = (roi_batch_ind * channels + c) * height * width;
-            float out_sum = 0;
+            T out_sum = 0;
             for (int h = hstart; h < hend; ++h){
               for (int w = wstart; w < wend; ++w){
                  int bottom_index = h * width + w;
@@ -224,8 +220,8 @@ class PSROIAveragePooling2D(function.Function):
               }
             }
 
-            float bin_area = (hend - hstart) * (wend - wstart);
-            top_data = is_empty? (float) 0. : out_sum / bin_area;
+            T bin_area = (hend - hstart) * (wend - wstart);
+            top_data = is_empty? (T) 0. : out_sum / bin_area;
             ''', 'ps_roi_average_pooling_2d_fwd'
         )(bottom_data, bottom_rois, bottom_roi_indices,
           self.spatial_scale, channels, height, width,
@@ -295,13 +291,13 @@ class PSROIAveragePooling2D(function.Function):
         bottom_diff = cuda.cupy.zeros(self._bottom_data_shape, np.float32)
         cuda.elementwise(
             '''
-            raw float32 top_diff, raw float32 bottom_rois,
+            raw T top_diff, raw T bottom_rois,
             raw int32 bottom_roi_indices,
-            float32 spatial_scale, int32 channels, int32 height, int32 width,
+            T spatial_scale, int32 channels, int32 height, int32 width,
             int32 pooled_dim, int32 pooled_height, int32 pooled_width,
             int32 group_size
             ''',
-            'raw float32 bottom_diff',
+            'raw T bottom_diff',
             '''
             int ph = (i / pooled_width) % pooled_height;
             int pw = i % pooled_width;
@@ -310,31 +306,27 @@ class PSROIAveragePooling2D(function.Function):
 
             // [start, end) interval for spatial sampling
             int roi_batch_ind = bottom_roi_indices[n];
-            float roi_start_h = static_cast<float>(
-                round(bottom_rois[n * 4 + 0])) * spatial_scale;
-            float roi_start_w = static_cast<float>(
-                round(bottom_rois[n * 4 + 1])) * spatial_scale;
-            float roi_end_h = static_cast<float>(
-                round(bottom_rois[n * 4 + 2])) * spatial_scale;
-            float roi_end_w = static_cast<float>(
-                round(bottom_rois[n * 4 + 3])) * spatial_scale;
+            T roi_start_h = bottom_rois[n * 4 + 0] * spatial_scale;
+            T roi_start_w = bottom_rois[n * 4 + 1] * spatial_scale;
+            T roi_end_h = bottom_rois[n * 4 + 2] * spatial_scale;
+            T roi_end_w = bottom_rois[n * 4 + 3] * spatial_scale;
 
             // Force too small ROIs to be 1x1
-            float roi_height = max(roi_end_h - roi_start_h, 0.1);
-            float roi_width = max(roi_end_w - roi_start_w, 0.1); // avoid 0
+            T roi_height = max(roi_end_h - roi_start_h, 0.1);
+            T roi_width = max(roi_end_w - roi_start_w, 0.1); // avoid 0
 
             // Compute w and h at bottom
-            float bin_size_h = roi_height / static_cast<float>(pooled_height);
-            float bin_size_w = roi_width / static_cast<float>(pooled_width);
+            T bin_size_h = roi_height / static_cast<T>(pooled_height);
+            T bin_size_w = roi_width / static_cast<T>(pooled_width);
 
             int hstart = floor(
-                static_cast<float>(ph) * bin_size_h + roi_start_h);
+                static_cast<T>(ph) * bin_size_h + roi_start_h);
             int wstart = floor(
-                static_cast<float>(pw) * bin_size_w + roi_start_w);
+                static_cast<T>(pw) * bin_size_w + roi_start_w);
             int hend = ceil(
-                static_cast<float>(ph + 1.0) * bin_size_h + roi_start_h);
+                static_cast<T>(ph + 1.0) * bin_size_h + roi_start_h);
             int wend = ceil(
-                static_cast<float>(pw + 1.0) * bin_size_w + roi_start_w);
+                static_cast<T>(pw + 1.0) * bin_size_w + roi_start_w);
 
             // Add roi offsets and clip to input boundaries
             hstart = min(max(hstart, 0), height);
@@ -345,9 +337,9 @@ class PSROIAveragePooling2D(function.Function):
 
             // Compute c at bottom
             int gh = floor(
-                static_cast<float>(ph) * group_size / pooled_height);
+                static_cast<T>(ph) * group_size / pooled_height);
             int gw = floor(
-                static_cast<float>(pw) * group_size / pooled_width);
+                static_cast<T>(pw) * group_size / pooled_width);
             gh = min(max(gh, 0), group_size - 1);
             gw = min(max(gw, 0), group_size - 1);
             int c = (ctop * group_size + gh) * group_size + gw;
@@ -357,8 +349,8 @@ class PSROIAveragePooling2D(function.Function):
             int top_offset =
                 (n * pooled_dim + ctop) * pooled_height * pooled_width;
 
-            float bin_area = (hend - hstart) * (wend - wstart);
-            float diff_val = is_empty ? (float) 0. :
+            T bin_area = (hend - hstart) * (wend - wstart);
+            T diff_val = is_empty ? (T) 0. :
                 top_diff[top_offset + ph * pooled_width + pw] / bin_area;
             for (int h = hstart; h < hend; ++h){
               for (int w = wstart; w < wend; ++w){
