6341172ef40f3ab833bd05c928bf139956904711
Shingo Kitagawa <shingogo@hotmail.co.jp>
2019-02-20 02:48:44 +0900

    update train code for fcis params

--- examples/fcis/train_sbd.py
+++ examples/fcis/train_sbd.py
@@ -87,9 +87,21 @@ def main():
     test_dataset = SBDInstanceSegmentationDataset(split='val')
 
     # model
+    proposal_creator_params = {
+        'nms_thresh': 0.7,
+        'n_train_pre_nms': 6000,
+        'n_train_post_nms': 300,
+        'n_test_pre_nms': 6000,
+        'n_test_post_nms': 300,
+        'force_cpu_nms': False,
+        'min_size': 16,
+    }
     fcis = FCISResNet101(
         n_fg_class=len(sbd_instance_segmentation_label_names),
-        pretrained_model='imagenet', iter2=False)
+        pretrained_model='imagenet',
+        anchor_scales=(8, 16, 32),
+        iter2=False,
+        proposal_creator_params=proposal_creator_params)
     fcis.use_preset('evaluate')
     model = FCISTrainChain(fcis)
 
