6513e2480e98ce3773ec5566c9923be51f75bb23
Yusuke Niitani <niitani@preferred.jp>
2019-03-06 13:20:10 +0900

    delete +1

--- chainercv/links/model/mask_rcnn/misc.py
+++ chainercv/links/model/mask_rcnn/misc.py
@@ -24,7 +24,7 @@ def mask_to_segm(mask, bbox, segm_size, index=None, pad=1):
     bbox = chainer.backends.cuda.to_cpu(bbox)
     padded_segm_size = segm_size + pad * 2
     cv2_expand_scale = padded_segm_size / segm_size
-    bbox = _expand_boxes(bbox, cv2_expand_scale).astype(np.int32)
+    bbox = _integerize_bbox(_expand_boxes(bbox, cv2_expand_scale))
 
     segm = []
     if index is None:
@@ -35,11 +35,11 @@ def mask_to_segm(mask, bbox, segm_size, index=None, pad=1):
     for i, bb in zip(index, bbox):
         y_min = max(bb[0], 0)
         x_min = max(bb[1], 0)
-        y_max = min(bb[2] + 1, H)
-        x_max = min(bb[3] + 1, W)
+        y_max = min(bb[2], H)
+        x_max = min(bb[3], W)
         cropped_m = mask[i, y_min:y_max, x_min:x_max]
         cropped_m = chainer.backends.cuda.to_cpu(cropped_m)
-        if cropped_m.shape[0] <= 1 or cropped_m.shape[1] <= 1:
+        if cropped_m.shape[0] == 0 or cropped_m.shape[1] == 0:
             segm.append(np.zeros((segm_size, segm_size), dtype=np.float32))
             continue
 
@@ -70,25 +70,28 @@ def segm_to_mask(segm, bbox, size, pad=1):
     padded_mask = np.zeros(
         (segm_size + pad * 2, segm_size + pad * 2), dtype=np.float32)
 
-    bbox = _expand_boxes(bbox, cv2_expand_scale)
+    bbox = _integerize_bbox(_expand_boxes(bbox, cv2_expand_scale))
     for i, (bb, sgm) in enumerate(zip(bbox, segm)):
-        bb = bb.astype(np.int32)
         padded_mask[1:-1, 1:-1] = sgm
 
-        bb_height = np.maximum(bb[2] - bb[0] + 1, 1)
-        bb_width = np.maximum(bb[3] - bb[1] + 1, 1)
+        bb_height = np.maximum(bb[2] - bb[0], 1)
+        bb_width = np.maximum(bb[3] - bb[1], 1)
 
         crop_mask = cv2.resize(padded_mask, (bb_width, bb_height))
         crop_mask = crop_mask > 0.5
 
         y_min = max(bb[0], 0)
         x_min = max(bb[1], 0)
-        y_max = min(bb[2] + 1, H)
-        x_max = min(bb[3] + 1, W)
+        y_max = min(bb[2], H)
+        x_max = min(bb[3], W)
         mask[i, y_min:y_max, x_min:x_max] = crop_mask[
             (y_min - bb[0]):(y_max - bb[0]),
             (x_min - bb[1]):(x_max - bb[1])]
     return mask
+
+
+def _integerize_bbox(bbox):
+    return np.round(bbox).astype(np.int32)
 
 
 def _expand_boxes(bbox, scale):
