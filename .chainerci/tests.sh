4b012895646eebcf519f8f2bd4321690126daf44
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-03-06 21:05:44 +0900

    set MPLBACKEND

--- .chainerci/tests.sh
+++ .chainerci/tests.sh
@@ -2,5 +2,6 @@
 set -eu
 
 docker run --runtime=nvidia --rm --volume $(realpath .):/mnt \
+       --env MPLBACKEND=agg \
        hakuyume/chainercv:chainer${CHAINER}-devel \
        sh -ec "pip${PYTHON} install --user -e /mnt; mpiexec -n 2 --allow-run-as-root python${PYTHON} -m pytest -m 'not slow' /mnt/tests"
