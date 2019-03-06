5fd6892b316694143b343ad56b0efd718498c519
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-03-06 21:36:35 +0900

    fix

--- .chainerci/tests.sh
+++ .chainerci/tests.sh
@@ -5,7 +5,7 @@ docker run --runtime=nvidia -i --rm \
        --volume $(realpath .):/mnt --workdir /mnt \
        --env MPLBACKEND=agg \
        hakuyume/chainercv:chainer${CHAINER}-devel \
-       sh -ec << EOD
+       sh -e << EOD
 pip${PYTHON} install --user -e .
 mpiexec -n 2 --allow-run-as-root \
         python${PYTHON} -m pytest --color=no -m 'not slow' tests
