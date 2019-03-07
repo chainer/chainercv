55444ffff15379e44f750323d57b3016900ec8bd
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-03-07 19:34:12 +0900

    split cpu/gpu tests

--- .chainerci/tests.sh
+++ .chainerci/tests.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/env sh
 set -eu
 
+if [ "$(nvidia-smi --query-gpu=count --format=csv,noheader)" -gt 0 ]; then
+    MARKS='gpu and not slow'
+else
+    MARKS='not gpu and not slow'
+fi
+
 docker run --runtime=nvidia --interactive --rm \
        --volume $(realpath .):/mnt --workdir /mnt \
        --env MPLBACKEND=agg \
@@ -8,5 +14,5 @@ docker run --runtime=nvidia --interactive --rm \
        sh -e << EOD
 pip${PYTHON} install --user -e .
 mpiexec -n 2 --allow-run-as-root \
-        python${PYTHON} -m pytest --color=no -m 'not slow' tests
+        python${PYTHON} -m pytest --color=no -m '${MARKS}' tests
 EOD
