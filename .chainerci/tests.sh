30d1c1cf73980fc202a0466c80e7f6bd97fedd1a
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-03-12 08:25:58 +0900

    sperate cpu/gpu tests

--- .chainerci/tests.sh
+++ .chainerci/tests.sh
@@ -3,39 +3,44 @@ set -eux
 
 TEMP=$(mktemp -d)
 mount -t tmpfs tmpfs ${TEMP}/ -o size=100%
+apt-get install -y --no-install-recommends unzip
+gsutil -q cp gs://chainercv-pfn-public-ci/datasets-tiny.zip ${TEMP}/
+unzip -q ${TEMP}/datasets-tiny.zip -d ${TEMP}/
+rm ${TEMP}/datasets-tiny.zip
 
-if nvidia-smi --query-gpu=count --format=csv; then
-    mkdir -p ${TEMP}/.chainer
+# rename tests for pytest-xdist
+for TEST in $(find tests/ -name '*.py')
+do
+    cat - << EOD >> ${TEST}
+def rename_tests(module_name):
+    import inspect
+    import re
+    import sys
+    import unittest
 
-    docker run --runtime=nvidia --interactive --rm \
-       --volume $(pwd):/chainercv/ --workdir /chainercv/ \
-       --volume ${TEMP}/.chainer/:/root/.chainer/ \
-       --env MPLBACKEND=agg \
-       hakuyume/chainercv:chainer${CHAINER}-devel \
-       sh -ex << EOD
-pip${PYTHON} install --user -e .
-python${PYTHON} -m pytest --color=no -m 'gpu and not mpi' tests/
-mpiexec -n 2 --allow-run-as-root \
-        python${PYTHON} -m pytest --color=no -m 'gpu and mpi' tests/
+    module = sys.modules[module_name]
+    for name, cls in inspect.getmembers(module, inspect.isclass):
+        if not issubclass(cls, unittest.TestCase):
+            continue
+        new_name = re.sub(r'(?s)_{.+}', '', name)
+        new_cls = type(new_name, (cls,), {})
+        setattr(module, new_name, new_cls)
+        delattr(module, name)
+
+
+rename_tests(__name__)
 EOD
-else
-    apt-get install -y --no-install-recommends unzip
-    gsutil -q cp gs://chainercv-pfn-public-ci/datasets-tiny.zip ${TEMP}/
-    unzip -q ${TEMP}/datasets-tiny.zip -d ${TEMP}/
-    rm ${TEMP}/datasets-tiny.zip
+done
 
-    docker run --interactive --rm \
+docker run --interactive --rm \
        --volume $(pwd):/chainercv/ --workdir /chainercv/ \
        --volume ${TEMP}/.chainer/:/root/.chainer/ \
        --env MPLBACKEND=agg \
        hakuyume/chainercv:chainer${CHAINER}-devel \
        sh -ex << EOD
+pip${PYTHON} install --user pytest-xdist
 pip${PYTHON} install --user -e .
-curl -LO https://raw.githubusercontent.com/imos/public/master/parallel-pytest.py
-python${PYTHON} parallel-pytest.py \
-                --pytest='python${PYTHON} -m pytest --color=no -m \'not gpu and not mpi\'' \
-                --threads=$(nproc) \
-                --directory=tests/
+python${PYTHON} -m pytest --color=no -n $(nproc) -m 'not gpu and not mpi' tests/
 mpiexec -n 2 --allow-run-as-root \
         python${PYTHON} -m pytest --color=no -m 'not gpu and mpi' tests/
 EOD
---
+++ .chainerci/tests_gpu.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env sh
+set -eux
+
+TEMP=$(mktemp -d)
+mount -t tmpfs tmpfs ${TEMP}/ -o size=100%
+mkdir -p ${TEMP}/.chainer
+
+docker run --runtime=nvidia --interactive --rm \
+       --volume $(pwd):/chainercv/ --workdir /chainercv/ \
+       --volume ${TEMP}/.chainer/:/root/.chainer/ \
+       --env MPLBACKEND=agg \
+       hakuyume/chainercv:chainer${CHAINER}-devel \
+       sh -ex << EOD
+pip${PYTHON} install --user -e .
+python${PYTHON} -m pytest --color=no -m 'gpu and not mpi' tests/
+mpiexec -n 2 --allow-run-as-root \
+        python${PYTHON} -m pytest --color=no -m 'gpu and mpi' tests/
+EOD
