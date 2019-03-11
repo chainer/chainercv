89d5fe5bb08e287767d5976bf1db877d9d48d7b6
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-03-11 15:28:38 +0900

    use imos-san's script instead of pytest-xdist

--- .chainerci/tests.sh
+++ .chainerci/tests.sh
@@ -5,31 +5,38 @@ TEMP=$(mktemp -d)
 mount -t tmpfs tmpfs ${TEMP}/ -o size=100%
 
 if nvidia-smi --query-gpu=count --format=csv; then
-    RUNTIME='--runtime=nvidia'
-    NUM=
-    MARKS='gpu'
-
     mkdir -p ${TEMP}/.chainer
-else
-    RUNTIME=
-    NUM="-n $(nproc)"
-    MARKS='not gpu'
 
-    apt-get install -y --no-install-recommends unzip
-    gsutil cp gs://chainercv-pfn-public-ci/datasets-tiny.zip ${TEMP}/
-    unzip -q ${TEMP}/datasets-tiny.zip -d ${TEMP}/
-    rm ${TEMP}/datasets-tiny.zip
-fi
-
-docker run ${RUNTIME} --interactive --rm \
+    docker run --runtime=nvidia --interactive --rm \
        --volume $(pwd):/chainercv/ --workdir /chainercv/ \
        --volume ${TEMP}/.chainer/:/root/.chainer/ \
        --env MPLBACKEND=agg \
        hakuyume/chainercv:chainer${CHAINER}-devel \
        sh -ex << EOD
-pip${PYTHON} install --user pytest-xdist
 pip${PYTHON} install --user -e .
-python${PYTHON} -m pytest --color=no ${NUM} -m '${MARKS} and not mpi' tests/
+python${PYTHON} -m pytest --color=no -m 'gpu and not mpi' tests/
 mpiexec -n 2 --allow-run-as-root \
-        python${PYTHON} -m pytest --color=no -m '${MARKS} and mpi' tests/
+        python${PYTHON} -m pytest --color=no -m 'gpu and mpi' tests/
 EOD
+else
+    apt-get install -y --no-install-recommends unzip
+    gsutil cp gs://chainercv-pfn-public-ci/datasets-tiny.zip ${TEMP}/
+    unzip -q ${TEMP}/datasets-tiny.zip -d ${TEMP}/
+    rm ${TEMP}/datasets-tiny.zip
+
+    docker run --runtime=nvidia --interactive --rm \
+       --volume $(pwd):/chainercv/ --workdir /chainercv/ \
+       --volume ${TEMP}/.chainer/:/root/.chainer/ \
+       --env MPLBACKEND=agg \
+       hakuyume/chainercv:chainer${CHAINER}-devel \
+       sh -ex << EOD
+pip${PYTHON} install --user -e .
+curl -LO https://raw.githubusercontent.com/imos/public/master/parallel-pytest.py
+python${PYTHON} parallel-pytest.py \
+                --pytest='python${PYTHON} -m pytest --color=no -m \'not gpu and not mpi\'' \
+                --threads=$(nproc) \
+                --directory=tests/
+mpiexec -n 2 --allow-run-as-root \
+        python${PYTHON} -m pytest --color=no -m 'not gpu and mpi' tests/
+EOD
+fi
