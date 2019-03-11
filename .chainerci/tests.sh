271f1f1641bec8599f999ea208d4f4fd18f31920
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-03-11 10:18:07 +0900

    check not gpu and slow

--- .chainerci/tests.sh
+++ .chainerci/tests.sh
@@ -1,21 +1,35 @@
 #! /usr/bin/env sh
 set -eux
 
+TEMP=$(mktemp -d)
+mount -t tmpfs tmpfs ${TEMP}/ -o size=100%
+
 if nvidia-smi --query-gpu=count --format=csv; then
     RUNTIME='--runtime=nvidia'
+    NUM=
     MARKS='gpu'
+
+    mkdir -p ${TEMP}/.chainer
 else
     RUNTIME=
-    MARKS='not gpu and not slow'
+    NUM="-n $(nproc)"
+    MARKS='not gpu'
+
+    apt-get install -y --no-install-recommends unzip
+    gsutil cp gs://chainercv-pfn-public-ci/datasets-tiny.zip ${TEMP}/
+    unzip -q ${TEMP}/datasets-tiny.zip -d ${TEMP}/
+    rm ${TEMP}/datasets-tiny.zip
 fi
 
 docker run ${RUNTIME} --interactive --rm \
        --volume $(pwd):/chainercv --workdir /chainercv \
+       --volume ${TEMP}/.chainer:/root/.chainer \
        --env MPLBACKEND=agg \
        hakuyume/chainercv:chainer${CHAINER}-devel \
        sh -ex << EOD
+pip${PYTHON} install --user pytest-xdist
 pip${PYTHON} install --user -e .
-python${PYTHON} -m pytest --color=no -m '${MARKS} and not mpi' tests
+python${PYTHON} -m pytest --color=no ${NUM} -m '${MARKS} and not mpi' tests
 mpiexec -n 2 --allow-run-as-root \
         python${PYTHON} -m pytest --color=no -m '${MARKS} and mpi' tests
 EOD
