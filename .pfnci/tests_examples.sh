bb3de9b6c37bd8346ae2d9f3eb0aa5d79da88ed1
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-03-13 00:47:50 +0900

    add tests_examples.sh

---
+++ .pfnci/tests_examples.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env sh
+set -eux
+
+TEMP=$(mktemp -d)
+mount -t tmpfs tmpfs ${TEMP}/ -o size=100%
+apt-get install -y --no-install-recommends unzip
+gsutil -q cp gs://chainercv-pfn-public-ci/datasets-tiny.zip ${TEMP}/
+unzip -q ${TEMP}/datasets-tiny.zip -d ${TEMP}/
+rm ${TEMP}/datasets-tiny.zip
+
+docker run --runtime=nvidia --interactive --rm \
+       --volume $(pwd):/chainercv/ --workdir /chainercv/ \
+       --volume ${TEMP}/.chainer/:/root/.chainer/ \
+       --env PYTHON=python${PYTHON} \
+       --env PIP=pip${PYTHON} \
+       --env MPIEXEC='mpiexec -n 2 --allow-run-as-root' \
+       --env MPLBACKEND=agg \
+       --env PFNCI_SKIP='echo SKIP:' \
+       hakuyume/chainercv:chainer${CHAINER}-devel \
+       sh -ex << 'EOD'
+${PIP} install --user -e .
+for SCRIPT in $(find examples_tests -name '*.sh')
+do
+    sh -ex ${SCRIPT}
+done
+EOD
