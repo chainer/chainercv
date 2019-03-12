54347c1ec203b805bb20516912b1a7b023b9ea89
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-03-12 14:55:15 +0900

    remove old hack

--- .pfnci/tests.sh
+++ .pfnci/tests.sh
@@ -8,32 +8,6 @@ gsutil -q cp gs://chainercv-pfn-public-ci/datasets-tiny.zip ${TEMP}/
 unzip -q ${TEMP}/datasets-tiny.zip -d ${TEMP}/
 rm ${TEMP}/datasets-tiny.zip
 
-set +x
-# rename tests for pytest-xdist
-for TEST in $(find tests/ -name '*.py')
-do
-    cat - << 'EOD' >> ${TEST}
-def rename_tests(module_name):
-    import inspect
-    import re
-    import sys
-    import unittest
-
-    module = sys.modules[module_name]
-    for name, cls in inspect.getmembers(module, inspect.isclass):
-        if not issubclass(cls, unittest.TestCase):
-            continue
-        delattr(module, name)
-        new_name = re.sub(r'(?s)_{.+}', '', name)
-        new_cls = type(new_name, (cls,), {})
-        setattr(module, new_name, new_cls)
-
-
-rename_tests(__name__)
-EOD
-done
-set -x
-
 docker run --interactive --rm \
        --volume $(pwd):/chainercv/ --workdir /chainercv/ \
        --volume ${TEMP}/.chainer/:/root/.chainer/ \
