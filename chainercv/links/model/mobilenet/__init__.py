690f35b3942f64aa4f105178702e566ec5b3a1aa
okdshin <kokuzen@gmail.com>
2019-03-28 07:10:01 +0000

    fix mobilenet/__init__.py

--- chainercv/links/model/mobilenet/__init__.py
+++ chainercv/links/model/mobilenet/__init__.py
@@ -1,4 +1,4 @@
 from chainercv.links.model.mobilenet.tf_convolution_2d import TFConvolution2D  # NOQA
 from chainercv.links.model.mobilenet.tf_conv_2d_bn_activ import TFConv2DBNActiv  # NOQA
 from chainercv.links.model.mobilenet.expanded_conv_2d import ExpandedConv2D  # NOQA
-from chainercv.links.model.mobilenet.mobilenetv2 import MobileNetV2  # NOQA
+from chainercv.links.model.mobilenet.mobilenet_v2 import MobileNetV2  # NOQA
