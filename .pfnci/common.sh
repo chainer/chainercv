a0e00f0dd67326ca9c2d78c2588258803db105a1
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-03-29 14:39:15 +0900

    use nvme disk

--- .pfnci/common.sh
+++ .pfnci/common.sh
@@ -13,5 +13,15 @@ else
     DOCKER_IMAGE=hakuyume/chainercv:chainer${CHAINER}-devel-minimal
 fi
 
+mkfs.btrfs /dev/nvme0n1
+mount /dev/nvme0n1 /mnt/
+btrfs subvolume create /mnt/docker
+btrfs subvolume create /mnt/temp
+umount /mnt/
+
+systemctl stop docker.service
+mount /dev/nvme0n1 /var/lib/docker/ -o subvol=docker
+systemctl start docker.service
+
 TEMP=$(mktemp -d)
-mount -t tmpfs tmpfs ${TEMP}/ -o size=100%
+mount /dev/nvme0n1 ${TEMP}/ -o subvol=temp
