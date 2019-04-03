1e949ccd5753b48ceb33aead81a0e7659e2d405c
Yusuke Niitani <yuyuniitani@gmail.com>
2019-04-03 23:20:44 +0900

    delete mask_rcnn_tests

--- examples_tests/mask_rcnn_tests/test_demo.sh
+++
@@ -1,8 +0,0 @@
-cd examples/mask_rcnn
-curl -L https://cloud.githubusercontent.com/assets/2062128/26187667/9cb236da-3bd5-11e7-8bcf-7dbd4302e2dc.jpg \
-     -o sample.jpg
-
-$PYTHON demo.py --model mask_rcnn_fpn_resnet50 sample.jpg
-$PYTHON demo.py --model mask_rcnn_fpn_resnet50 --gpu 0 sample.jpg
-$PYTHON demo.py --model mask_rcnn_fpn_resnet101 sample.jpg
-$PYTHON demo.py --model mask_rcnn_fpn_resnet101 --gpu 0 sample.jpg
--- examples_tests/mask_rcnn_tests/test_train_multi.sh
+++
@@ -1,4 +0,0 @@
-cd examples/mask_rcnn
-
-$MPIEXEC $PYTHON train_multi.py --model mask_rcnn_fpn_resnet50 --batchsize 4 --iteration 9 --step 6 8
-$MPIEXEC $PYTHON train_multi.py --model mask_rcnn_fpn_resnet101 --batchsize 4 --iteration 9 --step 6 8
