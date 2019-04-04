4c8113f8cb3e35ed0149bf0169a1ce32265369df
Shingo Kitagawa <shingogo@hotmail.co.jp>
2019-04-04 10:03:07 +0900

    use PFNCI_SKIP and SAMPLE_IMAGE

--- examples_tests/fcis_tests/test_demo.sh
+++ examples_tests/fcis_tests/test_demo.sh
@@ -1,10 +1,6 @@
 cd examples/fcis
-curl -L https://cloud.githubusercontent.com/assets/2062128/26187667/9cb236da-3bd5-11e7-8bcf-7dbd4302e2dc.jpg \
-     -o sample.jpg
 
-# CPU test is too slow
-# $PYTHON demo.py --dataset sbd sample.jpg
-$PYTHON demo.py --dataset sbd --gpu 0 sample.jpg
-# CPU test is too slow
-# $PYTHON demo.py --dataset coco sample.jpg
-$PYTHON demo.py --dataset coco --gpu 0 sample.jpg
+$PFNCI_SKIP $PYTHON demo.py --dataset sbd $SAMPLE_IMAGE
+$PYTHON demo.py --dataset sbd --gpu 0 $SAMPLE_IMAGE
+$PFNCI_SKIP $PYTHON demo.py --dataset coco $SAMPLE_IMAGE
+$PYTHON demo.py --dataset coco --gpu 0 $SAMPLE_IMAGE
