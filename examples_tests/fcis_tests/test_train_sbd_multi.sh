8fe04c32bd5cc7801d1962dfc593ba905fbf9607
Shingo Kitagawa <shingogo@hotmail.co.jp>
2019-04-23 13:42:31 +0900

    modify fcis examples test parameter

--- examples_tests/fcis_tests/test_train_sbd_multi.sh
+++ examples_tests/fcis_tests/test_train_sbd_multi.sh
@@ -1,5 +1,5 @@
 cd examples/fcis
-sed -e "s/split='train')/split='train').slice[:5]/" -i train_sbd_multi.py
-sed -e "s/split='val')/split='val').slice[:5]/" -i train_sbd_multi.py
+sed -e "s/split='train')/split='train').slice[:6]/" -i train_sbd_multi.py
+sed -e "s/split='val')/split='val').slice[:6]/" -i train_sbd_multi.py
 
-$MPIEXEC $PYTHON train_sbd_multi.py --batchsize 1 --epoch 2 --cooldown-epoch 1
+$MPIEXEC $PYTHON train_sbd_multi.py --batchsize 2 --epoch 2 --cooldown-epoch 1
