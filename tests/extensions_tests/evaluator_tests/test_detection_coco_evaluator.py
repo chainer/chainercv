3190abc17491e35cf77c4c9f0e6fd3e4185de27b
Yusuke Niitani <yuyuniitani@gmail.com>
2019-05-28 12:51:54 +0900

    update test_detection_coco_evaluator

--- tests/extensions_tests/evaluator_tests/test_detection_coco_evaluator.py
+++ tests/extensions_tests/evaluator_tests/test_detection_coco_evaluator.py
@@ -38,10 +38,85 @@ class _DetectionStubLink(chainer.Link):
         return bboxes, labels, scores
 
 
-@unittest.skipUnless(_available, 'pycocotools is not installed')
-class TestDetectionCOCOEvaluator(unittest.TestCase):
+# @unittest.skipUnless(_available, 'pycocotools is not installed')
+# class TestDetectionCOCOEvaluator(unittest.TestCase):
+# 
+#     def setUp(self):
+#         bboxes = [generate_random_bbox(5, (256, 324), 24, 120)
+#                   for _ in range(10)]
+#         areas = [[np.array([(bb[2] - bb[0]) * bb[3] - bb[0]]) for bb in bbox]
+#                  for bbox in bboxes]
+#         labels = 2 * np.ones((10, 5), dtype=np.int32)
+#         crowdeds = np.zeros((10, 5))
+#         self.dataset = TupleDataset(
+#             np.random.uniform(size=(10, 3, 32, 48)),
+#             bboxes, labels, areas, crowdeds)
+#         self.link = _DetectionStubLink(bboxes, labels)
+#         self.iterator = SerialIterator(
+#             self.dataset, 5, repeat=False, shuffle=False)
+#         self.evaluator = DetectionCOCOEvaluator(
+#             self.iterator, self.link, label_names=('cls0', 'cls1', 'cls2'))
+#         self.expected_ap = 1
+# 
+#     def test_evaluate(self):
+#         reporter = chainer.Reporter()
+#         reporter.add_observer('target', self.link)
+#         with reporter:
+#             mean = self.evaluator.evaluate()
+# 
+#         # No observation is reported to the current reporter. Instead the
+#         # evaluator collect results in order to calculate their mean.
+#         self.assertEqual(len(reporter.observation), 0)
+# 
+#         key = 'ap/iou=0.50:0.95/area=all/max_dets=100'
+#         np.testing.assert_equal(
+#             mean['target/m{}'.format(key)], self.expected_ap)
+#         np.testing.assert_equal(mean['target/{}/cls0'.format(key)], np.nan)
+#         np.testing.assert_equal(mean['target/{}/cls1'.format(key)], np.nan)
+#         np.testing.assert_equal(
+#             mean['target/{}/cls2'.format(key)], self.expected_ap)
+# 
+#     def test_call(self):
+#         mean = self.evaluator()
+#         # main is used as default
+#         key = 'ap/iou=0.50:0.95/area=all/max_dets=100'
+#         np.testing.assert_equal(mean['main/m{}'.format(key)], self.expected_ap)
+#         np.testing.assert_equal(mean['main/{}/cls0'.format(key)], np.nan)
+#         np.testing.assert_equal(mean['main/{}/cls1'.format(key)], np.nan)
+#         np.testing.assert_equal(
+#             mean['main/{}/cls2'.format(key)], self.expected_ap)
+# 
+#     def test_evaluator_name(self):
+#         self.evaluator.name = 'eval'
+#         mean = self.evaluator()
+#         # name is used as a prefix
+# 
+#         key = 'ap/iou=0.50:0.95/area=all/max_dets=100'
+#         np.testing.assert_equal(
+#             mean['eval/main/m{}'.format(key)], self.expected_ap)
+#         np.testing.assert_equal(mean['eval/main/{}/cls0'.format(key)], np.nan)
+#         np.testing.assert_equal(mean['eval/main/{}/cls1'.format(key)], np.nan)
+#         np.testing.assert_equal(
+#             mean['eval/main/{}/cls2'.format(key)], self.expected_ap)
+# 
+#     def test_current_report(self):
+#         reporter = chainer.Reporter()
+#         with reporter:
+#             mean = self.evaluator()
+#         # The result is reported to the current reporter.
+#         self.assertEqual(reporter.observation, mean)
 
-    def _set_up(self, comm):
+
+@unittest.skipUnless(_available, 'pycocotools is not installed')
+class TestDetectionCOCOEvaluatorMPI(unittest.TestCase):
+
+    def setUp(self):
+        comm = create_communicator('naive')
+        self.comm = comm
+
+        batchsize_per_process = 5
+        batchsize = (batchsize_per_process * comm.size
+                     if comm is not None else batchsize_per_process)
         if comm is None or comm.rank == 0:
             bboxes = [generate_random_bbox(5, (256, 324), 24, 120)
                       for _ in range(10)]
@@ -53,118 +128,55 @@ class TestDetectionCOCOEvaluator(unittest.TestCase):
                 np.random.uniform(size=(10, 3, 32, 48)),
                 bboxes, labels, areas, crowdeds)
             initial_count = 0
-            iterator = SerialIterator(
-                dataset, 5 * comm.size, repeat=False, shuffle=False)
+            self.multi_iterator = SerialIterator(
+                dataset, batchsize, repeat=False, shuffle=False)
+            self.single_iterator = SerialIterator(
+                dataset, batchsize, repeat=False, shuffle=False)
         else:
             bboxes = None
             labels = None
-            initial_count = comm.rank * 5
-            iterator = None
+            initial_count = comm.rank * batchsize_per_process
+            self.multi_iterator = None
+            self.single_iterator = None
 
         if comm is not None:
             bboxes = comm.bcast_obj(bboxes)
             labels = comm.bcast_obj(labels)
-        self.link = _DetectionStubLink(bboxes, labels, initial_count)
-        self.expected_ap = 1
-        self.evaluator = DetectionCOCOEvaluator(
-            iterator, self.link, label_names=('cls0', 'cls1', 'cls2'),
-            comm=comm)
 
-    def _check_evaluate(self, comm=None):
-        self._set_up(comm)
+        self.bboxes = bboxes
+        self.labels = labels
+        self.initial_count = initial_count
+
+    def test_consistency(self):
+        multi_link = _DetectionStubLink(
+            self.bboxes, self.labels, self.initial_count)
+        multi_evaluator = DetectionCOCOEvaluator(
+            self.single_iterator, multi_link,
+             label_names=('cls0', 'cls1', 'cls2'),
+            comm=self.comm)
+
         reporter = chainer.Reporter()
-        reporter.add_observer('target', self.link)
+        reporter.add_observer('target', multi_link)
         with reporter:
-            mean = self.evaluator.evaluate()
-        if comm is not None and not comm.rank == 0:
-            self.assertEqual(mean, {})
+            multi_mean = multi_evaluator.evaluate()
+
+        if self.comm is not None and not self.comm.rank == 0:
+            self.assertEqual(multi_mean, {})
             return
 
-        # No observation is reported to the current reporter. Instead the
-        # evaluator collect results in order to calculate their mean.
-        self.assertEqual(len(reporter.observation), 0)
+        single_link = _DetectionStubLink(
+            self.bboxes, self.labels)
+        single_evaluator = DetectionCOCOEvaluator(
+            self.single_iterator, single_link,
+             label_names=('cls0', 'cls1', 'cls2'))
 
-        key = 'ap/iou=0.50:0.95/area=all/max_dets=100'
-        np.testing.assert_equal(
-            mean['target/m{}'.format(key)], self.expected_ap)
-        np.testing.assert_equal(mean['target/{}/cls0'.format(key)], np.nan)
-        np.testing.assert_equal(mean['target/{}/cls1'.format(key)], np.nan)
-        np.testing.assert_equal(
-            mean['target/{}/cls2'.format(key)], self.expected_ap)
-
-    def test_evaluate(self):
-        self._check_evaluate()
-
-    @attr.mpi
-    def test_evaluate_with_comm(self):
-        comm = create_communicator('naive')
-        self._check_evaluate(comm)
-
-    def _check_call(self, comm=None):
-        self._set_up(comm)
-        mean = self.evaluator()
-        if comm is not None and not comm.rank == 0:
-            self.assertEqual(mean, {})
-            return
-        # main is used as default
-        key = 'ap/iou=0.50:0.95/area=all/max_dets=100'
-        np.testing.assert_equal(mean['main/m{}'.format(key)], self.expected_ap)
-        np.testing.assert_equal(mean['main/{}/cls0'.format(key)], np.nan)
-        np.testing.assert_equal(mean['main/{}/cls1'.format(key)], np.nan)
-        np.testing.assert_equal(
-            mean['main/{}/cls2'.format(key)], self.expected_ap)
-
-    def test_call(self):
-        self._check_call()
-
-    @attr.mpi
-    def test_call_with_comm(self):
-        comm = create_communicator('naive')
-        self._check_call(comm)
-
-    def _check_evaluator_name(self, comm=None):
-        self._set_up(comm)
-        self.evaluator.name = 'eval'
-        mean = self.evaluator()
-        # name is used as a prefix
-        if comm is not None and not comm.rank == 0:
-            self.assertEqual(mean, {})
-            return
-
-        key = 'ap/iou=0.50:0.95/area=all/max_dets=100'
-        np.testing.assert_equal(
-            mean['eval/main/m{}'.format(key)], self.expected_ap)
-        np.testing.assert_equal(mean['eval/main/{}/cls0'.format(key)], np.nan)
-        np.testing.assert_equal(mean['eval/main/{}/cls1'.format(key)], np.nan)
-        np.testing.assert_equal(
-            mean['eval/main/{}/cls2'.format(key)], self.expected_ap)
-
-    def test_evaluator_name(self):
-        self._check_evaluator_name()
-
-    @attr.mpi
-    def test_evaluator_name_with_comm(self):
-        comm = create_communicator('naive')
-        self._check_evaluator_name(comm)
-
-    def _check_current_report(self, comm=None):
-        self._set_up(comm)
-        reporter = chainer.Reporter()
+        reporter.add_observer('target', single_link)
         with reporter:
-            mean = self.evaluator()
-        if comm is not None and not comm.rank == 0:
-            self.assertEqual(mean, {})
-            return
-        # The result is reported to the current reporter.
-        self.assertEqual(reporter.observation, mean)
+            single_mean = single_evaluator.evaluate()
 
-    def test_current_report(self):
-        self._check_current_report()
-
-    @attr.mpi
-    def test_current_report_with_comm(self):
-        comm = create_communicator('naive')
-        self._check_current_report(comm)
+        self.assertEqual(set(multi_mean.keys()), set(single_mean.keys()))
+        for key in multi_mean.keys():
+            np.testing.assert_equal(single_mean[key], multi_mean[key])
 
 
 testing.run_module(__name__, __file__)
